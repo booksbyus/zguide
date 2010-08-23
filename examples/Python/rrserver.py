f626be78fdb230f9a117f8edfb88e303a4f5aa11
Pieter Hintjens <ph@imatix.com>
2010-08-23 15:03:01 +0200

    Did not need time package

--- examples/Python/rrserver.py
+++ examples/Python/rrserver.py
@@ -1,10 +1,9 @@
 #
 #   Request-reply service in Python
-#   Connects REP socket to tcp://*:5560
+#   Connects REP socket to tcp://localhost:5560
 #   Expects "Hello" from client, replies with "World"
 #
 import zmq
-import time
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
