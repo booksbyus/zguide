c1fe4e9b9625f3fa3e24ed9af55ea6df62f2c91e
Pieter Hintjens <ph@imatix.com>
2010-09-05 11:10:09 +0200

    Cleaned up C++ examples - fixed mtrelay.cpp to use PAIR sockets - added workaround for issue 62 to mtrelay.cpp

--- examples/C++/mtrelay.cpp
+++ examples/C++/mtrelay.cpp
@@ -10,35 +10,27 @@
 //  Step 1 pushes one message to step 2
 
 void *step1 (void *arg) {
-
-	zmq::context_t * context = static_cast<zmq::context_t*>(arg);
-
+	zmq::context_t *context = static_cast<zmq::context_t*>(arg);
 	zmq::socket_t sender (*context, ZMQ_PAIR);
     sender.connect("inproc://step2");
 
     zmq::message_t message;
     sender.send(message);
-
     return (NULL);
 }
 
 //  Step 2 relays the signal to step 3
 
 void *step2 (void *arg) {
-
-	zmq::context_t * context = static_cast<zmq::context_t*>(arg);
-
+	zmq::context_t *context = static_cast<zmq::context_t*>(arg);
 	zmq::socket_t receiver (*context, ZMQ_PAIR);
     receiver.bind("inproc://step2");
-
-    zmq::socket_t sender (*context, ZMQ_PAIR);
-    sender.connect("inproc://step3");
-
     zmq::message_t message;
     receiver.recv(&message);
 
+    zmq::socket_t sender (*context, ZMQ_PAIR);
+    sender.connect("inproc://step3");
     sender.send(message);
-
     return (NULL);
 }
 
@@ -46,17 +38,18 @@ void *step2 (void *arg) {
 
 int main () {
 	zmq::context_t context(1);
-
-	zmq::socket_t receiver (context, ZMQ_PAIR);
+    zmq::socket_t receiver (context, ZMQ_PAIR);
     receiver.bind("inproc://step3");
 
     pthread_t thread;
     pthread_create (&thread, NULL, step2, &context);
+    //  Workaround for 0MQ issue 62
+    sleep (1);
     pthread_create (&thread, NULL, step1, &context);
 
     zmq::message_t message;
 	receiver.recv(&message);
-    std::cout << "Test successful!" << std::endl;
 
+    std::cout << "Test successful!" << std::endl;
     return 0;
 }
