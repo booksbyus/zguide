5115c900bd55fba70248ab86921717afc03597a6
Pieter Hintjens <ph@imatix.com>
2010-09-05 22:12:39 +0200

    Fixed mtrelay to get around inproc bind/connect issue

--- examples/C++/mtrelay.cpp
+++ examples/C++/mtrelay.cpp
@@ -7,48 +7,53 @@
 #include <pthread.h>
 #include <iostream>
 
-//  Step 1 pushes one message to step 2
-
 void *step1 (void *arg) {
-	zmq::context_t *context = static_cast<zmq::context_t*>(arg);
-	zmq::socket_t sender (*context, ZMQ_PAIR);
-    sender.connect("inproc://step2");
+    zmq::context_t *context = static_cast<zmq::context_t*>(arg);
 
+    //  Signal downstream to step 2
+    zmq::socket_t sender (*context, ZMQ_PAIR);
+    sender.connect("inproc://step2");
     zmq::message_t message;
     sender.send(message);
+
     return (NULL);
 }
 
 //  Step 2 relays the signal to step 3
 
 void *step2 (void *arg) {
-	zmq::context_t *context = static_cast<zmq::context_t*>(arg);
-	zmq::socket_t receiver (*context, ZMQ_PAIR);
+    zmq::context_t *context = static_cast<zmq::context_t*>(arg);
+
+    //  Bind to inproc: endpoint, then start upstream thread
+    zmq::socket_t receiver (*context, ZMQ_PAIR);
     receiver.bind("inproc://step2");
+    pthread_t thread;
+    pthread_create (&thread, NULL, step1, context);
+
+    //  Wait for signal
     zmq::message_t message;
     receiver.recv(&message);
 
+    //  Signal downstream to step 3
     zmq::socket_t sender (*context, ZMQ_PAIR);
     sender.connect("inproc://step3");
     sender.send(message);
+
     return (NULL);
 }
 
-//  Main program starts steps 1 and 2 and acts as step 3
-
 int main () {
-	zmq::context_t context(1);
+    zmq::context_t context(1);
+
+    //  Bind to inproc: endpoint, then start upstream thread
     zmq::socket_t receiver (context, ZMQ_PAIR);
     receiver.bind("inproc://step3");
-
     pthread_t thread;
     pthread_create (&thread, NULL, step2, &context);
-    //  Workaround for 0MQ issue 62
-    sleep (1);
-    pthread_create (&thread, NULL, step1, &context);
 
+    //  Wait for signal
     zmq::message_t message;
-	receiver.recv(&message);
+    receiver.recv(&message);
 
     std::cout << "Test successful!" << std::endl;
     return 0;
