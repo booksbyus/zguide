681822e97ba3843476c9c265576424a55eaf141f
Pieter Hintjens <ph@imatix.com>
2010-09-05 22:12:34 +0200

    Fixed mtrelay to get around inproc bind/connect issue

--- examples/C/mtrelay.c
+++ examples/C/mtrelay.c
@@ -3,42 +3,44 @@
 //
 #include "zhelpers.h"
 
-//  Step 1 pushes one message to step 2
-
 void *step1 (void *context) {
+    //  Signal downstream to step 2
     void *sender = zmq_socket (context, ZMQ_PAIR);
     zmq_connect (sender, "inproc://step2");
     s_send (sender, "");
+
     return (NULL);
 }
 
-//  Step 2 relays the signal to step 3
-
 void *step2 (void *context) {
+    //  Bind to inproc: endpoint, then start upstream thread
     void *receiver = zmq_socket (context, ZMQ_PAIR);
     zmq_bind (receiver, "inproc://step2");
+    pthread_t thread;
+    pthread_create (&thread, NULL, step1, context);
+
+    //  Wait for signal
     char *string = s_recv (receiver);
     free (string);
 
+    //  Signal downstream to step 3
     void *sender = zmq_socket (context, ZMQ_PAIR);
     zmq_connect (sender, "inproc://step3");
     s_send (sender, "");
+
     return (NULL);
 }
 
-//  Main program starts steps 1 and 2 and acts as step 3
-
 int main () {
     void *context = zmq_init (1);
+
+    //  Bind to inproc: endpoint, then start upstream thread
     void *receiver = zmq_socket (context, ZMQ_PAIR);
     zmq_bind (receiver, "inproc://step3");
-
     pthread_t thread;
     pthread_create (&thread, NULL, step2, context);
-    //  Workaround for 0MQ issue 62
-    sleep (1);
-    pthread_create (&thread, NULL, step1, context);
 
+    //  Wait for signal
     char *string = s_recv (receiver);
     free (string);
 
