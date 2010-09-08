7dfe5e506f33952f4220b7743f17f03781d7caa6
Nicola Peduzzi <thenikso@gmail.com>
2010-09-08 14:55:34 +0200

    Added more Java examples

--- examples/Java/msreader.java
+++ examples/Java/msreader.java
@@ -1,6 +1,46 @@
-No-one has translated the msreader example into Java yet.  Be the first to create
-msreader in Java and get one free Internet!  If you're the author of the Java
-binding, this is a great way to get people to use 0MQ in Java.
+import org.zeromq.ZMQ;
 
-To submit a translation, just email it to zeromq-dev.zeromq.org.
-Subscribe to this list at http://lists.zeromq.org/mailman/listinfo/zeromq-dev.
+//
+//  Reading from multiple sockets in Java
+//  This version uses a simple recv loop
+//
+//  Nicola Peduzzi <thenikso@gmail.com>
+//
+public class msreader {
+
+	public static void main(String[] args) throws Exception {
+		//  Prepare our context and sockets
+		ZMQ.Context context = ZMQ.context(1);
+
+		// Connect to task ventilator
+		ZMQ.Socket receiver = context.socket(ZMQ.PULL);
+		receiver.connect("tcp://localhost:5557");
+
+		//  Connect to weather server
+		ZMQ.Socket subscriber = context.socket(ZMQ.SUB);
+		subscriber.connect("tcp://localhost:5556");
+		subscriber.subscribe("10001 ".getBytes());
+
+		//  Initialize poll set
+		ZMQ.Poller items = context.poller(2);
+		items.register(receiver, 0);
+		items.register(subscriber, 0);
+
+		//  Process messages from both sockets
+		//  We prioritize traffic from the task ventilator
+		while (true) {
+			//  Process any waiting tasks
+			byte[] task;
+			while((task = receiver.recv(ZMQ.NOBLOCK)) != null) {
+					//  process task
+			}
+			//  Process any waiting weather updates
+			byte[] update;
+			while ((update = subscriber.recv(ZMQ.NOBLOCK)) != null) {
+				//  process weather update
+			}
+			//  No activity, so sleep for 1 msec
+			Thread.sleep(1000000);
+		}
+	}
+}
