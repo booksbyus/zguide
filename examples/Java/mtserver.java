2bbeebb61eb0b1403b62486dc6d124429eddc2c7
Pieter Hintjens <ph@imatix.com>
2010-09-09 11:27:09 +0200

    Updated stub to explain contribution process

--- examples/Java/mtserver.java
+++ examples/Java/mtserver.java
@@ -2,5 +2,12 @@ No-one has translated the mtserver example into Java yet.  Be the first to creat
 mtserver in Java and get one free Internet!  If you're the author of the Java
 binding, this is a great way to get people to use 0MQ in Java.
 
-To submit a translation, just email it to zeromq-dev.zeromq.org.
+To submit a new translation email it to 1000 4 20 24 25 29 30 44 46 107 109 114 121 1000EMAIL).  Please:
+
+* Stick to identical functionality and naming used in examples so that readers
+  can easily compare languages.
+* You MUST place your name as author in the examples so readers can contact you.
+* You MUST state in the email that you license your code under the MIT/X11
+  license.
+
 Subscribe to this list at http://lists.zeromq.org/mailman/listinfo/zeromq-dev.
--- examples/Lua/lruqueue.lua
+++ examples/Lua/lruqueue.lua
@@ -2,5 +2,12 @@ No-one has translated the lruqueue example into Lua yet.  Be the first to create
 lruqueue in Lua and get one free Internet!  If you're the author of the Lua
 binding, this is a great way to get people to use 0MQ in Lua.
 
-To submit a translation, just email it to 1000 4 20 24 25 29 30 44 46 107 109 114 121 1000EMAIL).
+To submit a new translation email it to 1000 4 20 24 25 29 30 44 46 107 109 114 121 1000EMAIL).  Please:
+
+* Stick to identical functionality and naming used in examples so that readers
+  can easily compare languages.
+* You MUST place your name as author in the examples so readers can contact you.
+* You MUST state in the email that you license your code under the MIT/X11
+  license.
+
 Subscribe to this list at http://lists.zeromq.org/mailman/listinfo/zeromq-dev.
