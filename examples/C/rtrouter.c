9e12caa26664646ce61793e23e963afb1eb6d150
Pieter Hintjens <ph@imatix.com>
2010-09-19 17:12:38 -0500

    Fixed socket names to match text

--- examples/C/rtrouter.c
+++ examples/C/rtrouter.c
@@ -7,24 +7,24 @@ int main () {
     void *context = zmq_init (1);
 
     void *server = zmq_socket (context, ZMQ_XREP);
-    zmq_setsockopt (server, ZMQ_IDENTITY, "SERVER", 6);
+    zmq_setsockopt (server, ZMQ_IDENTITY, "WORKER", 6);
     zmq_bind (server, "ipc://rtrouter.ipc");
 
     void *client = zmq_socket (context, ZMQ_XREP);
-    zmq_setsockopt (client, ZMQ_IDENTITY, "CLIENT", 6);
+    zmq_setsockopt (client, ZMQ_IDENTITY, "FRONTEND", 8);
     zmq_connect (client, "ipc://rtrouter.ipc");
 
     //  Give client time to connect...
     sleep (1);
 
-    s_sendmore (client, "SERVER");
+    s_sendmore (client, "WORKER");
     s_sendmore (client, "");
-    s_send (client, "send to client");
+    s_send (client, "send to worker");
     s_dump (server);
 
-    s_sendmore (server, "CLIENT");
+    s_sendmore (server, "FRONTEND");
     s_sendmore (server, "");
-    s_send (server, "send to server");
+    s_send (server, "send to front-end");
     s_dump (client);
 
     zmq_term (context);
