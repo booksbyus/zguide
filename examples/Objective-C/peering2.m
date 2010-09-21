7b8d8dfa6c5a07624abf76b141db7f0b5bb58493
Pieter Hintjens <ph@imatix.com>
2010-09-21 14:18:40 -0500

    Added example stub

---
+++ examples/Objective-C/peering2.m
@@ -0,0 +1,13 @@
+No-one has translated the peering2 example into Objective-C yet.  Be the first to create
+peering2 in Objective-C and get one free Internet!  If you're the author of the Objective-C
+binding, this is a great way to get people to use 0MQ in Objective-C.
+
+To submit a new translation email it to 1000 4 20 24 25 29 30 44 46 107 109 114 121 1000EMAIL).  Please:
+
+* Stick to identical functionality and naming used in examples so that readers
+  can easily compare languages.
+* You MUST place your name as author in the examples so readers can contact you.
+* You MUST state in the email that you license your code under the MIT/X11
+  license.
+
+Subscribe to this list at http://lists.zeromq.org/mailman/listinfo/zeromq-dev.
