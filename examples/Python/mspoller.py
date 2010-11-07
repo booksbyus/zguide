cc89cc09d6df98563fde84ff116f7ab69d6a9652
Kamil Kisiel <kamil@kamilkisiel.net>
2010-11-07 01:16:38 -0700

    Pythonic implementation of mspoller

--- examples/Python/mspoller.py
+++ examples/Python/mspoller.py
@@ -1,13 +1,36 @@
-No-one has translated the mspoller example into Python yet.  Be the first to create
-mspoller in Python and get one free Internet!  If you're the author of the Python
-binding, this is a great way to get people to use 0MQ in Python.
+#
+# Reading from multiple sockets
+# This version uses socket.poll()
+#
+import zmq
 
-To submit a new translation email it to zeromq-dev@lists.zeromq.org.  Please:
+context = zmq.Context()
 
-* Stick to identical functionality and naming used in examples so that readers
-  can easily compare languages.
-* You MUST place your name as author in the examples so readers can contact you.
-* You MUST state in the email that you license your code under the MIT/X11
-  license.
+# Connect to task ventilator
+receiver = context.socket(zmq.PULL)
+receiver.connect("tcp://localhost:5557")
 
-Subscribe to this list at http://lists.zeromq.org/mailman/listinfo/zeromq-dev.
+# Connect to weather server
+subscriber = context.socket(zmq.SUB)
+subscriber.connect("tcp://localhost:5556")
+subscriber.setsockopt(zmq.SUBSCRIBE, "10001")
+
+# Initialize the poll set
+items = zmq.Poller()
+items.register(receiver, flags=zmq.POLLIN)
+items.register(subscriber, flags=zmq.POLLIN)
+
+# Process messages from both sockets
+while True:
+    for item in items.poll(timeout=None):
+        if item[0] == receiver and item[1] & zmq.POLLIN:
+            message = receiver.recv()
+            # Process task
+        if item[0] == subscriber and item[1] & zmq.POLLIN:
+            message = subscriber.recv()
+            # Process weather update
+
+# We never get here
+receiver.close()
+subscriber.close()
+context.term()
