fbce3fa4d2e00fe9100708257017347282ff4c05
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2011-01-26 23:31:32 -0500

    Objective-C version of hwserver.m and build environment.
    Signed-off-by: Jeremy W. Sherman <jeremyw.sherman@gmail.com>

--- examples/Objective-C/hwserver.m
+++ examples/Objective-C/hwserver.m
@@ -1,13 +1,51 @@
-No-one has translated the hwserver example into Objective-C yet.  Be the first to create
-hwserver in Objective-C and get one free Internet!  If you're the author of the Objective-C
-binding, this is a great way to get people to use 0MQ in Objective-C.
+//
+//  Hello World server
+//  Binds REP socket to tcp://*:5555
+//  Expects "Hello" from client, replies with "World"
+//
+#import <Foundation/Foundation.h>
+#import "ZMQObjc.h"
 
-To submit a new translation email it to zeromq-dev@lists.zeromq.org.  Please:
+int
+main(void)
+{
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	ZMQContext *ctx = [[[ZMQContext alloc] initWithIOThreads:1] autorelease];
 
-* Stick to identical functionality and naming used in examples so that readers
-  can easily compare languages.
-* You MUST place your name as author in the examples so readers can contact you.
-* You MUST state in the email that you license your code under the MIT/X11
-  license.
+	/* Get a socket to talk to clients. */
+	static NSString *const kEndpoint = @"tcp://*:5555";
+	ZMQSocket *responder = [ctx socketWithType:ZMQ_REP];
+	[responder bindToEndpoint:kEndpoint];
 
-Subscribe to this list at http://lists.zeromq.org/mailman/listinfo/zeromq-dev.
+	for (;;) {
+		/* Create a local pool so that autoreleased objects can be disposed of
+		 * at the end of each go through the loop.
+		 * Otherwise, memory usage would continue to rise
+		 * until the end of the process.
+		 */
+		NSAutoreleasePool *localPool = [[NSAutoreleasePool alloc] init];
+
+		/* Block waiting for next request from client. */
+		NSData *request = [responder receiveDataWithFlags:0];
+		NSString *text = [[[NSString alloc]
+				initWithData:request encoding:NSUTF8StringEncoding] autorelease];
+		NSLog(@"Received request: %@", text);
+
+		/* "Work" for a bit. */
+		sleep(1);
+
+		/* Send reply back to client. */
+		static NSString *const kWorld = @"World";
+		const char *replyCString = [kWorld UTF8String];
+		const NSUInteger replyLength = [kWorld
+				lengthOfBytesUsingEncoding:NSUTF8StringEncoding];
+		NSData *reply = [NSData dataWithBytes:replyCString length:replyLength];
+		[responder sendData:reply withFlags:0];
+
+		[localPool drain];
+	}
+
+	/* Dispose of the context and socket. */
+	[pool drain];
+	return EXIT_SUCCESS;
+}
