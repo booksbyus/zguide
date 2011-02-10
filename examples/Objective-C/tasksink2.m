10b666a92fa437fb4578cdf897efddde685d0640
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2011-02-10 01:22:49 -0500

    ObjC: Add clean shutdown examples taskwork2 and tasksink2.
    Signed-off-by: Jeremy W. Sherman <jeremyw.sherman@gmail.com>

--- examples/Objective-C/tasksink2.m
+++ examples/Objective-C/tasksink2.m
@@ -1,13 +1,81 @@
-No-one has translated the tasksink2 example into Objective-C yet.  Be the first to create
-tasksink2 in Objective-C and get one free Internet!  If you're the author of the Objective-C
-binding, this is a great way to get people to use 0MQ in Objective-C.
+/* tasksink.m: PULLs workers' results from tcp://localhost:5558/. */
+/* You can wire up the vent, workers, and sink like so:
+ *    $ ./tasksink &
+ *    $ ./taskwork &  # Repeat this as many times as you want workers.
+ *    $ ./taskvent &
+ */
+#import <Foundation/Foundation.h>
+#import "ZMQObjC.h"
 
-To submit a new translation email it to zeromq-dev@lists.zeromq.org.  Please:
+#import <sys/time.h>
+#define NSEC_PER_MSEC (1000000)
+#define MSEC_PER_SEC  (1000)
 
-* Stick to identical functionality and naming used in examples so that readers
-  can easily compare languages.
-* You MUST place your name as author in the examples so readers can contact you.
-* You MUST state in the email that you license your code under the MIT/X11
-  license.
+int
+main(void)
+{
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
-Subscribe to this list at http://lists.zeromq.org/mailman/listinfo/zeromq-dev.
+	/* Prepare context and socket. */
+	ZMQContext *ctx = [[[ZMQContext alloc] initWithIOThreads:1U] autorelease];
+	ZMQSocket *pull = [ctx socketWithType:ZMQ_PULL];
+	[pull bindToEndpoint:@"tcp://*:5558"];
+
+	/* New control socket - send any message to kill workers. */
+	ZMQSocket *control = [ctx socketWithType:ZMQ_PUB];
+	[control bindToEndpoint:@"tcp://*:5559"];
+
+	/* Wait for batch start. */
+	/* Cast result to void because we don't actually care about the value.
+	 * The return value has been autoreleased, so no memory is leaked. */
+	(void)[pull receiveDataWithFlags:0];
+
+	/* Start clock. */
+	struct timeval tstart, tdiff, tend;
+	(void)gettimeofday(&tstart, NULL);
+
+	/* Process |kTaskCount| confirmations. */
+	static const int kTaskCount = 100;
+	for (int task = 0; task < kTaskCount; ++task) {
+		NSAutoreleasePool *p = [[NSAutoreleasePool alloc] init];
+
+		(void)[pull receiveDataWithFlags:0];
+
+		BOOL isMultipleOfTen = (0 == (task % 10));
+		if (isMultipleOfTen) {
+			fputs(":", stdout);
+		} else {
+			fputs(".", stdout);
+		}
+		fflush(stdout);
+
+		[p drain];
+	}
+	fputc('\n', stdout);
+
+	/* Stop clock. */
+	(void)gettimeofday(&tend, NULL);
+
+	/* Calculate the difference. */
+	tdiff.tv_sec = tend.tv_sec - tstart.tv_sec;
+	tdiff.tv_usec = tend.tv_usec - tstart.tv_usec;
+	if (tdiff.tv_usec < 0) {
+		tdiff.tv_sec -= 1;
+		tdiff.tv_usec += NSEC_PER_SEC;
+	}
+
+	/* Convert it to milliseconds. */
+	unsigned long totalMsec = tdiff.tv_sec * MSEC_PER_SEC
+			+ tdiff.tv_usec / NSEC_PER_MSEC;
+	NSLog(@"Total elapsed time: %lu ms", totalMsec);
+
+	/* Kill workers. Any message will do, including an empty one. */
+	[control sendData:nil withFlags:0];
+
+	/* Give 0MQ time to deliver the kill message. */
+	sleep(1);
+
+	[ctx closeSockets];
+	[pool drain];
+	return EXIT_SUCCESS;
+}
--- examples/Objective-C/taskwork2.m
+++ examples/Objective-C/taskwork2.m
@@ -1,13 +1,68 @@
-No-one has translated the taskwork2 example into Objective-C yet.  Be the first to create
-taskwork2 in Objective-C and get one free Internet!  If you're the author of the Objective-C
-binding, this is a great way to get people to use 0MQ in Objective-C.
+/* taskwork2.m: PULLs workload from tcp://localhost:5557
+ * PUSHes results to tcp://localhost:5558
+ * SUBs to tcp://localhost:5559 to receive kill signal (*** NEW ***)
+ */
+#import <Foundation/Foundation.h>
+#import "ZMQObjC.h"
+#define NSEC_PER_MSEC (1000000)
 
-To submit a new translation email it to zeromq-dev@lists.zeromq.org.  Please:
+int
+main(void)
+{
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	ZMQContext *ctx = [[[ZMQContext alloc] initWithIOThreads:1U] autorelease];
 
-* Stick to identical functionality and naming used in examples so that readers
-  can easily compare languages.
-* You MUST place your name as author in the examples so readers can contact you.
-* You MUST state in the email that you license your code under the MIT/X11
-  license.
+	/* (jws/2011-02-05)!!!: Do NOT terminate the endpoint with a final slash.
+	 * If you connect to @"tcp://localhost:5557/", you will get
+	 *    Assertion failed: rc == 0 (zmq_connecter.cpp:46)
+	 * instead of a connected socket. Binding works fine, though. */
+	ZMQSocket *pull = [ctx socketWithType:ZMQ_PULL];
+	[pull connectToEndpoint:@"tcp://localhost:5557"];
 
-Subscribe to this list at http://lists.zeromq.org/mailman/listinfo/zeromq-dev.
+	ZMQSocket *push = [ctx socketWithType:ZMQ_PUSH];
+	[push connectToEndpoint:@"tcp://localhost:5558"];
+
+	ZMQSocket *control = [ctx socketWithType:ZMQ_SUB];
+	[control setData:nil forOption:ZMQ_SUBSCRIBE];
+	[control connectToEndpoint:@"tcp://localhost:5559"];
+
+	/* Process tasks forever, multiplexing between |pull| and |control|. */
+	enum {POLL_PULL, POLL_CONTROL};
+	zmq_pollitem_t items[2];
+	[pull getPollItem:&items[POLL_PULL] forEvents:ZMQ_POLLIN];
+	[control getPollItem:&items[POLL_CONTROL] forEvents:ZMQ_POLLIN];
+	size_t itemCount = sizeof(items)/sizeof(*items);
+
+	struct timespec t;
+	NSData *emptyData = [NSData data];
+	bool shouldExit = false;
+	while (!shouldExit) {
+		NSAutoreleasePool *p = [[NSAutoreleasePool alloc] init];
+
+		[ZMQContext pollWithItems:items count:itemCount
+				timeoutAfterUsec:ZMQPollTimeoutNever];
+		if (items[POLL_PULL].revents & ZMQ_POLLIN) {
+			NSData *d = [pull receiveDataWithFlags:0];
+			NSString *s = [NSString stringWithUTF8String:[d bytes]];
+			t.tv_sec = 0;
+			t.tv_nsec = [s integerValue] * NSEC_PER_MSEC;
+			printf("%d.", [s intValue]);
+			fflush(stdout);
+
+			/* Do work, then report finished. */
+			(void)nanosleep(&t, NULL);
+			[push sendData:emptyData withFlags:0];
+		}
+
+		/* Any inbound data on |control| signals us to die. */
+		if (items[POLL_CONTROL].revents & ZMQ_POLLIN) {
+			/* Do NOT just break here: |p| must be drained first. */
+			shouldExit = true;
+		}
+		[p drain];
+	}
+
+	[ctx closeSockets];
+	[pool drain];
+	return EXIT_SUCCESS;
+}
