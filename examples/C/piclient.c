359f68000640576f9d10f30e5d374b3ad11c8b43
Pieter Hintjens <ph@imatix.com>
2011-02-27 21:44:29 +0100

    Working on pirate patterns

--- examples/C/piclient.c
+++ examples/C/piclient.c
@@ -1,54 +1,77 @@
 //
 //  Client-side pirate
 //  Use zmq_poll to do a safe request-reply
-//  To run, start hwserver and then randomly kill/restart it
+//  To run, start piserver and then randomly kill/restart it
 //
 #include "zhelpers.h"
 
-#define REQUEST_TIMEOUT     2500    //  msecs
+#define REQUEST_TIMEOUT     2500    //  msecs, (> 1000!)
 #define REQUEST_RETRIES     3       //  Before we abandon
 
-int main () {
-    void *context = zmq_init (1);
+//  Helper function that returns a new configured socket
+//  connected to the Hello World server
+//
+static void *
+s_client_socket (void *context) {
+    printf ("I: connecting to server...\n");
     void *client = zmq_socket (context, ZMQ_REQ);
     zmq_connect (client, "tcp://localhost:5555");
 
-    //  Socket to talk to server
-    printf ("Connecting to hello world server...\n");
+    //  Configure socket to not wait at close time
+    int linger = 0;
+    zmq_setsockopt (client, ZMQ_LINGER, &linger, sizeof (linger));
+    return client;
+}
 
+int main (void)
+{
+    s_version_assert (2, 1);
+    void *context = zmq_init (1);
+    void *client = s_client_socket (context);
+
+    int sequence = 0;
     int retries_left = REQUEST_RETRIES;
-    while (1) {
-        s_send (client, "HELLO");
+    while (retries_left) {
+        //  We send a request, then we work to get a reply
+        char request [10];
+        sprintf (request, "%d", ++sequence);
+        s_send (client, request);
 
-        zmq_pollitem_t items [] = { { client, 0, ZMQ_POLLIN, 0 } };
-        zmq_poll (items, 1, REQUEST_TIMEOUT * 1000);
+        int expect_reply = 1;
+        while (expect_reply) {
+            //  Poll socket for a reply, with timeout
+            zmq_pollitem_t items [] = { { client, 0, ZMQ_POLLIN, 0 } };
+            zmq_poll (items, 1, REQUEST_TIMEOUT * 1000);
 
-        if (items [0].revents & ZMQ_POLLIN) {
-            //  We got a reply from the server
-            char *reply = s_recv (client);
-            free (reply);
-            printf ("Server replied\n");
-            retries_left = REQUEST_RETRIES;
-        }
-        else {
-            //  No response from server
+            //  If we got a reply, process it
+            if (items [0].revents & ZMQ_POLLIN) {
+                //  We got a reply from the server, must match sequence
+                char *reply = s_recv (client);
+                if (atoi (reply) == sequence) {
+                    printf ("I: server replied OK (%s)\n", reply);
+                    retries_left = REQUEST_RETRIES;
+                    expect_reply = 0;
+                }
+                else
+                    printf ("E: malformed reply from server: %s\n", reply);
+
+                free (reply);
+            }
+            else
             if (--retries_left == 0) {
                 printf ("E: server seems to be offline, abandoning\n");
                 break;
             }
             else {
                 printf ("W: no response from server, retrying...\n");
-                //  Create a new socket, old one will be confused
+                //  Old socket will be confused; close it and open a new one
                 zmq_close (client);
-                client = zmq_socket (context, ZMQ_REQ);
-                zmq_connect (client, "tcp://localhost:5555");
+                client = s_client_socket (context);
+                //  Send request again, on new socket
+                s_send (client, request);
             }
         }
     }
-    //  Discard any outgoing requests
-    int linger = 0;
-    zmq_setsockopt (client, ZMQ_LINGER, &linger, sizeof (linger));
-    
     zmq_close (client);
     zmq_term (context);
     return 0;
---
+++ examples/C/piworker1.c
@@ -0,0 +1,49 @@
+//
+//  Client-side pirate worker
+//  Connects REQ socket to tcp://*:5556
+//  Implements worker part of LRU queueing
+//
+#include "zhelpers.h"
+#include "zmsg.c"
+
+int main (void)
+{
+    srandom ((unsigned) time (NULL));
+
+    void *context = zmq_init (1);
+    void *worker = zmq_socket (context, ZMQ_REQ);
+
+    //  Set random identity to make tracing easier
+    char identity [10];
+    sprintf (identity, "%04X-%04X", randof (0x10000), randof (0x10000));
+    zmq_setsockopt (worker, ZMQ_IDENTITY, identity, strlen (identity));
+    zmq_connect (worker, "tcp://localhost:5556");
+
+    //  Tell queue we're ready for work
+    printf ("I: worker %s is ready\n", identity);
+    s_send (worker, "READY");
+
+    int cycles = 0;
+    while (1) {
+        zmsg_t *zmsg = zmsg_recv (worker);
+        cycles++;
+
+        //  Simulate various problems, after a few cycles
+        if (cycles > 3 && randof (5) == 0) {
+            printf ("I: worker %s simulating a crash\n", identity);
+            zmsg_destroy (&zmsg);
+            break;
+        }
+        else
+        if (cycles > 3 && randof (5) == 0) {
+            printf ("I: worker %s simulating CPU overload\n", identity);
+            sleep (5);
+        }
+        printf ("I: worker %s normal reply (%s)\n", identity, zmsg_body (zmsg));
+        sleep (1);
+        zmsg_send (&zmsg, worker);
+    }
+    zmq_close (worker);
+    zmq_term (context);
+    return 0;
+}
