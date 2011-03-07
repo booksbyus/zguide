35a1bf485e9520ec8b38a3126834e698d8181953
Hoelzlwimmer Andreas <Andreas.Hoelzlwimmer@fh-hagenberg.at>
2011-03-07 16:59:48 +0100

    Translated zmsg into C++

---
+++ examples/C++/zmsg.hpp
@@ -0,0 +1,372 @@
+/*  =========================================================================
+    zmsg.class
+
+    Multipart message class for example applications.
+
+    Follows the ZFL class conventions and is further developed as the ZFL
+    zfl_msg class.  See http://zfl.zeromq.org for more details.
+
+    -------------------------------------------------------------------------
+    Copyright (c) 1991-2010 iMatix Corporation <www.imatix.com>
+    Copyright other contributors as noted in the AUTHORS file.
+
+    This file is part of the ZeroMQ Guide: http://zguide.zeromq.org
+
+    This is free software; you can redistribute it and/or modify it under the
+    terms of the GNU Lesser General Public License as published by the Free
+    Software Foundation; either version 3 of the License, or (at your option)
+    any later version.
+
+    This software is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABIL-
+    ITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+    Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+    =========================================================================
+
+    Andreas Hoelzlwimmer <andreas.hoelzlwimmer@fh-hagenberg.at>
+*/
+
+#ifndef __ZMSG_H_INCLUDED__
+#define __ZMSG_H_INCLUDED__
+
+#include "zhelpers.hpp"
+
+#include <vector>
+#include <string>
+#include <stdarg.h>
+
+class zmsg {
+public:
+   typedef std::basic_string<unsigned char> ustring;
+
+   zmsg() {
+
+   }
+
+   //  --------------------------------------------------------------------------
+   //  Constructor, sets initial body
+   zmsg(char *body) {
+      body_set(body);
+   }
+
+   virtual ~zmsg() {
+      clear();
+   }
+
+   //  --------------------------------------------------------------------------
+   //  Erases all messages
+   void clear() {
+      m_part_data.clear();
+   }
+
+   void set_part(size_t part_nbr, unsigned char *data) {
+      if (part_nbr < m_part_data.size() && part_nbr >= 0) {
+         m_part_data[part_nbr] = data;
+      }
+   }
+
+   bool recv(zmq::socket_t & socket) {
+      clear();
+      while(1) {
+         zmq::message_t message(0);
+         try {
+            if (!socket.recv(&message, 0)) {
+               return false;
+            }
+         } catch (zmq::error_t error) {
+            printf("E: %s\n", error.what());
+            return false;
+         }
+         ustring data = (unsigned char*) message.data();
+         //std::cerr << "recv: \"" << (unsigned char*) message.data() << "\", size " << message.size() << std::endl;
+         if (message.size() == 17 && ((unsigned char *)message.data())[0] == 0) {
+            char *uuidstr = encode_uuid((unsigned char*) message.data());
+            push_back(uuidstr);
+         }
+         else {
+            data[message.size()] = 0;
+            push_back((char *)data.c_str());
+         }
+         int64_t more;
+         size_t more_size = sizeof(more);
+         socket.getsockopt(ZMQ_RCVMORE, &more, &more_size);
+         if (!more) {
+            break;
+         }
+      }
+      return true;
+   }
+
+   void send(zmq::socket_t & socket) {
+       for (size_t part_nbr = 0; part_nbr < m_part_data.size(); part_nbr++) {
+          zmq::message_t message;
+          ustring data = m_part_data[part_nbr];
+          if (data.size() == 33 && data [0] == '@') {
+             ustring uuidbin = decode_uuid ((char *) data.c_str());
+             message.rebuild(17);
+             memcpy(message.data(), uuidbin.c_str(), uuidbin.size());
+             //message.rebuild((void *) uuidbin.c_str(), 17, NULL, NULL);
+          }
+          else {
+             message.rebuild(data.size());
+             memcpy(message.data(), data.c_str(), data.size());
+             //(void *) data.c_str(), data.size(), NULL, NULL);
+          }
+          try {
+             socket.send(message, part_nbr < m_part_data.size() - 1 ? ZMQ_SNDMORE : 0);
+          } catch (zmq::error_t error) {
+             assert(error.num()!=0);
+          }
+       }
+       clear();
+   }
+
+   size_t parts() {
+      return m_part_data.size();
+   }
+
+   void body_set(const char *body) {
+      if (m_part_data.size() > 0) {
+         m_part_data.erase(m_part_data.end()-1);
+      }
+      push_back((char*)body);
+   }
+
+   void
+   body_fmt (const char *format, ...)
+   {
+       char value [255 + 1];
+       va_list args;
+
+       va_start (args, format);
+       vsnprintf (value, 255, format, args);
+       va_end (args);
+
+       body_set (value);
+   }
+
+   char * body ()
+   {
+       if (m_part_data.size())
+           return ((char *) m_part_data [m_part_data.size() - 1].c_str());
+       else
+           return 0;
+   }
+
+   // zmsg_push
+   void push_front(char *part) {
+      m_part_data.insert(m_part_data.begin(), (unsigned char*)part);
+   }
+
+   // zmsg_append
+   void push_back(char *part) {
+      m_part_data.push_back((unsigned char*)part);
+   }
+
+   //  --------------------------------------------------------------------------
+   //  Formats 17-byte UUID as 33-char string starting with '@'
+   //  Lets us print UUIDs as C strings and use them as addresses
+   //
+   static char *
+   encode_uuid (unsigned char *data)
+   {
+       static char
+           hex_char [] = "0123456789ABCDEF";
+
+       assert (data [0] == 0);
+       char *uuidstr = new char[34];
+       uuidstr [0] = '@';
+       int byte_nbr;
+       for (byte_nbr = 0; byte_nbr < 16; byte_nbr++) {
+           uuidstr [byte_nbr * 2 + 1] = hex_char [data [byte_nbr + 1] >> 4];
+           uuidstr [byte_nbr * 2 + 2] = hex_char [data [byte_nbr + 1] & 15];
+       }
+       uuidstr [33] = 0;
+       return (uuidstr);
+   }
+
+
+   // --------------------------------------------------------------------------
+   // Formats 17-byte UUID as 33-char string starting with '@'
+   // Lets us print UUIDs as C strings and use them as addresses
+   //
+   static unsigned char *
+   decode_uuid (char *uuidstr)
+   {
+       static char
+           hex_to_bin [128] = {
+              -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, /* */
+              -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, /* */
+              -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, /* */
+               0, 1, 2, 3, 4, 5, 6, 7, 8, 9,-1,-1,-1,-1,-1,-1, /* 0..9 */
+              -1,10,11,12,13,14,15,-1,-1,-1,-1,-1,-1,-1,-1,-1, /* A..F */
+              -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, /* */
+              -1,10,11,12,13,14,15,-1,-1,-1,-1,-1,-1,-1,-1,-1, /* a..f */
+              -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1 }; /* */
+
+       assert (strlen (uuidstr) == 33);
+       assert (uuidstr [0] == '@');
+       unsigned char *data = new unsigned char[17];
+       int byte_nbr;
+       data [0] = 0;
+       for (byte_nbr = 0; byte_nbr < 16; byte_nbr++)
+           data [byte_nbr + 1]
+               = (hex_to_bin [uuidstr [byte_nbr * 2 + 1] & 127] << 4)
+               + (hex_to_bin [uuidstr [byte_nbr * 2 + 2] & 127]);
+
+       return (data);
+   }
+
+   // zmsg_pop
+   ustring pop_front() {
+      if (m_part_data.size() == 0) {
+         return 0;
+      }
+      ustring part = m_part_data.front();
+      m_part_data.erase(m_part_data.begin());
+      return part;
+   }
+
+   void append (const char *part)
+   {
+       assert (part);
+       push_back((char*)part);
+   }
+
+   char *address() {
+      if (m_part_data.size()>0) {
+         return (char*)m_part_data[0].c_str();
+      } else {
+         return 0;
+      }
+   }
+
+   void wrap(const char *address, const char *delim) {
+      if (delim) {
+         push_front((char*)delim);
+      }
+      push_front((char*)address);
+   }
+
+   char * unwrap() {
+      if (m_part_data.size() == 0) {
+         return NULL;
+      }
+      char *addr = (char*)pop_front().c_str();
+      if (*address() == 0) {
+         pop_front();
+      }
+      return addr;
+   }
+
+   void dump() {
+      for (unsigned int part_nbr = 0; part_nbr < m_part_data.size(); part_nbr++) {
+          ustring data = m_part_data [part_nbr];
+
+          // Dump the message as text or binary
+          int is_text = 1;
+          for (unsigned int char_nbr = 0; char_nbr < data.size(); char_nbr++)
+              if (data [char_nbr] < 32 || data [char_nbr] > 127)
+                  is_text = 0;
+
+          fprintf (stderr, "[%03d] ", (int) data.size());
+          for (unsigned int char_nbr = 0; char_nbr < data.size(); char_nbr++) {
+              if (is_text)
+                  fprintf (stderr, "%c", data [char_nbr]);
+              else
+                  fprintf (stderr, "%02X", (unsigned char) data [char_nbr]);
+          }
+          fprintf (stderr, "\n");
+      }
+      fprintf (stderr, "\n");
+      fflush (stderr);
+   }
+
+   static int
+   test(int verbose)
+   {
+      zmq::context_t context(1);
+      zmq::socket_t output(context, ZMQ_XREQ);
+      try {
+         output.bind("ipc://zmsg_selftest.ipc");
+      } catch (zmq::error_t error) {
+         assert(error.num()!=0);
+      }
+      zmq::socket_t input(context, ZMQ_XREP);
+      try {
+         input.connect("ipc://zmsg_selftest.ipc");
+      } catch (zmq::error_t error) {
+         assert(error.num()!=0);
+      }
+
+      zmsg zm;
+      zm.body_set((char *)"Hello");
+      assert (strcmp(zm.body(), "Hello") == 0);
+
+      zm.send(output);
+      assert(zm.parts()==0);
+
+      zm.recv(input);
+      assert (zm.parts() == 2);
+      if(verbose) {
+         zm.dump();
+      }
+
+      assert (strcmp(zm.body(), "Hello") == 0);
+
+      zm.clear();
+      zm.body_set("Hello");
+      zm.wrap("address1", "");
+      zm.wrap("address2", 0);
+      assert (zm.parts() == 4);
+      zm.send(output);
+
+      zm.recv(input);
+      if (verbose) {
+         zm.dump();
+      }
+      assert (zm.parts() == 5);
+      assert (strlen(zm.address()) == 33);
+      zm.unwrap();
+      assert (strcmp(zm.address(), "address2") == 0);
+      zm.body_fmt ("%c%s", 'W', "orld");
+      zm.send(output);
+
+      zm.recv (input);
+      zm.unwrap ();
+      assert (zm.parts () == 4);
+      assert (strcmp (zm.body (), "World") == 0);
+      std::string part = zm.unwrap ();
+      assert (part.compare("address2") == 0);
+
+      // Pull off address 1, check that empty part was dropped
+      part = zm.unwrap ();
+      assert (part.compare("address1") == 0);
+      assert (zm.parts () == 1);
+
+      // Check that message body was correctly modified
+      part = (char*)zm.pop_front ().c_str();
+      assert (part.compare("World") == 0);
+      assert (zm.parts () == 0);
+
+      // Check append method
+      zm.append ("Hello");
+      zm.append ("World!");
+      assert (zm.parts () == 2);
+      assert (strcmp (zm.body (), "World!") == 0);
+
+      zm.clear();
+      assert (zm.parts() == 0);
+
+      printf ("OK\n");
+      return 0;
+   }
+
+private:
+   std::vector<std::basic_string<unsigned char> > m_part_data;
+};
+
+#endif /* ZMSG_H_ */
