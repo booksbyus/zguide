ec8afa009b4e5cb42d451f26d779a61f80cfa72c
Pieter Hintjens <ph@imatix.com>
2011-03-08 09:36:32 +0100

    Titanic work in progress

--- examples/C/ticlient.c
+++ examples/C/ticlient.c
@@ -6,13 +6,42 @@
 
 int main (void)
 {
-    mdcli_t *session = mdcli_new ("tcp://localhost:5555", 1);
+    mdcli_t *session = mdcli_new ("tcp://localhost:5555", 0);
 
+    //  Send request to Titanic
     zmsg_t *request = zmsg_new ("Hello world");
     zmsg_push (request, "echo");
     zmsg_t *reply = mdcli_send (session, "titanic-request", &request);
+    if (!reply) {
+        printf ("E: can't reach titanic-request service\n");
+        return 0;
+    }
+    char *uuid = zmsg_pop (reply);
     zmsg_destroy (&reply);
+    printf ("I: request UUID: %s\n", uuid);
 
+    //  Wait until we get a reply
+    while (1) {
+        request = zmsg_new (uuid);
+        reply = mdcli_send (session, "titanic-reply", &request);
+        if (!reply) {
+            printf ("E: can't reach titanic-reply service\n");
+            return 0;
+        }
+        char *status = zmsg_pop (reply);
+        if (atoi (status) == 200) {
+            printf ("Reply: %s\n", zmsg_body (reply));
+            zmsg_destroy (&reply);
+
+            //  Close request
+            request = zmsg_new (uuid);
+            reply = mdcli_send (session, "titanic-close", &request);
+            if (!reply)
+                printf ("E: can't reach titanic-close service\n");
+            break;
+        }
+    }
+    zmsg_destroy (&reply);
     mdcli_destroy (&session);
     return 0;
 }
--- examples/C/titanic.c
+++ examples/C/titanic.c
@@ -11,13 +11,14 @@
 //
 
 #include "mdwrkapi.c"
+#include "zfile.h"
 #include <uuid/uuid.h>
 
 //  Return a new UUID as a printable character string
 //  Caller must free returned string when finished with it
 
 static char *
-s_get_new_uuid (void)
+s_generate_uuid (void)
 {
     char hex_char [] = "0123456789ABCDEF";
     char *uuidstr = calloc (1, sizeof (uuid_t) * 2 + 1);
@@ -31,33 +32,68 @@ s_get_new_uuid (void)
     return (uuidstr);
 }
 
+//  Returns freshly allocated request filename for given UUID
+
+#define TITANIC_DIR ".titanic"
+
+static char *
+s_request_filename (char *uuid) {
+    char *filename = malloc (256);
+    snprintf (filename, 256, TITANIC_DIR "/%s.req", uuid);
+    return (filename);
+}
+
+//  Returns freshly allocated reply filename for given UUID
+
+static char *
+s_reply_filename (char *uuid) {
+    char *filename = malloc (256);
+    snprintf (filename, 256, TITANIC_DIR "/%s.rep", uuid);
+    return (filename);
+}
+
+
+//  -----------------------------------------------------------------------
+//  Titanic request service
 
 static void *
 titanic_request (void *args)
 {
-    mdwrk_t *session = mdwrk_new ("tcp://localhost:5555", "titanic-request", 1);
+    mdwrk_t *session = mdwrk_new (
+        "tcp://localhost:5555", "titanic-request", 1);
     zmsg_t *reply = NULL;
     while (1) {
+        //  Get next request from broker
         zmsg_t *request = mdwrk_recv (session, &reply);
         if (!request)
             break;
-        char *uuid = s_get_new_uuid ();
 
-        //- create <uuid>.req disk file with message
-        //- zmsg_save/zmsg_load
+        //  Generate UUID and save message to disk
+        char *uuid = s_generate_uuid ();
+        char *filename = s_request_filename (uuid);
+        FILE *file = fopen (filename, "w");
+        assert (file);
+        zmsg_save (&request, file);
+        fclose (file);
+        free (filename);
 
+        //  Reply to client with generated UUID
         reply = zmsg_new (uuid);
         free (uuid);
-        zmsg_destroy (&request);
     }
     mdwrk_destroy (&session);
     return 0;
 }
 
+
+//  -----------------------------------------------------------------------
+//  Titanic reply service
+
 static void *
 titanic_reply (void *context)
 {
-    mdwrk_t *session = mdwrk_new ("tcp://localhost:5555", "titanic-reply", 1);
+    mdwrk_t *session = mdwrk_new (
+        "tcp://localhost:5555", "titanic-reply", 1);
     zmsg_t *reply = NULL;
     while (1) {
         zmsg_t *request = mdwrk_recv (session, &reply);
@@ -71,10 +107,15 @@ titanic_reply (void *context)
     return 0;
 }
 
+
+//  -----------------------------------------------------------------------
+//  Titanic close service
+
 static void *
 titanic_close (void *context)
 {
-    mdwrk_t *session = mdwrk_new ("tcp://localhost:5555", "titanic-close", 1);
+    mdwrk_t *session = mdwrk_new (
+        "tcp://localhost:5555", "titanic-close", 1);
     zmsg_t *reply = NULL;
     while (1) {
         zmsg_t *request = mdwrk_recv (session, &reply);
@@ -90,8 +131,15 @@ titanic_close (void *context)
 int main (void)
 {
     s_version_assert (2, 1);
+
+    //  Prepare working directory
+    file_mkdir (TITANIC_DIR);
+
     pthread_t thread;
     pthread_create (&thread, NULL, titanic_request, NULL);
+//    pthread_create (&thread, NULL, titanic_reply, NULL);
+ //   pthread_create (&thread, NULL, titanic_close, NULL);
+    
     pthread_join (thread, NULL);
 
     // Dispatcher thread
