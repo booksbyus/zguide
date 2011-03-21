3cf21995d0822f142d0341a0a48b15e3ac4fd2c9
Robert G. Jakabosky <bobby@sharedrealm.com>
2011-03-21 00:58:03 -0700

    Add Lua version of zversion.

--- examples/Lua/zversion.lua
+++ examples/Lua/zversion.lua
@@ -1,13 +1,7 @@
-No-one has translated the zversion example into Lua yet.  Be the first to create
-zversion in Lua and get one free Internet!  If you're the author of the Lua
-binding, this is a great way to get people to use 0MQ in Lua.
+--
+--  Report 0MQ version
+--
+local zmq = require"zmq"
 
-To submit a new translation email it to zeromq-dev@lists.zeromq.org.  Please:
+print("Current 0MQ version is " .. table.concat(zmq.version(), '.'))
 
-* Stick to identical functionality and naming used in examples so that readers
-  can easily compare languages.
-* You MUST place your name as author in the examples so readers can contact you.
-* You MUST state in the email that you license your code under the MIT/X11
-  license.
-
-Subscribe to the email list at http://lists.zeromq.org/mailman/listinfo/zeromq-dev.
