13639a1e49484d0ba15343bda07c96f6fea7fcde
Robert G. Jakabosky <bobby@sharedrealm.com>
2011-03-21 23:09:44 -0700

    Add Lua version of zmsg object.  Added zhelpers utility functions.

---
+++ examples/Lua/zhelpers.lua
@@ -0,0 +1,134 @@
+
+local format = string.format
+local random = math.random
+local write = io.write
+
+local zmq = require"zmq"
+
+function printf(fmt, ...)
+    return write(format(fmt, ...))
+end
+
+function fprintf(file, fmt, ...)
+    return file:write(format(fmt, ...))
+end
+
+--  Provide random number from 0..(num-1)
+function randof(num)
+    return random(0, num-1)
+end
+
+--  Receive 0MQ string from socket and convert into C string
+--  Caller must free returned string. Returns NULL if the context
+--  is being terminated.
+function s_recv(socket)
+    return socket:recv()
+end
+
+--  Convert C string to 0MQ string and send to socket
+function s_send(socket, string)
+    return socket:recv(string)
+end
+
+--  Sends string as 0MQ string, as multipart non-terminal
+function s_sendmore(socket, string)
+    return socket:recv(string, zmq.SNDMORE)
+end
+
+--  Receives all message parts from socket, prints neatly
+--
+function s_dump(socket)
+    puts ("----------------------------------------");
+    while true do
+        --  Process all parts of the message
+        local data = socket:recv();
+
+        --  Dump the message as text or binary
+        local is_text = 1;
+        for i=1,#data do
+            local c = data:byte(i)
+            if (c < 32 or c > 127) then
+                is_text = 0
+                break
+            end
+        end
+
+        printf ("[%03d] ", #data);
+        if (is_text) then
+            write(data);
+        else
+            for i=1,#data do
+                printf ("%02X", data:byte(i));
+            end
+        end
+        printf ("\n");
+
+        --  Multipart detection
+        local more = socket:getopt(zmq.RCVMORE)
+        if (more == 0) then
+            break;      --  Last message part
+        end
+    end
+end
+
+--  Set simple random printable identity on socket
+--
+function s_set_id(socket)
+    local identity = format("%04X-%04X", randof (0x10000), randof (0x10000));
+    return socket:setopt(zmq.IDENTITY, identity);
+end
+
+
+--  Report 0MQ version number
+--
+function s_version()
+    local major, minor, patch = unpack(zmq.version())
+    printf ("Current 0MQ version is %d.%d.%d\n", major, minor, patch);
+end
+
+--  Require at least some specified version
+function s_version_assert(want_major, want_minor)
+    local major, minor, patch = unpack(zmq.version())
+    if (major < want_major or (major == want_major and minor < want_minor)) then
+        printf ("Current 0MQ version is %d.%d\n", major, minor);
+        printf ("Application needs at least %d.%d - cannot continue\n",
+            want_major, want_minor);
+        os.exit(-1);
+    end
+end
+
+-- try to get Lua socket module
+local has_socket, socket = pcall(require, "socket")
+
+--  Sleep for a number of milliseconds
+if has_socket then
+    function s_sleep(msecs)
+        socket.sleep(msecs / 1000)
+    end
+else
+    function s_sleep(msecs)
+        os.execute("sleep " .. tostring(msecs / 1000))
+    end
+end
+
+--  Return current system clock as milliseconds
+if socket then
+    function s_clock()
+        return socket.gettime() * 1000
+    end
+else
+    print("------ WARNING: Please install LuaSocket for a better clock source.")
+    function s_clock()
+        return os.time() * 1000
+    end
+end
+
+--  Print formatted string to stdout, prefixed by date/time and
+--  terminated with a newline.
+
+function s_console(format, ...)
+    print(os.date("%y-%m-%d %H:%M:%S "));
+    printf(fmt, ...);
+    print()
+end
+
--- examples/Lua/zmsg.lua
+++ examples/Lua/zmsg.lua
@@ -1,13 +1,443 @@
-No-one has translated the zmsg example into Lua yet.  Be the first to create
-zmsg in Lua and get one free Internet!  If you're the author of the Lua
-binding, this is a great way to get people to use 0MQ in Lua.
+require"zhelpers"
 
-To submit a new translation email it to zeromq-dev@lists.zeromq.org.  Please:
+local zmq = require"zmq"
 
-* Stick to identical functionality and naming used in examples so that readers
-  can easily compare languages.
-* You MUST place your name as author in the examples so readers can contact you.
-* You MUST state in the email that you license your code under the MIT/X11
-  license.
+local setmetatable = setmetatable
+local tinsert = table.insert
+local tremove = table.remove
+local char = string.char
+local tonumber = tonumber
+local assert = assert
+local tmpfile = io.tmpfile
+local stderr = io.stderr
+local fprintf = fprintf
+local printf = printf
+local print = print
 
-Subscribe to this list at http://lists.zeromq.org/mailman/listinfo/zeromq-dev.
+-- hex <-> binary conversion tables.
+hex_to_byte_map = {}
+byte_to_hex_map = {}
+for i=0,255 do
+    local byte = char(i)
+    local hex = ('%02X'):format(i)
+    -- byte to hex
+    byte_to_hex_map[byte] = hex
+    -- hex to byte
+    hex_to_byte_map[hex] = byte
+end
+
+--  --------------------------------------------------------------------------
+--  Formats 17-byte UUID as 33-char string starting with '@'
+--  Lets us print UUIDs as C strings and use them as addresses
+--
+local function s_encode_uuid(data)
+    assert(data:byte(1) == 0)
+    -- remove first 0 byte
+    data = data:sub(2)
+    return '@' .. data:gsub("(.)", byte_to_hex_map)
+end
+
+
+--  --------------------------------------------------------------------------
+--  Formats 17-byte UUID as 33-char string starting with '@'
+--  Lets us print UUIDs as C strings and use them as addresses
+--
+local function s_decode_uuid(uuidstr)
+    assert(uuidstr:sub(1,1) == '@')
+    -- remove '@'
+    uuidstr = uuidstr:sub(2)
+    -- uppercase
+    uuidstr = uuidstr:upper()
+    return '\0' .. uuidstr:gsub("(%x%x)", hex_to_byte_map)
+end
+
+
+local msg_mt = {}
+msg_mt.__index = msg_mt
+
+--  --------------------------------------------------------------------------
+--  Constructor, sets initial body if provided
+local function zmsg_new(body)
+    return setmetatable({body}, msg_mt)
+end
+
+
+--  --------------------------------------------------------------------------
+--  Compare two messages
+
+function msg_mt:__eq(msg2)
+    local len = #self
+    -- different lengths?
+    if len ~= #msg2 then return false end
+    for i=1,len do
+        -- different parts?
+        if self[i] ~= msg2[i] then
+            return false
+        end
+    end
+    -- same message contents.
+    return true
+end
+
+
+--  --------------------------------------------------------------------------
+--  Duplicate message
+
+function msg_mt:dup()
+    local dup = zmsg_new()
+
+    for i=1,#self do
+        dup[i] = self[i]
+    end
+    return dup
+end
+
+
+--  --------------------------------------------------------------------------
+--  Send message to socket
+--  Destroys message after sending
+
+function msg_mt:send(socket)
+    assert(socket)
+    local len = #self
+
+    for i=1,len do
+        local part = self[i]
+
+        --  Unmangle 0MQ identities for writing to the socket
+        if (#part == 33 and part:sub(1,1) == '@') then
+            part = s_decode_uuid (part)
+        end
+        socket:send(part, (i < len) and zmq.SNDMORE or 0)
+        -- clear zmsg
+        self[i] = nil
+    end
+end
+
+
+--  --------------------------------------------------------------------------
+--  Report size of message
+
+function msg_mt:parts()
+    return #self
+end
+
+
+--  --------------------------------------------------------------------------
+--  Return pointer to message body, if any
+--  Caller should not modify the provided data
+
+function msg_mt:body()
+    return self[#self]
+end
+
+
+--  --------------------------------------------------------------------------
+--  Set message body as copy of provided string
+--  If message is empty, creates a new message body
+
+function msg_mt:body_set(body)
+    self[#self] = body
+end
+
+
+--  --------------------------------------------------------------------------
+--  Set message body using printf format
+--  If message is empty, creates a new message body
+--  Hard-coded to max. 255 characters for this simplified class
+
+function msg_mt:body_fmt(fmt, ...)
+    self[#self] = fmt:format(...)
+end
+
+
+--  --------------------------------------------------------------------------
+--  Push message part to front of message parts
+
+function msg_mt:push(part)
+    tinsert(self, 1, part)
+end
+
+
+--  --------------------------------------------------------------------------
+--  Pop message part off front of message parts
+--  Caller should free returned string when finished with it
+
+function msg_mt:pop()
+    return tremove(self, 1)
+end
+
+
+--  --------------------------------------------------------------------------
+--  Append message part to end of message parts
+
+function msg_mt:append(part)
+    self[#self + 1] = part
+end
+
+
+--  --------------------------------------------------------------------------
+--  Return pointer to outer message address, if any
+--  Caller should not modify the provided data
+
+function msg_mt:address()
+    return self[1]
+end
+
+
+--  --------------------------------------------------------------------------
+--  Wraps message in new address envelope
+--  If delim is not null, creates two-part envelope
+--  Call this _after_ zmq_body_set(), not before
+
+function msg_mt:wrap(address, delim)
+    assert(address)
+
+    --  Push optional delimiter and then address
+    if delim then
+        tinsert(self, 1, delim)
+    end
+    tinsert(self, 1, address)
+end
+
+
+--  --------------------------------------------------------------------------
+--  Unwraps outer message envelope and returns address
+--  Discards empty message part after address, if any
+--  Caller should free returned string when finished with it
+
+function msg_mt:unwrap()
+    assert(self)
+
+    local address = tremove(self, 1)
+    -- check for deliminator
+    local peek = self[1]
+    if (peek and #peek == 0) then
+        -- pop deliminator
+        tremove(self, 1)
+    end
+    return address
+end
+
+
+--  --------------------------------------------------------------------------
+--  Save message to a file
+
+function msg_mt:save(file)
+    assert(file)
+
+    for i=1,#self do
+        local part = self[i]
+        -- prefix each message part with: "<8 digit hex number>:"
+        file:write(("%08X:"):format(#part))
+        file:write(part)
+    end
+end
+
+
+--  --------------------------------------------------------------------------
+--  Dump message to stderr, for debugging and tracing
+
+function msg_mt:dump()
+    fprintf(stderr, "--------------------------------------\n")
+    for i=1,#self do
+        local data = self[i]
+
+        --  Dump the message as text or binary
+        local is_text = true
+        for i=1,#data do
+            local c = data:byte(i)
+            if (c < 32 or c > 127) then
+                is_text = false
+                break
+            end
+        end
+
+        fprintf(stderr, "[%03d] ", #data)
+        if (is_text) then
+            stderr:write(data)
+        else
+            for i=1,#data do
+                fprintf(stderr, "%02X", data:byte(i))
+            end
+        end
+        fprintf(stderr, "\n")
+    end
+    stderr:flush()
+end
+
+module(...)
+
+-- export zmsg_new()
+_M.new = zmsg_new
+
+
+--  --------------------------------------------------------------------------
+--  Receive message from socket
+--  Creates a new message and returns it
+--  Blocks on recv if socket is not ready for input
+
+function recv(socket)
+    assert(socket)
+    local self = zmsg_new()
+
+    while true do
+        local data, err = socket:recv()
+        if not data then
+            return self, err
+        end
+        --  We handle 0MQ UUIDs as printable strings
+        if (#data == 17 and data:byte(1) == 0) then
+            --  Store message part as string uuid
+            data = s_encode_uuid (data)
+        end
+        self[#self + 1] = data
+
+        if (socket:getopt(zmq.RCVMORE) == 0) then
+            break      --  Last message part
+        end
+    end
+    return self
+end
+
+
+--  --------------------------------------------------------------------------
+--  Load message from file
+--  Creates a new message and returns as many parts as can be read.
+
+function load(file)
+    assert(file)
+    local self = zmsg_new ()
+
+    while true do
+        -- Read part prefix
+        local size = file:read(9)
+        if not size then break end
+        -- decode part size to number
+        size = tonumber(size:sub(1,8), 16)
+        -- Read part from file
+        local part = file:read(size)
+        if not part then break end
+        self[#self + 1] = part
+    end
+    return self
+end
+
+
+--  --------------------------------------------------------------------------
+--  Runs self test of class
+
+local function test_msg_save_load(msg, verbose)
+    -- create temp. file.
+    local file = tmpfile()
+    -- print all test messages.
+    if verbose then
+        fprintf(stderr, "Dump test messages:\n")
+        msg:dump()
+    end
+    -- save all messages
+    msg:save(file)
+    local write_len = file:seek()
+    -- load all messages
+    file:seek('set', 0)
+    local msg2 = load(file)
+    if verbose then
+        fprintf(stderr, "Load test messages from temp. file:\n")
+        msg2:dump()
+    end
+    local read_len = file:seek()
+    assert(write_len == read_len, "Different number of bytes written vs. read.")
+    assert(msg == msg2, "Save/Load corrupted message.")
+    file:close()
+end
+
+function run_test (verbose)
+    local msg
+    local part
+    local address
+    local msgs = {}
+
+    printf (" * zmsg: ")
+
+    --  Prepare our context and sockets
+    local context = zmq.init(1)
+    local output = context:socket(zmq.XREQ)
+    assert(output:bind("ipc://zmsg_selftest.ipc"))
+    local input = context:socket(zmq.XREP)
+    assert(input:connect("ipc://zmsg_selftest.ipc"))
+
+    --  Test send and receive of single-part message
+    msg = new ("Hello")
+    assert(msg:body() == "Hello")
+    msgs[#msgs + 1] = msg:dup()
+    msg:send(output)
+
+    msg = recv (input)
+    msgs[#msgs + 1] = msg:dup()
+    assert(msg:parts() == 2)
+    if (verbose) then
+        msg:dump()
+    end
+    assert(msg:body() == "Hello")
+
+    --  Test send and receive of multi-part message
+    msg = new ("Hello")
+    msg:wrap("address1", "")
+    msg:wrap("address2")
+    assert(msg:parts() == 4)
+    msgs[#msgs + 1] = msg:dup()
+    msg:send(output)
+
+    msg = recv (input)
+    msgs[#msgs + 1] = msg:dup()
+    if (verbose) then
+        msg:dump()
+    end
+    assert(msg:parts() == 5)
+    address = msg:address()
+    assert(#address == 33)
+    address = msg:unwrap()
+    assert(msg:address() == "address2")
+    msg:body_fmt("%s%s", 'W', "orld")
+    msg:send(output)
+
+    msg = recv (input)
+    msgs[#msgs + 1] = msg:dup()
+    address = msg:unwrap()
+    assert(msg:parts() == 4)
+    assert(msg:body() == "World")
+    local part = msg:unwrap()
+    assert(part == "address2")
+
+    --  Pull off address 1, check that empty part was dropped
+    part = msg:unwrap()
+    assert(part == "address1")
+    assert(msg:parts() == 1)
+
+    --  Check that message body was correctly modified
+    part = msg:pop()
+    assert(part == "World")
+    assert(msg:parts() == 0)
+
+    --  Check append method
+    msg:append("Hello")
+    msg:append("World!")
+    assert(msg:parts() == 2)
+    assert(msg:body() == "World!")
+
+    printf ("OK\n")
+
+    printf (" * zmsg save/load: ")
+
+    for i=1,#msgs do
+        test_msg_save_load(msgs[i], verbose)
+    end
+
+    printf ("OK\n")
+
+    input:close()
+    output:close()
+    context:term()
+    return 0
+end
+
