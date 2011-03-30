11e47fe3c31e091a288b7950b27e780c333d19e0
Robert G. Jakabosky <bobby@sharedrealm.com>
2011-03-30 12:47:30 -0700

    Improved speed of zmsg object.

--- examples/Lua/zmsg.lua
+++ examples/Lua/zmsg.lua
@@ -11,6 +11,7 @@ local zmq = require"zmq"
 local setmetatable = setmetatable
 local tinsert = table.insert
 local tremove = table.remove
+local tconcat = table.concat
 local char = string.char
 local tonumber = tonumber
 local assert = assert
@@ -24,6 +25,7 @@ for i=0,255 do
     local byte = char(i)
     local hex = ('%02X'):format(i)
     -- byte to hex
+    byte_to_hex_map[i] = hex
     byte_to_hex_map[byte] = hex
     -- hex to byte
     hex_to_byte_map[hex] = byte
@@ -33,11 +35,14 @@ end
 --  Formats 17-byte UUID as 33-char string starting with '@'
 --  Lets us print UUIDs as C strings and use them as addresses
 --
+local tmp_enc = {}
 local function s_encode_uuid(data)
     assert(data:byte(1) == 0)
-    -- remove first 0 byte
-    data = data:sub(2)
-    return '@' .. data:gsub("(.)", byte_to_hex_map)
+    tmp_enc[1] = '@'
+    for i=2,17 do
+        tmp_enc[i] = byte_to_hex_map[data:byte(i,i)]
+    end
+    return tconcat(tmp_enc, '', 1, 17)
 end
 
 
@@ -45,18 +50,31 @@ end
 --  Formats 17-byte UUID as 33-char string starting with '@'
 --  Lets us print UUIDs as C strings and use them as addresses
 --
+local tmp_dec = {}
 local function s_decode_uuid(uuidstr)
     assert(uuidstr:sub(1,1) == '@')
-    -- remove '@'
-    uuidstr = uuidstr:sub(2)
-    -- uppercase
-    uuidstr = uuidstr:upper()
-    return '\0' .. uuidstr:gsub("(%x%x)", hex_to_byte_map)
+    tmp_dec[1] = '\0'
+    local n=1
+    for i=2,33,2 do
+        n = n + 1
+        tmp_dec[n] = hex_to_byte_map[uuidstr:sub(i,i+1)]
+    end
+    return tconcat(tmp_dec, '', 1, n)
 end
 
 
 local msg_mt = {}
 msg_mt.__index = msg_mt
+
+local function treverse(t)
+    local len = #t
+    local half = math.floor(len / 2)
+    for i=1,half do
+        t[len], t[i] = t[i], t[len]
+        len = len - 1
+    end
+    return t
+end
 
 --  --------------------------------------------------------------------------
 --  Constructor, sets initial body if provided
@@ -104,14 +122,14 @@ function msg_mt:send(socket)
     assert(socket)
     local len = #self
 
-    for i=1,len do
+    for i=len,1,-1 do
         local part = self[i]
 
         --  Unmangle 0MQ identities for writing to the socket
         if (#part == 33 and part:sub(1,1) == '@') then
             part = s_decode_uuid (part)
         end
-        socket:send(part, (i < len) and zmq.SNDMORE or 0)
+        socket:send(part, (i > 1) and zmq.SNDMORE or 0)
         -- clear zmsg
         self[i] = nil
     end
@@ -130,11 +148,7 @@ end
 --  Return pointer to message body, if any
 
 function msg_mt:body()
-    local len = #self
-    if len > 0 then
-        return self[len]
-    end
-    return nil
+    return self[1]
 end
 
 
@@ -143,12 +157,7 @@ end
 --  If message is empty, creates a new message body
 
 function msg_mt:body_set(body)
-    local len = #self
-    if len > 0 then
-        self[len] = body
-    else
-        self[1] = body
-    end
+    self[1] = body
 end
 
 
@@ -166,7 +175,7 @@ end
 --  Push message part to front of message parts
 
 function msg_mt:push(part)
-    tinsert(self, 1, part)
+    self[#self + 1] = part
 end
 
 
@@ -174,7 +183,10 @@ end
 --  Pop message part off front of message parts
 
 function msg_mt:pop()
-    return tremove(self, 1)
+    local len = #self
+    local part = self[len]
+    self[len] = nil
+    return part
 end
 
 
@@ -182,7 +194,7 @@ end
 --  Append message part to end of message parts
 
 function msg_mt:append(part)
-    self[#self + 1] = part
+    tinsert(self, 1, part)
 end
 
 
@@ -190,7 +202,7 @@ end
 --  Return pointer to outer message address, if any
 
 function msg_mt:address()
-    return self[1]
+    return self[#self]
 end
 
 
@@ -234,7 +246,7 @@ end
 function msg_mt:save(file)
     assert(file)
 
-    for i=1,#self do
+    for i=#self,1,-1 do
         local part = self[i]
         -- prefix each message part with: "<8 digit hex number>:"
         file:write(("%08X:"):format(#part))
@@ -248,7 +260,7 @@ end
 
 function msg_mt:dump()
     fprintf(stderr, "--------------------------------------\n")
-    for i=1,#self do
+    for i=#self,1,-1 do
         local data = self[i]
 
         --  Dump the message as text or binary
@@ -292,7 +304,7 @@ function recv(socket, flags)
     while true do
         local data, err = socket:recv(flags)
         if not data then
-            return self, err
+            return treverse(self), err
         end
         --  We handle 0MQ UUIDs as printable strings
         if (#data == 17 and data:byte(1) == 0) then
@@ -305,7 +317,7 @@ function recv(socket, flags)
             break      --  Last message part
         end
     end
-    return self
+    return treverse(self)
 end
 
 
@@ -328,7 +340,7 @@ function load(file)
         if not part then break end
         self[#self + 1] = part
     end
-    return self
+    return treverse(self)
 end
 
 
