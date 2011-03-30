2b20bbd0d4f09ea265378b4d5e172f2551176f8d
Robert G. Jakabosky <bobby@sharedrealm.com>
2011-03-30 13:25:57 -0700

    Corrected whitespace style.

--- examples/Lua/mdcliapi.lua
+++ examples/Lua/mdcliapi.lua
@@ -19,34 +19,34 @@ local obj_mt = {}
 obj_mt.__index = obj_mt
 
 function obj_mt:set_timeout(timeout)
-	self.timeout = timeout
+    self.timeout = timeout
 end
 
 function obj_mt:set_retries(retries)
-	self.retries = retries
+    self.retries = retries
 end
 
 function obj_mt:destroy()
-	if self.client then self.client:close() end
-	self.context:term()
+    if self.client then self.client:close() end
+    self.context:term()
 end
 
 local function s_mdcli_connect_to_broker(self)
-	-- close old socket.
-	if self.client then
-		self.poller:remove(self.client)
-		self.client:close()
-	end
-	self.client = assert(self.context:socket(zmq.REQ))
-	assert(self.client:setopt(zmq.LINGER, 0))
-	assert(self.client:connect(self.broker))
-	if self.verbose then
-		s_console("I: connecting to broker at %s...", self.broker)
-	end
-	-- add socket to poller
-	self.poller:add(self.client, zmq.POLLIN, function()
-		self.got_reply = true
-	end)
+    -- close old socket.
+    if self.client then
+        self.poller:remove(self.client)
+        self.client:close()
+    end
+    self.client = assert(self.context:socket(zmq.REQ))
+    assert(self.client:setopt(zmq.LINGER, 0))
+    assert(self.client:connect(self.broker))
+    if self.verbose then
+        s_console("I: connecting to broker at %s...", self.broker)
+    end
+    -- add socket to poller
+    self.poller:add(self.client, zmq.POLLIN, function()
+        self.got_reply = true
+    end)
 end
 
 --
@@ -54,72 +54,72 @@ end
 -- Returns the reply message or nil if there was no reply.
 --
 function obj_mt:send(service, request)
-	-- Prefix request with protocol frames
-	-- Frame 1: "MDPCxy" (six bytes, MDP/Client x.y)
-	-- Frame 2: Service name (printable string)
-	request:push(service)
-	request:push(mdp.MDPC_CLIENT)
-	if self.verbose then
-		s_console("I: send request to '%s' service:", service)
-		request:dump()
-	end
+    -- Prefix request with protocol frames
+    -- Frame 1: "MDPCxy" (six bytes, MDP/Client x.y)
+    -- Frame 2: Service name (printable string)
+    request:push(service)
+    request:push(mdp.MDPC_CLIENT)
+    if self.verbose then
+        s_console("I: send request to '%s' service:", service)
+        request:dump()
+    end
 
-	local retries = self.retries
-	while (retries > 0) do
-		local msg = request:dup()
-		msg:send(self.client)
-		self.got_reply = false
+    local retries = self.retries
+    while (retries > 0) do
+        local msg = request:dup()
+        msg:send(self.client)
+        self.got_reply = false
 
-		while true do
-			local cnt = assert(self.poller:poll(self.timeout * 1000))
-			if cnt ~= 0 and self.got_reply then
-				local msg = zmsg.recv(self.client)
-				if self.verbose then
-					s_console("I: received reply:")
-					msg:dump()
-				end
-				assert(msg:parts() >= 3)
+        while true do
+            local cnt = assert(self.poller:poll(self.timeout * 1000))
+            if cnt ~= 0 and self.got_reply then
+                local msg = zmsg.recv(self.client)
+                if self.verbose then
+                    s_console("I: received reply:")
+                    msg:dump()
+                end
+                assert(msg:parts() >= 3)
 
-				local header = msg:pop()
-				assert(header == mdp.MDPC_CLIENT)
-				local reply_service = msg:pop()
-				assert(reply_service == service)
-				return msg
-			else
-				retries = retries - 1
-				if (retries > 0) then
-					if self.verbose then
-						s_console("W: no reply, reconnecting...")
-					end
-					-- Reconnect
-					s_mdcli_connect_to_broker(self)
-					break -- outer loop will resend request.
-				else
-					if self.verbose then
-						s_console("W: permanent error, abandoning request")
-					end
-					return nil -- Giving up
-				end
-			end
-		end
-	end
+                local header = msg:pop()
+                assert(header == mdp.MDPC_CLIENT)
+                local reply_service = msg:pop()
+                assert(reply_service == service)
+                return msg
+            else
+                retries = retries - 1
+                if (retries > 0) then
+                    if self.verbose then
+                        s_console("W: no reply, reconnecting...")
+                    end
+                    -- Reconnect
+                    s_mdcli_connect_to_broker(self)
+                    break -- outer loop will resend request.
+                else
+                    if self.verbose then
+                        s_console("W: permanent error, abandoning request")
+                    end
+                    return nil -- Giving up
+                end
+            end
+        end
+    end
 end
 
 module(...)
 
 function new(broker, verbose)
-	s_version_assert (2, 1);
-	local self = setmetatable({
-		context = zmq.init(1),
-		poller = zpoller.new(1),
-		broker = broker,
-		verbose = verbose,
-		timeout = 2500, -- msecs
-		retries = 3,    -- before we abandon
-	}, obj_mt)
+    s_version_assert (2, 1);
+    local self = setmetatable({
+        context = zmq.init(1),
+        poller = zpoller.new(1),
+        broker = broker,
+        verbose = verbose,
+        timeout = 2500, -- msecs
+        retries = 3,    -- before we abandon
+    }, obj_mt)
 
-	s_mdcli_connect_to_broker(self)
-	return self
+    s_mdcli_connect_to_broker(self)
+    return self
 end
 
 setmetatable(_M, { __call = function(self, ...) return new(...) end })
--- examples/Lua/mdwrkapi.lua
+++ examples/Lua/mdwrkapi.lua
@@ -21,145 +21,145 @@ local obj_mt = {}
 obj_mt.__index = obj_mt
 
 function obj_mt:set_heartbeat(heartbeat)
-	self.heartbeat = heartbeat
+    self.heartbeat = heartbeat
 end
 
 function obj_mt:set_reconnect(reconnect)
-	self.reconnect = reconnect
+    self.reconnect = reconnect
 end
 
 function obj_mt:destroy()
-	if self.worker then self.worker:close() end
-	self.context:term()
+    if self.worker then self.worker:close() end
+    self.context:term()
 end
 
 -- Send message to broker
 -- If no msg is provided, create one internally
 local function s_mdwrk_send_to_broker(self, command, option, msg)
-	msg = msg or zmsg.new()
+    msg = msg or zmsg.new()
 
-	-- Stack protocol envelope to start of message
-	if option then
-		msg:push(option)
-	end
-	msg:push(command)
-	msg:push(mdp.MDPW_WORKER)
-	msg:push("")
+    -- Stack protocol envelope to start of message
+    if option then
+        msg:push(option)
+    end
+    msg:push(command)
+    msg:push(mdp.MDPW_WORKER)
+    msg:push("")
 
-	if self.verbose then
-		s_console("I: sending %s to broker", mdp.mdps_commands[command])
-		msg:dump()
-	end
-	msg:send(self.worker)
+    if self.verbose then
+        s_console("I: sending %s to broker", mdp.mdps_commands[command])
+        msg:dump()
+    end
+    msg:send(self.worker)
 end
 
 local function s_mdwrk_connect_to_broker(self)
-	-- close old socket.
-	if self.worker then
-		self.poller:remove(self.worker)
-		self.worker:close()
-	end
-	self.worker = assert(self.context:socket(zmq.XREQ))
-	assert(self.worker:setopt(zmq.LINGER, 0))
-	assert(self.worker:connect(self.broker))
-	if self.verbose then
-		s_console("I: connecting to broker at %s...", self.broker)
-	end
-	-- Register service with broker
-	s_mdwrk_send_to_broker(self, mdp.MDPW_READY, self.service)
-	-- If liveness hits zero, queue is considered disconnected
-	self.liveness = HEARTBEAT_LIVENESS
-	self.heartbeat_at = s_clock() + self.heartbeat
-	-- add socket to poller
-	self.poller:add(self.worker, zmq.POLLIN, function()
-		self.got_msg = true
-	end)
+    -- close old socket.
+    if self.worker then
+        self.poller:remove(self.worker)
+        self.worker:close()
+    end
+    self.worker = assert(self.context:socket(zmq.XREQ))
+    assert(self.worker:setopt(zmq.LINGER, 0))
+    assert(self.worker:connect(self.broker))
+    if self.verbose then
+        s_console("I: connecting to broker at %s...", self.broker)
+    end
+    -- Register service with broker
+    s_mdwrk_send_to_broker(self, mdp.MDPW_READY, self.service)
+    -- If liveness hits zero, queue is considered disconnected
+    self.liveness = HEARTBEAT_LIVENESS
+    self.heartbeat_at = s_clock() + self.heartbeat
+    -- add socket to poller
+    self.poller:add(self.worker, zmq.POLLIN, function()
+        self.got_msg = true
+    end)
 end
 
 --
 -- Send reply, if any, to broker and wait for next request.
 --
 function obj_mt:recv(reply)
-	-- Format and send the reply if we are provided one
-	if reply then
-		assert(self.reply_to)
-		reply:wrap(self.reply_to, "")
-		self.reply_to = nil
-		s_mdwrk_send_to_broker(self, mdp.MDPW_REPLY, nil, reply)
-	end
-	self.expect_reply = true
+    -- Format and send the reply if we are provided one
+    if reply then
+        assert(self.reply_to)
+        reply:wrap(self.reply_to, "")
+        self.reply_to = nil
+        s_mdwrk_send_to_broker(self, mdp.MDPW_REPLY, nil, reply)
+    end
+    self.expect_reply = true
 
-	self.got_msg = false
-	while true do
-		local cnt = assert(self.poller:poll(self.heartbeat * 1000))
-		if cnt ~= 0 and self.got_msg then
-			self.got_msg = false
-			local msg = zmsg.recv(self.worker)
-			if self.verbose then
-				s_console("I: received message from broker:")
-				msg:dump()
-			end
-			self.liveness = HEARTBEAT_LIVENESS
-			-- Don't try to handle errors, just assert noisily
-			assert(msg:parts() >= 3)
+    self.got_msg = false
+    while true do
+        local cnt = assert(self.poller:poll(self.heartbeat * 1000))
+        if cnt ~= 0 and self.got_msg then
+            self.got_msg = false
+            local msg = zmsg.recv(self.worker)
+            if self.verbose then
+                s_console("I: received message from broker:")
+                msg:dump()
+            end
+            self.liveness = HEARTBEAT_LIVENESS
+            -- Don't try to handle errors, just assert noisily
+            assert(msg:parts() >= 3)
 
-			local empty = msg:pop()
-			assert(empty == "")
+            local empty = msg:pop()
+            assert(empty == "")
 
-			local header = msg:pop()
-			assert(header == mdp.MDPW_WORKER)
+            local header = msg:pop()
+            assert(header == mdp.MDPW_WORKER)
 
-			local command = msg:pop()
-			if command == mdp.MDPW_REQUEST then
-				-- We should pop and save as many addresses as there are
-				-- up to a null part, but for now, just save one...
-				self.reply_to = msg:unwrap()
-				return msg -- We have a request to process
-			elseif command == mdp.MDPW_HEARTBEAT then
-				-- Do nothing for heartbeats
-			elseif command == mdp.MDPW_DISCONNECT then
-				-- dis-connect and re-connect to broker.
-				s_mdwrk_connect_to_broker(self)
-			else
-				s_console("E: invalid input message (%d)", command:byte(1,1))
-				msg:dump()
-			end
-		else
-			self.liveness = self.liveness - 1
-			if (self.liveness == 0) then
-				if self.verbose then
-					s_console("W: disconnected from broker - retrying...")
-				end
-				-- sleep then Reconnect
-				s_sleep(self.reconnect)
-				s_mdwrk_connect_to_broker(self)
-			end
+            local command = msg:pop()
+            if command == mdp.MDPW_REQUEST then
+                -- We should pop and save as many addresses as there are
+                -- up to a null part, but for now, just save one...
+                self.reply_to = msg:unwrap()
+                return msg -- We have a request to process
+            elseif command == mdp.MDPW_HEARTBEAT then
+                -- Do nothing for heartbeats
+            elseif command == mdp.MDPW_DISCONNECT then
+                -- dis-connect and re-connect to broker.
+                s_mdwrk_connect_to_broker(self)
+            else
+                s_console("E: invalid input message (%d)", command:byte(1,1))
+                msg:dump()
+            end
+        else
+            self.liveness = self.liveness - 1
+            if (self.liveness == 0) then
+                if self.verbose then
+                    s_console("W: disconnected from broker - retrying...")
+                end
+                -- sleep then Reconnect
+                s_sleep(self.reconnect)
+                s_mdwrk_connect_to_broker(self)
+            end
 
-			-- Send HEARTBEAT if it's time
-			if (s_clock() > self.heartbeat_at) then
-				s_mdwrk_send_to_broker(self, mdp.MDPW_HEARTBEAT)
-				self.heartbeat_at = s_clock() + self.heartbeat
-			end
-		end
-	end
+            -- Send HEARTBEAT if it's time
+            if (s_clock() > self.heartbeat_at) then
+                s_mdwrk_send_to_broker(self, mdp.MDPW_HEARTBEAT)
+                self.heartbeat_at = s_clock() + self.heartbeat
+            end
+        end
+    end
 end
 
 module(...)
 
 function new(broker, service, verbose)
-	s_version_assert(2, 1);
-	local self = setmetatable({
-		context = zmq.init(1),
-		poller = zpoller.new(1),
-		broker = broker,
-		service = service,
-		verbose = verbose,
-		heartbeat = 2500, -- msecs
-		reconnect = 2500, -- msecs
-	}, obj_mt)
+    s_version_assert(2, 1);
+    local self = setmetatable({
+        context = zmq.init(1),
+        poller = zpoller.new(1),
+        broker = broker,
+        service = service,
+        verbose = verbose,
+        heartbeat = 2500, -- msecs
+        reconnect = 2500, -- msecs
+    }, obj_mt)
 
-	s_mdwrk_connect_to_broker(self)
-	return self
+    s_mdwrk_connect_to_broker(self)
+    return self
 end
 
 setmetatable(_M, { __call = function(self, ...) return new(...) end })
