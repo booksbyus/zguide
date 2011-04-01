d1e87bccf285577ebd0e28da35acdafd2be7ba4a
Pieter Hintjens <ph@imatix.com>
2011-04-01 16:48:04 +0200

    Removed useless assertion

--- examples/C/mdcliapi2.c
+++ examples/C/mdcliapi2.c
@@ -11,17 +11,17 @@
     This file is part of the ZeroMQ Guide: http://zguide.zeromq.org
 
     This is free software; you can redistribute it and/or modify it under
-    the terms of the GNU Lesser General Public License as published by 
-    the Free Software Foundation; either version 3 of the License, or (at 
+    the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or (at
     your option) any later version.
 
     This software is distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of 
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
     Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public 
-    License along with this program. If not, see 
+    You should have received a copy of the GNU Lesser General Public
+    License along with this program. If not, see
     <http://www.gnu.org/licenses/>.
     =====================================================================
 */
@@ -166,7 +166,6 @@ mdcli_recv (mdcli_t *self)
         free (header);
 
         char *service = zmsg_pop (msg);
-        assert (streq (service, service));
         free (service);
 
         return msg;     //  Success
