a79116ceb3f59cf2469a229b1dc055aa4095363e
Pieter Hintjens <ph@imatix.com>
2011-04-02 14:16:15 +0200

    Removed s_version from zhelpers.h

--- examples/C/zversion.c
+++ examples/C/zversion.c
@@ -5,6 +5,9 @@
 
 int main (void)
 {
-    s_version ();
+    int major, minor, patch;
+    zmq_version (&major, &minor, &patch);
+    printf ("Current 0MQ version is %d.%d.%d\n", major, minor, patch);
+
     return EXIT_SUCCESS;
 }
