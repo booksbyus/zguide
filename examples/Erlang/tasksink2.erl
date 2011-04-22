2e86363e94c6fe1de28c6136acfa7c8fa1860159
Mikhail Kulemin <mihkulemin@gmail.com>
2011-04-23 00:29:35 +0400

    Add tasksink2/taskwork2 with active sockets - polling like

---
+++ examples/Erlang/tasksink2.erl
@@ -0,0 +1,38 @@
+%% Task sink
+
+-module(tasksink2).
+-export([run/0]).
+
+run() ->
+	{ok,Context} = erlzmq:context(),
+	{ok,Socket} = erlzmq:socket(Context,pull),
+	ok = erlzmq:bind(Socket,"tcp://*:5558"),
+
+	{ok,Controller} = erlzmq:socket(Context,pub),
+	ok = erlzmq:bind(Controller,"tcp://*:5559"),
+
+	{ok,_} = erlzmq:recv(Socket),
+	Start = now(),
+	
+	loop(Socket,0,Start,Controller),
+	
+	ok = erlzmq:close(Controller),	
+	ok = erlzmq:close(Socket),
+	ok = erlzmq:term(Context).
+
+loop(_,100,Start,Controller) ->
+	io:format("~nTotal elapsed time: ~B msec~n",[timer:now_diff(now(),Start) div 1000]),
+	ok = erlzmq:send(Controller,<<"KILL">>);
+loop(Socket,N,Start,Controller) ->
+	{ok,_} = erlzmq:recv(Socket),
+	
+	Symb = case (N+1) rem 10 of
+		0 -> ":";
+		_ -> "."
+	end,
+	io:format("~s",[Symb]),
+	loop(Socket,N+1,Start,Controller).
+
+
+
+
---
+++ examples/Erlang/taskwork2.erl
@@ -0,0 +1,42 @@
+% Task worker
+
+-module(taskwork2).
+-export([run/0]).
+
+run() ->
+	{ok,Context} = erlzmq:context(),
+	
+	{ok,Receiver} = erlzmq:socket(Context,[pull,{active,true}]),
+	ok = erlzmq:connect(Receiver,"tcp://localhost:5557"),
+
+	{ok,Sender} = erlzmq:socket(Context,push),
+	ok = erlzmq:connect(Sender,"tcp://localhost:5558"),
+
+	{ok,Controller} = erlzmq:socket(Context,[sub,{active,true}]),
+	ok = erlzmq:connect(Controller,"tcp://localhost:5559"),
+	ok = erlzmq:setsockopt(Controller,subscribe,""),
+
+
+
+	ok = loop(Receiver,Sender,Controller),
+
+	ok = erlzmq:close(Receiver),
+	ok = erlzmq:close(Sender),
+	ok = erlzmq:close(Controller),
+	ok = erlzmq:term(Context).
+
+loop(Receiver,Sender,Controller) ->
+	receive 
+		{zmq,Receiver,Work} ->
+			io:format(".",[]),
+
+			Time = list_to_integer(binary_to_list(Work)),
+			timer:sleep(Time),
+
+			ok = erlzmq:send(Sender,<<"">>),
+			loop(Receiver,Sender,Controller);
+		{zmq,Controller,<<"KILL">>} ->
+			ok
+	end.	
+	
+
