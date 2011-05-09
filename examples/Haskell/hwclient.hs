c32f628687a0128cd15e516e50332a258d02fe87
Dr. ERDI Gergo <gergo@erdi.hu>
2011-05-09 22:28:32 +0200

    Haskell transliteration of Hello World server/client

---
+++ examples/Haskell/hwclient.hs
@@ -0,0 +1,25 @@
+-- |
+-- Hello World client in Haskell
+-- Binds REP socket to tcp://*:5555
+-- Sends "Hello" to server, expects "World" back
+-- 
+-- Transliterated to Haskell by Gergo Erdi <gergo@erdi.hu>
+
+module Main where
+
+import System.ZMQ
+import Control.Monad (forM_)
+import Data.ByteString.Char8 (pack, unpack)
+
+main = withContext 1 $ \context -> do  
+  putStrLn "Connecting to Hello World server..."  
+  withSocket context Req $ \socket -> do
+    connect socket "tcp://localhost:5555"
+    forM_ [1..10] $ \i -> do
+      putStrLn $ unwords ["Sending request", show i]
+      send socket request []
+    
+      reply <- receive socket []
+      putStrLn $ unwords ["Received reply:", unpack reply]    
+
+  where request = pack "Hello"
---
+++ examples/Haskell/hwserver.hs
@@ -0,0 +1,28 @@
+-- |
+-- Hello World server in Haskell
+-- Binds REP socket to tcp://*:5555
+-- Expects "Hello" from client, replies with "World"
+-- 
+-- Transliterated to Haskell by Gergo Erdi <gergo@erdi.hu>
+
+module Main where
+
+import System.ZMQ
+import Control.Monad (forever)
+import Data.ByteString.Char8 (pack, unpack)
+import System.Posix (sleep)
+
+main = withContext 1 $ \context -> do  
+  putStrLn "Starting Hello World server"
+  withSocket context Rep $ \socket -> do
+    bind socket "tcp://*:5555"
+  
+    putStrLn "Entering main loop"
+    forever $ do
+      message <- receive socket []
+      putStrLn $ unwords ["Received request:", unpack message]    
+    
+      -- Simulate doing some 'work'
+      sleep 5
+
+      send socket (pack "World") []
