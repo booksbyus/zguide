b5009001d6d716eb4ca3626d74d7b11c559c199b
rick <technoweenie@gmail.com>
2011-05-22 07:13:16 -0400

    add task example for node.js

---
+++ examples/Node.js/tasksink.js
@@ -0,0 +1,30 @@
+// Task sink in node.js
+// Binds PULL socket to tcp://localhost:5558
+// Collects results from workers via that socket.
+
+var zmq = require('zeromq')
+  , receiver = zmq.createSocket('pull')
+
+var started = false
+  , i = 0
+  , label = "Total elapsed time"
+
+receiver.on('message', function() {
+  // wait for start of batch
+  if(!started) {
+    console.time(label)
+    started = true
+
+  // process 100 confirmations
+  } else {
+    i += 1
+    process.stdout.write(i % 10 == 0 ? ':' : '.')
+    if(i == 100) {
+      console.timeEnd(label)
+      receiver.close()
+      process.exit()
+    }
+  }
+})
+
+receiver.bindSync("tcp://*:5558")
---
+++ examples/Node.js/taskwork.js
@@ -0,0 +1,32 @@
+// Task worker in node.js
+// Connects PULL socket to tcp://localhost:5557
+// Collects workloads from ventilator via that socket
+// Connects PUSH socket to tcp://localhost:5558
+// Sends results to sink via that socket
+
+var zmq = require('zeromq')
+  , receiver = zmq.createSocket('pull')
+  , sender   = zmq.createSocket('push')
+
+receiver.on('message', function(buf) {
+  var msec = parseInt(buf.toString())
+
+  // simple progress indicator for the viewer
+  process.stdout.write(buf.toString() + ".")
+
+  // do the work
+  // not a great node sample for zeromq,
+  // node receives messages while timers run.
+  setTimeout(function() {
+    sender.send("")
+  }, msec)
+})
+
+receiver.connect('tcp://localhost:5557')
+sender.connect('tcp://localhost:5558')
+
+process.on('SIGINT', function() {
+  receiver.close()
+  sender.close()
+  process.exit()
+})
