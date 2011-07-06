bf4d941a9f5744a9c92cc50de22fe8b176b65df9
Pieter Hintjens <ph@imatix.com>
2011-07-06 20:31:50 +0200

    Removed router-to-router examples from Chapter 3

--- listings/C/listing_25.c
+++ listings/C/listing_25.c
@@ -1,4 +1,4 @@
-    zloop_t *reactor = zloop_new ();
-    zloop_reader (reactor, self->backend, s_handle_backend, self);
-    zloop_start (reactor);
-    zloop_destroy (&reactor);
+zloop_t *reactor = zloop_new ();
+zloop_reader (reactor, self->backend, s_handle_backend, self);
+zloop_start (reactor);
+zloop_destroy (&reactor);
