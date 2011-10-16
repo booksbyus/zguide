cf91a44ffc8ab75a11ecc4608f1aecea56bbb420
Isaiah <issaria@gmail.com>
2011-10-16 12:02:15 +0800

    added leiningen project.clj to run examples with lein run -m example name: command not found

---
+++ examples/Clojure/project.clj
@@ -0,0 +1,7 @@
+(defproject examples "1.0.0-SNAPSHOT"
+  :source-path "../Clojure/"
+  :description "0MQ examples in Clojure"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [backtype/jzmq "2.1.0"]
+                 ])
