2c4b9e6243635843e65b325a7c60a5059330fb9f
Tomas Roos <ptomasroos@gmail.com>
2012-02-10 11:54:26 +0100

    verified and refactored mtserver.cs

--- examples/C#/mtserver.cs
+++ examples/C#/mtserver.cs
@@ -2,49 +2,52 @@
 //  Multithreaded Hello World server
 //
 
-//  Author:     Michael Compton
-//  Email:      michael.compton@littleedge.co.uk
+//  Author:     Michael Compton, Tomas Roos
+//  Email:      michael.compton@littleedge.co.uk, ptomasroos@gmail.com
 
-using System;
 using System.Text;
 using System.Threading;
 using ZMQ;
 
-namespace ZMQGuide {
-    class Program {
-
-        static void WorkerRoutine(object context) {
-            //  Socket to talk to dispatcher
-            Socket receiver = ((Context)context).Socket(SocketType.REP);
-            receiver.Connect("inproc://workers");
-            while (true) {
-                string message = receiver.Recv(Encoding.Unicode);
-                //  Do some 'work'
-                Thread.Sleep(1000);
-                //  Send reply back to client
-                receiver.Send("World", Encoding.Unicode);
-            }
-        }
-
-        static void Main(string[] args) {
-            using (Context context = new Context(1)) {
-                using (Socket clients = context.Socket(SocketType.ROUTER),
-                workers = context.Socket(SocketType.DEALER)) {
-                    //  Socket to talk to clients
+namespace ZMQGuide
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new Context(1))
+            {
+                using (Socket clients = context.Socket(SocketType.ROUTER), workers = context.Socket(SocketType.DEALER))
+                {
                     clients.Bind("tcp://*:5555");
-                    //  Socket to talk to workers
                     workers.Bind("inproc://workers");
-                    
-                    //  Launch pool of worker threads
-                    Thread[] workerThreads = new Thread[5];
-                    for (int count = 0; count < workerThreads.Length; count++) {
-                        workerThreads[count] = new Thread(WorkerRoutine);
-                        workerThreads[count].Start(context);
+
+                    var workerThreads = new Thread[5];
+                    for (int threadId = 0; threadId < workerThreads.Length; threadId++)
+                    {
+                        workerThreads[threadId] = new Thread(WorkerRoutine);
+                        workerThreads[threadId].Start(context);
                     }
 
                     //  Connect work threads to client threads via a queue
+                    //  Devices will be depricated from 3.x
                     Socket.Device.Queue(clients, workers);
                 }
+            }
+        }
+
+        private static void WorkerRoutine(object context)
+        {
+            Socket receiver = ((Context)context).Socket(SocketType.REP);
+            receiver.Connect("inproc://workers");
+
+            while (true)
+            {
+                string message = receiver.Recv(Encoding.Unicode);
+
+                Thread.Sleep(1000); //  Simulate 'work'
+                
+                receiver.Send("World", Encoding.Unicode);
             }
         }
     }
