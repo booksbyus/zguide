1f29de39974bdc720142ca302067f2a9632c2d2a
Tomas Roos <ptomasroos@gmail.com>
2012-02-10 13:41:40 +0100

    verified and refactored durapub2

--- examples/C#/durapub2.cs
+++ examples/C#/durapub2.cs
@@ -10,35 +10,33 @@ using System.Text;
 using System.Threading;
 using ZMQ;
 
-namespace ZMQGuide {
-    class Program {
-        static void Main(string[] args) {
-            using (Context context = new Context(1)) {
-                using (Socket sync = context.Socket(SocketType.PULL),
-                    publisher = context.Socket(SocketType.PUB)) {
-
-                    //  Subscriber tells us when it's ready here
+namespace ZMQGuide
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new Context(1))
+            {
+                using (Socket sync = context.Socket(SocketType.PULL), publisher = context.Socket(SocketType.PUB))
+                {
                     sync.Bind("tcp://*:5564");
-                    
-                    //  Prevent publisher overflow from slow subscribers
-                    publisher.HWM = 1;
 
+                    publisher.HWM = 1;
                     //  Specify swap space in bytes, this covers all subscribers
                     publisher.Swap = 25000000;
-                    
-                    //  We send updates via this socket
                     publisher.Bind("tcp://*:5565");
 
                     //  Wait for synchronization request
                     sync.Recv();
-                    
-                    //  Now broadcast exactly 10 updates with pause
-                    for (int updateNbr = 0; updateNbr < 10; updateNbr++) {
-                        publisher.Send("Update " + updateNbr, Encoding.Unicode);
+
+                    for (int updateNumber = 0; updateNumber < 10; updateNumber++)
+                    {
+                        publisher.Send("Update " + updateNumber, Encoding.Unicode);
                         Thread.Sleep(1000);
                     }
+
                     publisher.Send("END", Encoding.Unicode);
-                    Thread.Sleep(1000);   //  Give 0MQ/2.0.x time to flush output
                 }
             }
         }
