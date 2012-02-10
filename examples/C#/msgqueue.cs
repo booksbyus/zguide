d4851f9f7884f574be41f7a7798bb267110a808f
Tomas Roos <ptomasroos@gmail.com>
2012-02-10 11:41:07 +0100

    clarified msgqueue and that it replaces rrbroker.cs and is depricated, soon to be removed

--- examples/C#/msgqueue.cs
+++ examples/C#/msgqueue.cs
@@ -3,25 +3,26 @@
 //  Same as request-reply broker but using QUEUE device
 //
 
-//  Author:     Michael Compton
-//  Email:      michael.compton@littleedge.co.uk
+//  Author:     Michael Compton, Tomas Roos
+//  Email:      michael.compton@littleedge.co.uk, ptomasroos@gmail.com
 
-using System;
-using System.Text;
 using ZMQ;
 
-namespace ZMQGuide {
-    class Program {
-        static void Main(string[] args) {
-            using (Context context = new Context(1)) {
-                using (Socket frontend = context.Socket(SocketType.ROUTER),
-                backend = context.Socket(SocketType.DEALER)) {
-                    //  Socket facing clients
+namespace ZMQGuide
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new Context(1))
+            {
+                using (Socket frontend = context.Socket(SocketType.ROUTER), backend = context.Socket(SocketType.DEALER))
+                {
                     frontend.Bind("tcp://*:5559");
-                    //  Socket facing services
                     backend.Bind("tcp://*:5560");
 
-                    //  Start built-in device
+                    //  Note that the Devices will be removed in the 3.x release.
+                    //  This replaces the example in rrbroker.cs
                     Socket.Device.Queue(frontend, backend);
                 }
             }
