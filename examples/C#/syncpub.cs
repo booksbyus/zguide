e1b40cf24bd50e5b6478c06dff733569b566716c
Tomas Roos <ptomasroos@gmail.com>
2012-02-10 12:26:46 +0100

    node coordination

--- examples/C#/syncpub.cs
+++ examples/C#/syncpub.cs
@@ -2,38 +2,43 @@
 //  Synchronized publisher
 //
 
-//  Author:     Michael Compton
-//  Email:      michael.compton@littleedge.co.uk
+//  Author:     Michael Compton, Tomas Roos
+//  Email:      michael.compton@littleedge.co.uk, ptomasroos@gmail.com
 
 using System;
 using System.Text;
 using System.Threading;
 using ZMQ;
 
-namespace ZMQGuide {
-    class Program {
-        const int NUM_OF_SUBSCRIBERS = 10;
-
-        static void Main(string[] args) {
-            using (Context context = new Context(1)) {
-                using (Socket publisher = context.Socket(SocketType.PUB),
-                    syncService = context.Socket(SocketType.REP)) {
-                    //  Socket to talk to clients
+namespace ZMQGuide
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new Context(1))
+            {
+                using (Socket publisher = context.Socket(SocketType.PUB), syncService = context.Socket(SocketType.REP))
+                {
                     publisher.Bind("tcp://*:5561");
-                    //  Socket to receive signals
                     syncService.Bind("tcp://*:5562");
 
                     //  Get synchronization from subscribers
-                    for (int count = 0; count < NUM_OF_SUBSCRIBERS; count++) {
+                    const int subscribersToWaitFor = 10;
+                    for (int count = 0; count < subscribersToWaitFor; count++)
+                    {
                         syncService.Recv();
                         syncService.Send("", Encoding.Unicode);
                     }
+
                     //  Now broadcast exactly 1M updates followed by END
-                    for (int nbrOfUpdates = 0; nbrOfUpdates < 1000000; nbrOfUpdates++) {
+                    const int updatesToSend = 1000000;
+                    for (int updateId = 0; updateId < updatesToSend; updateId++)
+                    {
                         publisher.Send("Rhubard", Encoding.Unicode);
                     }
+
                     publisher.Send("END", Encoding.Unicode);
-                    Thread.Sleep(1000);  //  Give 0MQ/2.0.x time to flush output
                 }
             }
         }
