7419b0722f73cfaf8981291a65953110ad217257
Tomas Roos <ptomasroos@gmail.com>
2012-02-10 10:20:32 +0100

    fixed mspoller, reader, tasksink2 taskwork2 and added interrupt.cs example

--- examples/C#/msreader.cs
+++ examples/C#/msreader.cs
@@ -3,47 +3,61 @@
 //  This version uses a simple recv loop
 //
 
-//  Author:     Michael Compton
-//  Email:      michael.compton@littleedge.co.uk
+//  Author:     Michael Compton, Tomas Roos
+//  Email:      michael.compton@littleedge.co.uk, ptomasroos@gmail.com
 
 using System;
 using System.Text;
 using System.Threading;
 using ZMQ;
 
-namespace ZMQGuide {
-    class Program {
-        static void Main(string[] args) {
-            //  Prepare our context and socket
-            using (Context context = new Context(1)) {
+namespace ZMQGuide
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new Context(1))
+            {
                 //  Connect to task ventilator and weather server
-                using (Socket receiver = context.Socket(SocketType.PULL),
-                    subscriber = context.Socket(SocketType.SUB)) {
+                using (Socket receiver = context.Socket(SocketType.PULL), subscriber = context.Socket(SocketType.SUB))
+                {
                     receiver.Connect("tcp://localhost:5557");
                     subscriber.Connect("tcp://localhost:5556");
                     subscriber.Subscribe("10001 ", Encoding.Unicode);
 
                     //  Process messages from both sockets
                     //  We prioritize traffic from the task ventilator
-                    while (true) {
+                    while (true)
+                    {
                         //  Process any waiting tasks
-                        while (true) {
-                            byte[] msg = receiver.Recv(SendRecvOpt.NOBLOCK);
-                            if (msg != null) {
+                        while (true)
+                        {
+                            byte[] message = receiver.Recv(SendRecvOpt.NOBLOCK);
+                            if (message != null)
+                            {
                                 Console.WriteLine("Process Task");
-                            } else {
+                            }
+                            else
+                            {
                                 break;
                             }
                         }
+
                         //  Process any waiting weather updates
-                        while (true) {
-                            byte[] msg = subscriber.Recv(SendRecvOpt.NOBLOCK);
-                            if (msg != null) {
+                        while (true)
+                        {
+                            byte[] message = subscriber.Recv(SendRecvOpt.NOBLOCK);
+                            if (message != null)
+                            {
                                 Console.WriteLine("Process Weather");
-                            } else {
+                            }
+                            else
+                            {
                                 break;
                             }
                         }
+
                         Thread.Sleep(1000);
                     }
                 }
