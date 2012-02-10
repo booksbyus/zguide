e59fb46bc920d84460fbd3c23e022a2cffa00826
Tomas Roos <ptomasroos@gmail.com>
2012-02-10 12:42:33 +0100

    pub-sub filtering using multipart message

--- examples/C#/psenvpub.cs
+++ examples/C#/psenvpub.cs
@@ -2,28 +2,34 @@
 //  Pubsub envelope publisher
 //
 
-//  Author:     Michael Compton
-//  Email:      michael.compton@littleedge.co.uk
+//  Author:     Michael Compton, Tomas Roos
+//  Email:      michael.compton@littleedge.co.uk, ptomasroos@gmail.com
 
 using System;
 using System.Text;
 using System.Threading;
 using ZMQ;
 
-namespace ZMQGuide {
-    class Program {
-        static void Main(string[] args) {
-            //  Prepare our context and publisher
-            using (Context context = new Context(1)) {
-                using (Socket publisher = context.Socket(SocketType.PUB)) {
+namespace ZMQGuide
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new Context(1))
+            {
+                using (Socket publisher = context.Socket(SocketType.PUB))
+                {
                     publisher.Bind("tcp://*:5563");
-                    while (true) {
+
+                    while (true)
+                    {
                         //  Write two messages, each with an envelope and content
                         publisher.SendMore("A", Encoding.Unicode);
                         publisher.Send("We don't want to see this.", Encoding.Unicode);
                         publisher.SendMore("B", Encoding.Unicode);
                         publisher.Send("We would like to see this.", Encoding.Unicode);
-                        Thread.Sleep(1000);
+                        Thread.Sleep(1000); // avoid flooding the publisher
                     }
                 }
             }
