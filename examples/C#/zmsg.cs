f7f7baf0546f4bead87ee7f02cbf601ba62bbefa
Tomas Roos <ptomasroos@gmail.com>
2012-02-15 14:26:57 +0100

    refactored

--- examples/C#/zmsg.cs
+++ examples/C#/zmsg.cs
@@ -1,102 +1,126 @@
-﻿//Multipart message class for example applications.
+﻿//
+//  Multipart message class for example applications.
 
 //  Author:     Michael Compton
 //  Email:      michael.compton@littleedge.co.uk
 
-using System;
 using System.Collections.Generic;
 using System.Text;
 using ZMQ;
 
-namespace ZMsg {
+namespace ZMQ
+{
+    public class ZMessage
+    {
+        readonly Encoding encoding = Encoding.Unicode;
+        readonly List<byte[]> frames = new List<byte[]>();
 
-    public class ZMessage {
-        Encoding _encoding = Encoding.Unicode;
-        List<byte[]> _msgParts = new List<byte[]>();
-
-        public ZMessage() { }
-
-        public ZMessage(Socket skt) {
-            Recv(skt);
+        public ZMessage()
+        {
         }
 
-        public ZMessage(string body, Encoding encoding) {
-            _encoding = encoding;
-            _msgParts.Add(_encoding.GetBytes(body));
+        public ZMessage(Socket socket)
+        {
+            Recv(socket);
         }
 
-        public ZMessage(string body) {
-            _msgParts.Add(_encoding.GetBytes(body));
+        public ZMessage(string body, Encoding encoding)
+        {
+            frames.Add(encoding.GetBytes(body));
+            this.encoding = encoding;
         }
 
-        public ZMessage(byte[] body) {
-            _msgParts.Add(body);
+        public ZMessage(string body)
+        {
+            frames.Add(encoding.GetBytes(body));
         }
 
-        public void Recv(Socket socket) {
-            Queue<byte[]> msgParts = socket.RecvAll();
+        public ZMessage(byte[] body)
+        {
+            frames.Add(body);
+        }
 
-            while (msgParts.Count > 0) {
-                _msgParts.Insert(0, (msgParts.Dequeue()));
+        public void Recv(Socket socket)
+        {
+            Queue<byte[]> receivedFrames = socket.RecvAll();
+
+            while (receivedFrames.Count > 0)
+            {
+                frames.Insert(0, (receivedFrames.Dequeue()));
             }
         }
 
-        public void Send(Socket socket) {
-            for (int index = _msgParts.Count - 1; index > 0; index--) {
-                socket.SendMore(_msgParts[index]);
+        public void Send(Socket socket)
+        {
+            for (int index = frames.Count - 1; index > 0; index--)
+            {
+                socket.SendMore(frames[index]);
             }
-            socket.Send(_msgParts[0]);
+
+            socket.Send(frames[0]);
         }
 
-        public string BodyToString() {
-            return _encoding.GetString(Body);
+        public string BodyToString()
+        {
+            return encoding.GetString(Body);
         }
 
-        public void StringToBody(string body) {
-            Body = _encoding.GetBytes(body);
+        public void StringToBody(string body)
+        {
+            Body = encoding.GetBytes(body);
         }
 
-        public void Append(byte[] data) {
-            _msgParts.Insert(0, data);
+        public void Append(byte[] data)
+        {
+            frames.Insert(0, data);
         }
 
-        public byte[] Pop() {
-            byte[] data = _msgParts[_msgParts.Count - 1];
-            _msgParts.RemoveAt(_msgParts.Count - 1);
+        public byte[] Pop()
+        {
+            byte[] data = frames[frames.Count - 1];
+            frames.RemoveAt(frames.Count - 1);
             return data;
         }
 
-        public void Push(byte[] data) {
-            _msgParts.Add(data);
+        public void Push(byte[] data)
+        {
+            frames.Add(data);
         }
 
-        public void Wrap(byte[] address, byte[] delim) {
-            if (delim != null) {
-                _msgParts.Add(delim);
+        public void Wrap(byte[] address, byte[] delim)
+        {
+            if (delim != null)
+            {
+                frames.Add(delim);
             }
-            _msgParts.Add(address);
+            frames.Add(address);
         }
 
-        public byte[] Unwrap() {
+        public byte[] Unwrap()
+        {
             byte[] addr = Pop();
-            if (Address.Length == 0) {
+            if (Address.Length == 0)
+            {
                 Pop();
             }
             return addr;
         }
 
-        public int PartCount {
-            get { return _msgParts.Count; }
+        public int FrameCount
+        {
+            get { return frames.Count; }
         }
 
-        public byte[] Address {
-            get { return _msgParts[_msgParts.Count - 1]; }
-            set { _msgParts.Add(value); }
+        public byte[] Address
+        {
+            get { return frames[frames.Count - 1]; }
+            set { frames.Add(value); }
         }
 
-        public byte[] Body {
-            get { return _msgParts[0]; }
-            set { _msgParts[0] = value; }
+        public byte[] Body
+        {
+            get { return frames[0]; }
+            set { frames[0] = value; }
         }
     }
 }
