d2439f27fcb814ac15f5ff87b4da38a4c4e6d544
Martin Trojer <martin.trojer@trayport.com>
2012-04-26 10:27:00 +0100

    correct version of jzmq in README/project.clj

--- examples/Clojure/project.clj
+++ examples/Clojure/project.clj
@@ -2,5 +2,5 @@
   :source-path "../Clojure/"
   :description "0MQ zguide in Clojure"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.zmq/zmq "1.0.0"]
+                 [org.zmq/zmq "2.1.0"]
                  ])
