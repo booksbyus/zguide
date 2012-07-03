1809c6eb496c1f87107ac79871eb9556e91dc96f
Iskren Chernev <iskren.chernev@gmail.com>
2012-07-03 23:02:05 +0300

    Fixed code style in node.js examples for chapter 1

--- examples/Node.js/wuclient.js
+++ examples/Node.js/wuclient.js
@@ -2,39 +2,45 @@
 // connects SUB socket to tcp://localhost:5556
 // collects weather updates and finds avg temp in zipcode
 
-var zmq = require('zmq')
+var zmq = require('zmq');
 
-console.log("Collecting updates from weather server...")
+console.log("Collecting updates from weather server...");
 
 // Socket to talk to server
-var subscriber = zmq.socket('sub')
+var subscriber = zmq.socket('sub');
 
 // Subscribe to zipcode, default is NYC, 10001
-var filter = null
-if(process.argv.length > 2) {
-  filter = process.argv[2]
+var filter = null;
+if (process.argv.length > 2) {
+  filter = process.argv[2];
 } else {
-  filter = "10001"
+  filter = "10001";
 }
-subscriber.subscribe(filter)
+console.log(filter);
+subscriber.subscribe(filter);
 
 // process 100 updates
 var total_temp = 0
-var temps      = 0
+  , temps      = 0;
 subscriber.on('message', function(data) {
-  var pieces = data.toString().split(" ")
-  var zipcode = parseInt(pieces[0])
-  var temperature = parseInt(pieces[1])
-  var relhumidity = parseInt(pieces[2])
-  temps += 1
-  total_temp += temperature
+  var pieces      = data.toString().split(" ")
+    , zipcode     = parseInt(pieces[0], 10)
+    , temperature = parseInt(pieces[1], 10)
+    , relhumidity = parseInt(pieces[2], 10);
 
-  if(temps == 100) {
-    console.log("Average temperature for zipcode '", filter, "' was", total_temp/100, "F")
-    total_temp = 0
-    temps = 0
+  temps += 1;
+  total_temp += temperature;
+
+  if (temps === 100) {
+    console.log([
+      "Average temperature for zipcode '",
+      filter,
+      "' was ",
+      (total_temp / temps).toFixed(2),
+      " F"].join(""));
+    total_temp = 0;
+    temps = 0;
   }
-})
+});
 
-subscriber.connect("tcp://localhost:5556")
-
+subscriber.connect("tcp://localhost:5556");
