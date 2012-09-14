93866974797cdd999f6b5de88930c097599a7b00
Harm Aarts <harmaarts@gmail.com>
2012-09-14 18:45:15 +0200

    prints errors

--- examples/Go/identity.go
+++ examples/Go/identity.go
@@ -13,21 +13,23 @@ func main() {
 	context, _ := zmq.NewContext()
 	defer context.Close()
 
-	sink, _ := context.NewSocket(zmq.ROUTER)
+	sink, err := context.NewSocket(zmq.ROUTER)
+	if err != nil { print(err) }
 	defer sink.Close()
 	sink.Connect("inproc://example")
 
-	//  First allow 0MQ to set the identity
-	anonymous, _ := context.NewSocket(zmq.REQ)
+	// First allow 0MQ to set the identity
+	anonymous, err := context.NewSocket(zmq.REQ)
+	if err != nil { print(err) }
 	anonymous.Connect("inproc://example")
-	anonymous.Send("ROUTER uses a generated UUID")
-	anonymous.Dump()
+	go anonymous.Send([]byte("ROUTER uses a generated UUID"), 0)
+	sink.Recv(0)
 
 	//  Then set the identity ourself
-	identified, _ := context.NewSocket(zmq.REQ)
+	identified, err := context.NewSocket(zmq.REQ)
+	if err != nil { print(err) }
 	identified.SetSockOptString(zmq.IDENTITY, "Hello")
 	identified.Connect("inproc://example")
-	identified.Send("ROUTER socket uses REQ's socket identity")
-	anonymous.Dump()
-
+	go identified.Send([]byte("ROUTER socket uses REQ's socket identity"), 0)
+	sink.Recv(0)
 }
