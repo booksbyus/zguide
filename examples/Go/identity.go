6f3e521527bef3cbe3b93ae5c70468d5f786af21
Harm Aarts <harmaarts@gmail.com>
2012-09-14 19:42:41 +0200

    removes bug in which I called Connect iso Bind

--- examples/Go/identity.go
+++ examples/Go/identity.go
@@ -7,6 +7,7 @@ package main
 
 import (
 	zmq "github.com/alecthomas/gozmq"
+	"fmt"
 )
 
 func main() {
@@ -16,20 +17,29 @@ func main() {
 	sink, err := context.NewSocket(zmq.ROUTER)
 	if err != nil { print(err) }
 	defer sink.Close()
-	sink.Connect("inproc://example")
+	sink.Bind("inproc://example")
 
 	// First allow 0MQ to set the identity
 	anonymous, err := context.NewSocket(zmq.REQ)
-	if err != nil { print(err) }
+	defer anonymous.Close()
+	if err != nil { fmt.Println(err) }
 	anonymous.Connect("inproc://example")
-	go anonymous.Send([]byte("ROUTER uses a generated UUID"), 0)
-	sink.Recv(0)
+	err = anonymous.Send([]byte("ROUTER uses a generated UUID"), 0)
+	if err != nil { fmt.Println(err) }
+
+	for {
+	  msgdata, err := sink.Recv(0)
+	  if err != nil { fmt.Println(err) }
+	  fmt.Println(msgdata)
+	}
 
 	//  Then set the identity ourself
 	identified, err := context.NewSocket(zmq.REQ)
 	if err != nil { print(err) }
+	defer identified.Close()
 	identified.SetSockOptString(zmq.IDENTITY, "Hello")
 	identified.Connect("inproc://example")
-	go identified.Send([]byte("ROUTER socket uses REQ's socket identity"), 0)
+	identified.Send([]byte("ROUTER socket uses REQ's socket identity"), zmq.NOBLOCK)
+
 	sink.Recv(0)
 }
