61516f985d884119dc0641a328d5a40feac67e35
Pieter Hintjens <ph@imatix.com>
2012-09-16 18:27:30 +0900

    Proper codec generator for MDP

---
+++ examples/Chapter6/mdp_client.h
@@ -0,0 +1,72 @@
+/*  =========================================================================
+    mdp_client.h
+
+    Generated C99 codec header for mdp_client
+    =========================================================================
+*/
+
+#ifndef __MDP_CLIENT_H_INCLUDED__
+#define __MDP_CLIENT_H_INCLUDED__
+
+/*  These are the mdp_client messages
+
+    REQUEST - Client request to broker
+        service       string 
+        body          frame 
+
+    REPLY - Response back to client
+        service       string 
+        body          frame 
+*/
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//  Opaque class structure
+typedef struct _mdp_client_t mdp_client_t;
+
+//  Create a new mdp_client
+mdp_client_t *
+    mdp_client_new (void);
+
+//  Destroy the mdp_client
+void
+    mdp_client_destroy (mdp_client_t **self_p);
+
+//  Receive and parse a mdp_client from the socket
+mdp_client_t *
+    mdp_client_recv (void *socket);
+
+//  Send the mdp_client to the socket, and destroy it
+int
+    mdp_client_send (mdp_client_t **self_p, void *socket);
+
+//  Get/set the message address
+zframe_t *
+    mdp_client_address (mdp_client_t *self);
+void
+    mdp_client_address_set (mdp_client_t *self, zframe_t *address);
+
+//  Get/set the service field
+char *
+    mdp_client_service (mdp_client_t *self);
+void
+    mdp_client_service_set (mdp_client_t *self, char *format, ...);
+
+//  Get/set the body field
+zframe_t *
+    mdp_client_body (mdp_client_t *self);
+void
+    mdp_client_body_set (mdp_client_t *self, zframe_t *frame);
+
+//  Self test of this class
+int
+    mdp_client_test (bool verbose);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ examples/Chapter6/mdp_worker.h
@@ -0,0 +1,94 @@
+/*  =========================================================================
+    mdp_worker.h
+
+    Generated C99 codec header for mdp_worker
+    =========================================================================
+*/
+
+#ifndef __MDP_WORKER_H_INCLUDED__
+#define __MDP_WORKER_H_INCLUDED__
+
+/*  These are the mdp_worker messages
+
+    READY - Worker tells broker it is ready
+        service       string 
+
+    REQUEST - Client request to broker
+        client        frame 
+        body          frame 
+
+    REPLY - Worker returns reply to broker
+        client        frame 
+        body          frame 
+
+    HEARBEAT - Either peer tells the other it's still alive
+
+    DISCONNECT - Either peer tells other the party is over
+*/
+
+#define MDP_WORKER_READY                1
+#define MDP_WORKER_REQUEST              2
+#define MDP_WORKER_REPLY                3
+#define MDP_WORKER_HEARBEAT             4
+#define MDP_WORKER_DISCONNECT           5
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//  Opaque class structure
+typedef struct _mdp_worker_t mdp_worker_t;
+
+//  Create a new mdp_worker
+mdp_worker_t *
+    mdp_worker_new (int id);
+
+//  Destroy the mdp_worker
+void
+    mdp_worker_destroy (mdp_worker_t **self_p);
+
+//  Receive and parse a mdp_worker from the socket
+mdp_worker_t *
+    mdp_worker_recv (void *socket);
+
+//  Send the mdp_worker to the socket, and destroy it
+int
+    mdp_worker_send (mdp_worker_t **self_p, void *socket);
+
+//  Get/set the message address
+zframe_t *
+    mdp_worker_address (mdp_worker_t *self);
+void
+    mdp_worker_address_set (mdp_worker_t *self, zframe_t *address);
+
+//  Get the mdp_worker id
+int
+    mdp_worker_id (mdp_worker_t *self);
+    
+//  Get/set the service field
+char *
+    mdp_worker_service (mdp_worker_t *self);
+void
+    mdp_worker_service_set (mdp_worker_t *self, char *format, ...);
+
+//  Get/set the client field
+zframe_t *
+    mdp_worker_client (mdp_worker_t *self);
+void
+    mdp_worker_client_set (mdp_worker_t *self, zframe_t *frame);
+
+//  Get/set the body field
+zframe_t *
+    mdp_worker_body (mdp_worker_t *self);
+void
+    mdp_worker_body_set (mdp_worker_t *self, zframe_t *frame);
+
+//  Self test of this class
+int
+    mdp_worker_test (bool verbose);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
