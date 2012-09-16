30635fe428b603f4d9e779469325fa18d29812e0
Harm Aarts <harmaarts@gmail.com>
2012-09-16 20:52:37 +0200

    fixes part of dump function, wip

--- examples/Go/identity.go
+++ examples/Go/identity.go
@@ -13,9 +13,21 @@ import (
 func dump(sink zmq.Socket) {
 	parts, err := sink.RecvMultipart(0)
 	if err != nil { fmt.Println(err) }
-	for _, msgdata := range parts{
-	  fmt.Printf("%T\n", msgdata)
-	  fmt.Println(msgdata)
+	for _, msgdata := range parts {
+	  is_text := true
+	  for char := range msgdata {
+		if char < 17 || char > 127 {
+		  is_text = false
+		}
+	  }
+	  if is_text {
+		fmt.Printf("%U\n", msgdata)
+	  } else {
+		fmt.Printf("%s\n", msgdata)
+	  }
+	  /*fmt.Printf("%T\n", msgdata)*/
+	  /*fmt.Printf("%s\n", msgdata)*/
+	  /*fmt.Printf("%x\n", msgdata)*/
 	}
 }
 
