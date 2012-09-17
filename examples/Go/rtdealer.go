177711c1df7041773b0a26f2bfe139e9157cac15
Harm Aarts <harmaarts@gmail.com>
2012-09-17 18:28:54 +0200

    implements main function, todo workers

--- examples/Go/rtdealer.go
+++ examples/Go/rtdealer.go
@@ -10,12 +10,19 @@ package main
 import (
   zmq "github.com/alecthomas/gozmq"
   "time"
+  "math/rand"
 )
+
+func workerTaskA() {
+}
+
+func workerTaskB() {
+}
 
 func main() {
 	context, _ := zmq.NewContext()
 	defer context.Close()
-	client, _ := zmq.NewSocket(zmq.ROUTER)
+	client, _ := context.NewSocket(zmq.ROUTER)
 	defer client.Close()
 
 	go workerTaskA()
@@ -26,7 +33,16 @@ func main() {
 	time.Sleep(time.Second)
 
 	//  Send 10 tasks scattered to A twice as often as B
-
-
-
+	for i := 0; i < 10; i++ {
+		parts := make([][]byte, 0)
+		if rand.Intn(10) < 3 {
+		  parts = append(parts, []byte("A"))
+		} else {
+		  parts = append(parts, []byte("B"))
+		}
+		parts = append(parts, []byte("This is the workload"))
+		client.SendMultipart(parts, 0)
+	}
+	client.SendMultipart([][]byte{[]byte("A"), []byte("END")}, 0)
+	client.SendMultipart([][]byte{[]byte("B"), []byte("END")}, 0)
 }
