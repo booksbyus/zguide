68db4a7b3f6038455825ccbb168a9e7dc1287186
theburningmonk <theburningmonk@gmail.com>
2012-09-18 21:59:07 +0100

    - added Chatper 5 Clone Pattern version 1

---
+++ examples/C#/clonecli1.cs
@@ -0,0 +1,57 @@
+﻿
+//
+//  Clone client, Model One in C#
+//
+
+//  Author:     Yan Cui
+//  Email:      theburningmonk@gmail.com
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ZMQ;
+
+namespace ZMQGuide
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new Context(1))
+            {
+                using (var subscriber = context.Socket(SocketType.SUB))
+                {
+                    subscriber.Connect("tcp://localhost:5556");
+                    subscriber.Subscribe(string.Empty, Encoding.Unicode);
+
+                    var interrupted = false;
+                    var sequence = 0L;
+                    var dict = new Dictionary<string, KvMsg>();
+
+                    Console.CancelKeyPress += (s, e) => { interrupted = true; };
+
+                    while (!interrupted)
+                    {
+                        KvMsg kvmsg;
+
+                        try
+                        {
+                            kvmsg = KvMsg.Recv(subscriber);
+                            Console.WriteLine("Received {0}", kvmsg);
+                        }
+                        catch (System.Exception)
+                        {
+                            break;
+                        }
+
+                        kvmsg.Store(dict);
+                        sequence++;
+                    }
+
+                    Console.WriteLine(" Interrupted\n{0} messages in\n", sequence);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ examples/C#/clonesrv1.cs
@@ -0,0 +1,55 @@
+﻿
+//
+//  Clone server, Model One in C#
+//
+
+//  Author:     Yan Cui
+//  Email:      theburningmonk@gmail.com
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using ZMQ;
+
+namespace ZMQGuide
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new Context(1))
+            {
+                using (var publisher = context.Socket(SocketType.PUB))
+                {
+                    publisher.Bind("tcp://*:5556");
+                    Thread.Sleep(TimeSpan.FromMilliseconds(200));
+
+                    var interrupted = false;
+                    var sequence = 0L;
+                    var random = new Random((int)DateTime.UtcNow.Ticks);
+                    var dict = new Dictionary<string, KvMsg>();
+
+                    Console.CancelKeyPress += (s, e) => { interrupted = true; };
+
+                    while (!interrupted)
+                    {
+                        sequence++;
+                        var kvmsg = new KvMsg(sequence);
+                        kvmsg.Key = random.Next(1, 10000).ToString();
+                        kvmsg.Body = random.Next(1, 1000000).ToString();
+
+                        kvmsg.Send(publisher);
+                        kvmsg.Store(dict);
+
+                        Console.WriteLine("Published {0}", kvmsg);
+
+                        Thread.Sleep(TimeSpan.FromMilliseconds(100));
+                    }
+
+                    Console.WriteLine(" Interrupted\n{0} messages out\n", sequence);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
