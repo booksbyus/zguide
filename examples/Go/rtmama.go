2be2c2b0e65ef03b9b42e00e29b928720e81efb8
Harm Aarts <harmaarts@gmail.com>
2012-10-07 13:36:11 +0200

    program is functional now; however ther reporting of processed items is weird. everytime there is a duplicate ID but with a different amount of processed tasks

--- examples/Go/rtmama.go
+++ examples/Go/rtmama.go
@@ -8,21 +8,46 @@ import (
 	zmq "github.com/alecthomas/gozmq"
 	"math/rand"
 	"time"
+	"fmt"
+	"strings"
 )
 
 const NBR_WORKERS = 10
 
-func (socket *zmq.Socket) setId() {
+func randomString() string {
+  source := "abcdefghijklmnopqrstuvwxyz"
+  target := make([]string, 20)
+  for i := 0; i < 20; i++ {
+	target[i] = string(source[rand.Intn(len(source))])
+  }
+  return strings.Join(target, "")
 }
 
-func workerTask() {
+func workerTask(randomId string) {
   context, _ := zmq.NewContext()
   defer context.Close()
 
   worker, _ := context.NewSocket(zmq.REQ)
+  /*worker.SetSockOptString(zmq.IDENTITY, randomString())*/
+  worker.SetSockOptString(zmq.IDENTITY, randomId)
+
+  worker.Connect("ipc://routing.ipc")
   defer worker.Close()
-  
-  /*worker.SetSockOptString(zmq.IDENTITY, )*/
+
+  var total int
+  for {
+	err := worker.Send([]byte("ready"), 0)
+	if err != nil { print(err) }
+	data, _ := worker.Recv(0)
+	if string(data) == "END" {
+	  id, _ := worker.GetSockOptString(zmq.IDENTITY)
+	  fmt.Printf("Processed: %2d tasks (%s)\n", total, id)
+	}
+	total += 1
+
+	msec := rand.Intn(1000)
+	time.Sleep(time.Duration(msec) * time.Millisecond)
+  }
 }
 
 //  While this example runs in a single process, that is just to make
@@ -39,26 +64,26 @@ func main() {
 
 	rand.Seed(time.Now().Unix())
 
-	print("spinning up workers")
 	for i := 0; i < NBR_WORKERS; i++ {
-	  go workerTask()
+	  go workerTask(randomString())
 	}
 
-	print("send work")
-	for i := 0; i < NBR_WORKERS; i++ {
+	assignments := make(map[string]int)
+	for i := 0; i < NBR_WORKERS * 10; i++ {
 	  //  LRU worker is next waiting in queue
 	  parts, err := client.RecvMultipart(0)
 	  if err != nil { print(err) }
 	  address := parts[0]
-	  client.SendMultipart([][]byte{address, []byte("This is the workload")}, 0)
+	  client.SendMultipart([][]byte{address, []byte(""), []byte("This is the workload")}, 0)
+	  assignments[string(address)] += 1
 	}
+	fmt.Println(assignments)
 
-	print("shutdown")
 	//  Now ask mamas to shut down and report their results
 	for i := 0; i < NBR_WORKERS; i++ {
 	  parts, err := client.RecvMultipart(0)
 	  if err != nil { print(err) }
 	  address := parts[0]
-	  client.SendMultipart([][]byte{address, []byte("END")}, 0)
+	  client.SendMultipart([][]byte{address, []byte(""), []byte("END")}, 0)
 	}
 }
