98bd0368a0abcdb2321709a9a04b0aa2d9d3c640
Harm Aarts <harmaarts@gmail.com>
2012-10-07 13:54:29 +0200

    fmt

--- examples/Go/rtmama.go
+++ examples/Go/rtmama.go
@@ -5,50 +5,52 @@
 package main
 
 import (
+	"fmt"
 	zmq "github.com/alecthomas/gozmq"
 	"math/rand"
-	"time"
-	"fmt"
 	"strings"
+	"time"
 )
 
 const NBR_WORKERS = 10
 
 func randomString() string {
-  source := "abcdefghijklmnopqrstuvwxyz"
-  target := make([]string, 20)
-  for i := 0; i < 20; i++ {
-	target[i] = string(source[rand.Intn(len(source))])
-  }
-  return strings.Join(target, "")
+	source := "abcdefghijklmnopqrstuvwxyz"
+	target := make([]string, 20)
+	for i := 0; i < 20; i++ {
+		target[i] = string(source[rand.Intn(len(source))])
+	}
+	return strings.Join(target, "")
 }
 
 func workerTask(randomId string) {
-  context, _ := zmq.NewContext()
-  defer context.Close()
+	context, _ := zmq.NewContext()
+	defer context.Close()
 
-  worker, _ := context.NewSocket(zmq.REQ)
-  /*worker.SetSockOptString(zmq.IDENTITY, randomString())*/
-  worker.SetSockOptString(zmq.IDENTITY, randomId)
+	worker, _ := context.NewSocket(zmq.REQ)
+	/*worker.SetSockOptString(zmq.IDENTITY, randomString())*/
+	worker.SetSockOptString(zmq.IDENTITY, randomId)
 
-  worker.Connect("ipc://routing.ipc")
-  defer worker.Close()
+	worker.Connect("ipc://routing.ipc")
+	defer worker.Close()
 
-  var total int
-  for {
-	err := worker.Send([]byte("ready"), 0)
-	if err != nil { print(err) }
-	data, _ := worker.Recv(0)
-	if string(data) == "END" {
-	  id, _ := worker.GetSockOptString(zmq.IDENTITY)
-	  fmt.Printf("Processed: %2d tasks (%s)\n", total, id)
-	  break
+	var total int
+	for {
+		err := worker.Send([]byte("ready"), 0)
+		if err != nil {
+			print(err)
+		}
+		data, _ := worker.Recv(0)
+		if string(data) == "END" {
+			id, _ := worker.GetSockOptString(zmq.IDENTITY)
+			fmt.Printf("Processed: %2d tasks (%s)\n", total, id)
+			break
+		}
+		total += 1
+
+		msec := rand.Intn(1000)
+		time.Sleep(time.Duration(msec) * time.Millisecond)
 	}
-	total += 1
-
-	msec := rand.Intn(1000)
-	time.Sleep(time.Duration(msec) * time.Millisecond)
-  }
 }
 
 //  While this example runs in a single process, that is just to make
@@ -66,25 +68,26 @@ func main() {
 	rand.Seed(time.Now().Unix())
 
 	for i := 0; i < NBR_WORKERS; i++ {
-	  go workerTask(randomString())
+		go workerTask(randomString())
 	}
 
-	assignments := make(map[string]int)
-	for i := 0; i < NBR_WORKERS * 10; i++ {
-	  //  LRU worker is next waiting in queue
-	  parts, err := client.RecvMultipart(0)
-	  if err != nil { print(err) }
-	  address := parts[0]
-	  client.SendMultipart([][]byte{address, []byte(""), []byte("This is the workload")}, 0)
-	  assignments[string(address)] += 1
+	for i := 0; i < NBR_WORKERS*10; i++ {
+		//  LRU worker is next waiting in queue
+		parts, err := client.RecvMultipart(0)
+		if err != nil {
+			print(err)
+		}
+		address := parts[0]
+		client.SendMultipart([][]byte{address, []byte(""), []byte("This is the workload")}, 0)
 	}
-	fmt.Println(assignments)
 
 	//  Now ask mamas to shut down and report their results
 	for i := 0; i < NBR_WORKERS; i++ {
-	  parts, err := client.RecvMultipart(0)
-	  if err != nil { print(err) }
-	  address := parts[0]
-	  client.SendMultipart([][]byte{address, []byte(""), []byte("END")}, 0)
+		parts, err := client.RecvMultipart(0)
+		if err != nil {
+			print(err)
+		}
+		address := parts[0]
+		client.SendMultipart([][]byte{address, []byte(""), []byte("END")}, 0)
 	}
 }
