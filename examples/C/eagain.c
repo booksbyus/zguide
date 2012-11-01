20cffe1e4ef8981741bf100bdf0ba30b718ba1a8
Pieter Hintjens <ph@imatix.com>
2012-11-01 13:54:14 +0100

    Added EAGAIN example for Ch 8

---
+++ examples/C/eagain.c
@@ -0,0 +1,23 @@
+#include <czmq.h>
+
+int main (void) {
+    zctx_t *ctx = zctx_new ();
+    
+    void *mailbox = zsocket_new (ctx, ZMQ_DEALER);
+    zsocket_set_sndhwm (mailbox, 4);
+    zsocket_set_sndtimeo (mailbox, 0);
+    zsocket_connect (mailbox, "tcp://localhost:9876");
+
+    int count;
+    for (count = 0; count < 10; count++) {
+        printf ("Sending message %d\n", count);
+        int rc = zstr_sendf (mailbox, "message %d", count);
+        if (rc == -1) {
+            printf ("W: %s\n", strerror (errno));
+            break;
+        }
+    }
+    sleep (1);
+    zctx_destroy (&ctx);
+    return 0;
+}
