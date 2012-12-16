e7b400a66ed5c8c58d61aa0c2e65caaa418307f8
Pieter Hintjens <ph@imatix.com>
2012-12-16 14:40:03 +0100

    Various cleanups
    * Separate print/web prefaces
    * Full list of contributors is too verbose for print
    * Renamed and simplified toolchain
    * Moved zap-pow image to start of text

--- README.txt
+++ README.txt
@@ -33,7 +33,7 @@ A 0MQ socket is what you get when you take a normal TCP socket, inject it with a
 License
 -------
 
-The text of "0MQ - The Guide" is copyright (c) 2009-2012 Pieter Hintjens, and is licensed under the Creative Commons Attribution-Share Alike 3.0 License. The source code examples are licensed under MIT/X11 unless otherwise specified in the code. `z2w` is placed into the public domain.
+The text of "0MQ - The Guide" is copyright (c) 2009-2012 Pieter Hintjens, and is licensed under the Creative Commons Attribution-Share Alike 3.0 License. The source code examples are licensed under MIT/X11 unless otherwise specified in the code. The tools in /bin are placed into the public domain.
 
 Contributions
 -------------
--- examples/C/hwclient.c
+++ examples/C/hwclient.c
@@ -17,21 +17,21 @@ int main (void)
     void *requester = zmq_socket (context, ZMQ_REQ);
     zmq_connect (requester, "tcp://localhost:5555");
 
-//     int request_nbr;
-//     for (request_nbr = 0; request_nbr != 10; request_nbr++) {
-//         zmq_msg_t request;
-//         zmq_msg_init_size (&request, 5);
-//         memcpy (zmq_msg_data (&request), "Hello", 5);
-//         printf ("Sending Hello %d...\n", request_nbr);
-//         zmq_msg_send (&request, requester, 0);
-//         zmq_msg_close (&request);
-// 
-//         zmq_msg_t reply;
-//         zmq_msg_init (&reply);
-//         zmq_msg_recv (&reply, requester, 0);
-//         printf ("Received World %d\n", request_nbr);
-//         zmq_msg_close (&reply);
-//     }
+    int request_nbr;
+    for (request_nbr = 0; request_nbr != 10; request_nbr++) {
+        zmq_msg_t request;
+        zmq_msg_init_size (&request, 5);
+        memcpy (zmq_msg_data (&request), "Hello", 5);
+        printf ("Sending Hello %d...\n", request_nbr);
+        zmq_msg_send (&request, requester, 0);
+        zmq_msg_close (&request);
+
+        zmq_msg_t reply;
+        zmq_msg_init (&reply);
+        zmq_msg_recv (&reply, requester, 0);
+        printf ("Received World %d\n", request_nbr);
+        zmq_msg_close (&reply);
+    }
     sleep (2);
     zmq_close (requester);
     zmq_ctx_destroy (context);
