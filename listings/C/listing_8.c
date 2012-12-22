a41917a205b56efbbeda4b3e6fa4114418196af5
Pieter Hintjens <ph@imatix.com>
2012-12-22 13:30:46 +0100

    Reduced image margins

--- listings/C/listing_8.c
+++ listings/C/listing_8.c
@@ -1,25 +1,6 @@
-//  Receive 0MQ string from socket and convert into C string
-static char *
-s_recv (void *socket) {
-    zmq_msg_t message;
-    zmq_msg_init (&message);
-    int size = zmq_msg_recv (&message, socket, 0);
-    if (size == -1)
-        return NULL;
-    char *string = malloc (size + 1);
-    memcpy (string, zmq_msg_data (&message), size);
-    zmq_msg_close (&message);
-    string [size] = 0;
-    return (string);
-}
-
-//  Convert C string to 0MQ string and send to socket
-static int
-s_send (void *socket, char *string) {
-    zmq_msg_t message;
-    zmq_msg_init_size (&message, strlen (string));
-    memcpy (zmq_msg_data (&message), string, strlen (string));
-    int size = zmq_msg_send (&message, socket, 0);
-    zmq_msg_close (&message);
-    return (size);
-}
+typedef struct {
+    void *socket;       //  0MQ socket to poll on
+    int fd;             //  OR, native file handle to poll on
+    short events;       //  Events to poll on
+    short revents;      //  Events returned after poll
+} zmq_pollitem_t;
