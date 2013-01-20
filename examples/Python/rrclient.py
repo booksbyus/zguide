d97d85df2dcd0b5e995d1f03ba0eef8f3d694033
Jens Rantil <jens.rantil@gmail.com>
2013-01-20 20:24:52 +0100

    Bugfix: Actually doing ten requests, not nine

--- examples/Python/rrclient.py
+++ examples/Python/rrclient.py
@@ -11,7 +11,7 @@ socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5559")
 
 #  Do 10 requests, waiting each time for a response
-for request in range(1,10):
+for request in range(1,11):
     socket.send("Hello")
     message = socket.recv()
     print "Received reply ", request, "[", message, "]"
