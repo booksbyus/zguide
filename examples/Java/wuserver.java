a258b733a8f933c7e01f66369c040e9f8ad9c1b5
Min(Dongmin Yu) <miniway@gmail.com>
2013-01-24 11:30:59 +0900

    java chapter 1 example

--- examples/Java/wuserver.java
+++ examples/Java/wuserver.java
@@ -6,30 +6,31 @@ import org.zeromq.ZMQ;
 //  Binds PUB socket to tcp://*:5556
 //  Publishes random weather updates
 //
-//  Nicola Peduzzi <thenikso@gmail.com>
-//
 public class wuserver {
 
-	public static void main(String[] args) {
-		//  Prepare our context and publisher
-		ZMQ.Context context = ZMQ.context(1);
+    public static void main (String[] args) throws Exception {
+        //  Prepare our context and publisher
+        ZMQ.Context context = ZMQ.context(1);
 
-		ZMQ.Socket publisher = context.socket(ZMQ.PUB);
-		publisher.bind("tcp://*:5556");
-		publisher.bind("ipc://weather");
+        ZMQ.Socket publisher = context.socket(ZMQ.PUB);
+        publisher.bind("tcp://*:5556");
+        publisher.bind("ipc://weather");
 
-		//  Initialize random number generator
-		Random srandom = new Random(System.currentTimeMillis());
-		while (true) {
-			//  Get values that will fool the boss
-			int zipcode, temperature, relhumidity;
-			zipcode = srandom.nextInt(100000) + 1;
-			temperature = srandom.nextInt(215) - 80 + 1;
-			relhumidity = srandom.nextInt(50) + 10 + 1;
+        //  Initialize random number generator
+        Random srandom = new Random(System.currentTimeMillis());
+        while (!Thread.currentThread ().isInterrupted ()) {
+            //  Get values that will fool the boss
+            int zipcode, temperature, relhumidity;
+            zipcode = 10000 + srandom.nextInt(10000) ;
+            temperature = srandom.nextInt(215) - 80 + 1;
+            relhumidity = srandom.nextInt(50) + 10 + 1;
 
-			//  Send message to all subscribers
-			String update = String.format("%05d %d %d\u0000", zipcode, temperature, relhumidity);
-			publisher.send(update.getBytes(), 0);
-		}
-	}
+            //  Send message to all subscribers
+            String update = String.format("%05d %d %d", zipcode, temperature, relhumidity);
+            publisher.send(update.getBytes(), 0);
+        }
+
+        publisher.close ();
+        context.term ();
+    }
 }
