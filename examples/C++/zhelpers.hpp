829c20f741307ce6b310b3579e8e50935e02a1f1
Pieter Hintjens <ph@imatix.com>
2013-02-26 10:34:17 +0100

    MORE flag was wrongly defined as int64_t

--- examples/C++/zhelpers.hpp
+++ examples/C++/zhelpers.hpp
@@ -68,9 +68,9 @@ static bool
 s_send (zmq::socket_t & socket, const std::string & string) {
 
     zmq::message_t message(string.size());
-    memcpy(message.data(), string.data(), string.size());
+    memcpy (message.data(), string.data(), string.size());
 
-    bool rc = socket.send(message);
+    bool rc = socket.send (message);
     return (rc);
 }
 
@@ -79,9 +79,9 @@ static bool
 s_sendmore (zmq::socket_t & socket, const std::string & string) {
 
     zmq::message_t message(string.size());
-    memcpy(message.data(), string.data(), string.size());
+    memcpy (message.data(), string.data(), string.size());
 
-    bool rc = socket.send(message, ZMQ_SNDMORE);
+    bool rc = socket.send (message, ZMQ_SNDMORE);
     return (rc);
 }
 
@@ -94,7 +94,6 @@ s_dump (zmq::socket_t & socket)
 
     while (1) {
         //  Process all parts of the message
-
         zmq::message_t message;
         socket.recv(&message);
 
@@ -109,27 +108,23 @@ s_dump (zmq::socket_t & socket)
         for (char_nbr = 0; char_nbr < size; char_nbr++) {
             byte = data [char_nbr];
             if (byte < 32 || byte > 127)
-              is_text = false;
+                is_text = false;
         }
-
         std::cout << "[" << std::setfill('0') << std::setw(3) << size << "]";
-
         for (char_nbr = 0; char_nbr < size; char_nbr++) {
-            if (is_text) {
+            if (is_text)
                 std::cout << (char)data [char_nbr];
-            } else {
+            else
                 std::cout << std::setfill('0') << std::setw(2)
                    << std::hex << (unsigned int) data [char_nbr];
-            }
         }
         std::cout << std::endl;
 
-        int64_t more;           //  Multipart detection
+        int more = 0;           //  Multipart detection
         size_t more_size = sizeof (more);
-        socket.getsockopt(ZMQ_RCVMORE, &more, &more_size);
-
+        socket.getsockopt (ZMQ_RCVMORE, &more, &more_size);
         if (!more)
-            break;      //  Last message part
+            break;              //  Last message part
     }
 }
 
@@ -140,8 +135,8 @@ s_set_id (zmq::socket_t & socket)
 {
     std::stringstream ss;
     ss << std::hex << std::uppercase
-          << std::setw(4) << std::setfill('0') << within (0x10000) << "-"
-          << std::setw(4) << std::setfill('0') << within (0x10000);
+       << std::setw(4) << std::setfill('0') << within (0x10000) << "-"
+       << std::setw(4) << std::setfill('0') << within (0x10000);
     socket.setsockopt(ZMQ_IDENTITY, ss.str().c_str(), ss.str().length());
     return ss.str();
 }
