69e01385a7e37969c23bd90cb716df8eb6abfe6a
Pieter Hintjens <ph@imatix.com>
2013-06-27 20:11:55 +0200

    Trivial fixes

--- examples/C/hwserver.c
+++ examples/C/hwserver.c
@@ -18,8 +18,8 @@ int main (void)
         char buffer [10];
         zmq_recv (responder, buffer, 10, 0);
         printf ("Received Hello\n");
-        zmq_send (responder, "World", 5, 0);
         sleep (1);          //  Do some 'work'
+        zmq_send (responder, "World", 5, 0);
     }
     return 0;
 }
