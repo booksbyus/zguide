53b551fc4d639d6b4b36f1755527fe01b3666048
Pierre Radermecker <pradermecker@yahoo.ca>
2013-07-07 22:45:22 +0200

    Update mtrelay to monadic layer (zeromq-haskell v.0.4)

--- examples/Haskell/mtrelay.hs
+++ examples/Haskell/mtrelay.hs
@@ -1,43 +1,44 @@
 -- |
 -- Multithreaded relay in Haskell
 -- 
--- Translated to Haskell by ERDI Gergo http://gergo.erdi.hu/
+-- Originally translated to Haskell by ERDI Gergo http://gergo.erdi.hu/
 
 module Main where
 
-import System.ZMQ
+import System.ZMQ3.Monadic (ZMQ, runZMQ, socket, Pair(..), bind, connect, receive, send, async, liftIO)
+
 import Data.ByteString.Char8 (pack, unpack)
-import Control.Concurrent (threadDelay, forkIO)
 
-step1 :: Context -> IO ()
-step1 context = do
+step1 :: ZMQ z ()
+step1 = do
   -- Connect to step2 and tell it we're ready
-  withSocket context Pair $ \xmitter -> do
-    connect xmitter "inproc://step2"
-    send xmitter (pack "READY") []
+  xmitter <- socket Pair
+  connect xmitter "inproc://step2"
+  send xmitter [] (pack "READY")
 
-step2 :: Context -> IO ()
-step2 context = do
-  withSocket context Pair $ \receiver -> do
+step2 :: ZMQ z ()
+step2  = do
+    receiver <- socket Pair
     -- Bind inproc socket before starting step1
     bind receiver "inproc://step2"
-    forkIO $ step1 context
+    async step1
     
     -- Wait for signal and pass it on
-    msg <- receive receiver []
+    msg <- receive receiver
     
     -- Connect to step3 and tell it we're ready
-    withSocket context Pair $ \xmitter -> do
-      connect xmitter "inproc://step3"
-      send xmitter msg []
+    xmitter <- socket Pair
+    connect xmitter "inproc://step3"
+    send xmitter [] msg
       
 main :: IO ()
-main = withContext 1 $ \context -> do  
-  -- Bind inproc socket before starting step2
-  withSocket context Pair $ \receiver -> do
+main = 
+    runZMQ $ do
+    -- Bind inproc socket before starting step2
+    receiver <- socket Pair
     bind receiver "inproc://step3"
-    forkIO $ step2 context
+    async step2
     
     -- Wait for signal
-    msg <- receive receiver []
-    putStrLn $ unwords ["Test successful!", unpack msg]
+    msg <- receive receiver
+    liftIO $ putStrLn $ unwords ["Test successful!", unpack msg]
