d3c0b697cf9ea003bce8ad6aea593c054c56ee0a
Pierre Radermecker <pradermecker@yahoo.ca>
2013-07-09 17:03:22 +0200

    Update tasksink2 to monadic layer (zeromq-haskell v.0.4)

--- examples/Haskell/tasksink2.hs
+++ examples/Haskell/tasksink2.hs
@@ -1,34 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+-- |
+-- Task sink
+-- Binds PULL socket to tcp://localhost:5558
+-- Collects results from workers via that socket
+-- 
+-- Originally translated to Haskell by ERDI Gergo http://gergo.erdi.hu/
+
 module Main where
 
-import System.ZMQ
+import System.ZMQ3.Monadic(runZMQ, socket, bind, receive, send, Pull(..), Pub(..), liftIO)
 import Control.Monad (forM_)
-import System.Random (randomRIO)
 import System.IO (hSetBuffering, stdout, BufferMode(..))
-import Data.Time.Clock
-import Data.ByteString.Char8 (pack)
+import Data.Time.Clock (getCurrentTime, diffUTCTime)
 
 main :: IO ()
-main = withContext 1 $ \context -> do  
-  withSocket context Pull $ \receiver -> do
-    bind receiver "tcp://*:5558"
-    withSocket context Pub $ \controller -> do
-        bind controller "tcp://*:5559"
-        
-        -- Wait for start of batch
-        _ <- receive receiver []
-    
-        -- Start our clock now
-        startTime <- getCurrentTime
-                    
-        -- Process 100 confirmations
-        hSetBuffering stdout NoBuffering
-        forM_ [1..100] $ \i -> do
-            _ <- receive receiver []
-            putStr $ if i `mod` 10 == 0 then ":" else "."
-      
-        endTime <- getCurrentTime
-        let elapsedTime = diffUTCTime endTime startTime
-        let elapsedMsec = elapsedTime * 1000
-        putStrLn $ unwords ["Total elapsed time:", show elapsedMsec, "msecs"]
+main = do
 
-        send controller (pack "KILL") []
+    hSetBuffering stdout NoBuffering
+
+    runZMQ $ do
+
+        receiver <- socket Pull
+        bind receiver "tcp://*:5558"
+
+        controller <- socket Pub
+        bind controller "tcp://*:5559"
+
+        -- Wait for start of batch
+        receive receiver
+        
+        -- Start our clock now
+        startTime <- liftIO $ getCurrentTime
+
+        -- Process 100 confirmations    
+        forM_ [1..100] $ \i -> do
+            receive receiver
+            liftIO $ putStr $ if i `mod` 10 == 0 then ":" else "."
+          
+        endTime <- liftIO $ getCurrentTime
+        let elapsedTime = diffUTCTime endTime startTime
+            elapsedMsec = elapsedTime * 1000
+        liftIO $ putStrLn $ unwords ["Total elapsed time:", show elapsedMsec, "msecs"]
+
+        send controller [] "KILL"
