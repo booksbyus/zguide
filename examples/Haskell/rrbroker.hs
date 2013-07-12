f0ab701b19a1c277fab54845b8a3ab5381c398fb
Pierre Radermecker <pradermecker@yahoo.ca>
2013-07-12 16:18:10 +0200

    Clean-up: remove useless code

--- examples/Haskell/rrbroker.hs
+++ examples/Haskell/rrbroker.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import System.ZMQ3.Monadic (runZMQ, socket, bind, receive, moreToReceive, proxy, send, poll, Poll(..), Event(..), Router(..), Dealer(..), Socket, Timeout, Flag (SendMore), liftIO)
+import System.ZMQ3.Monadic
 import Control.Monad (forever)    
 
 main :: IO ()
@@ -11,24 +11,3 @@ main =
         backend <- socket Dealer
         bind backend "tcp://*:5560"
         proxy frontend backend Nothing
-        --TODO I still need to find out how to use the new version of poll
-        --forever $ liftIO $ loopFunction frontend backend (-1)
-
---loopFunction :: Socket a -> Socket b -> Timeout -> IO ()
---loopFunction front back timeout = do
---    [[]] <- poll timeout [Sock front [In] Nothing, Sock back [In] Nothing]
---    return()
-    --case evts of
-    --    Nothing -> return()
-    --    evt : [next] ->  process front back evt
-
---process :: Socket a -> Socket b -> Event -> IO ()
---process sock_recv sock_to_send Err = return ()
---process sock_recv sock_to_send In = do
---    msg <- receive sock_recv
---    more <- moreToReceive sock_recv
---    if more 
---        then do
---            send sock_to_send [SendMore] msg
---            process sock_recv sock_to_send In
---        else send sock_to_send [] msg
