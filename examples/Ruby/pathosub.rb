bbdb97ef3a22a06df8f2d1aa8b56dcf7a8c3be1a
Brian Palmer <brianp@instructure.com>
2013-07-22 21:48:35 -0600

    add ruby translations for the Last-Value-Caching section

---
+++ examples/Ruby/pathosub.rb
@@ -0,0 +1,18 @@
+#
+# Pathological subscriber
+# Subscribes to one random topic and prints received messages
+#
+require 'ffi-rzmq'
+context = ZMQ::Context.new
+
+subscriber = context.socket(ZMQ::SUB)
+subscriber.connect(ARGV[0] || "tcp://localhost:5556")
+
+topic = "%03d" % [rand(1000)]
+subscriber.setsockopt(ZMQ::SUBSCRIBE, topic)
+
+loop do
+  subscriber.recv_strings(parts = [])
+  topic, data = parts
+  puts "#{topic}: #{data}"
+end
