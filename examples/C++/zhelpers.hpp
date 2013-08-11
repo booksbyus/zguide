1f9f03d11e48f7d2c22cf896f1f3c296b846543e
ric maicle <negerns@gmail.ccom>
2013-08-12 03:33:03 +0800

    Wrap s_catch_signal() in conditional compilation under Windows
    MSVS 2010 reports 'sigaction' as unknown identifier.

--- examples/C++/zhelpers.hpp
+++ examples/C++/zhelpers.hpp
@@ -232,12 +232,14 @@ static void s_signal_handler (int signal_value)
 
 static void s_catch_signals ()
 {
+#if (!defined(__WINDOWS__))
     struct sigaction action;
     action.sa_handler = s_signal_handler;
     action.sa_flags = 0;
     sigemptyset (&action.sa_mask);
     sigaction (SIGINT, &action, NULL);
     sigaction (SIGTERM, &action, NULL);
+#endif
 }
 
 #endif
