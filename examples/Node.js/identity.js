4efde23ca5a2c1ac6fb95695a5fea3a5368e9c42
Brandon Tilley <brandon@brandontilley.com>
2013-12-02 00:03:52 -0800

    Add Node.js version of `identity` example

---
+++ examples/Node.js/identity.js
@@ -0,0 +1,26 @@
+//  Demonstrate request-reply identities
+
+var zmq = require("zmq"),
+    zhelpers = require('./zhelpers');
+
+var sink = zmq.socket("router");
+sink.bind("inproc://example");
+
+sink.on("message", zhelpers.dumpFrames);
+
+//  First allow 0MQ to set the identity
+var anonymous = zmq.socket("req");
+anonymous.connect("inproc://example");
+anonymous.send("ROUTER uses generated UUID");
+
+//  Then set the identity ourselves
+var identified = zmq.socket("req");
+identified.identity = "PEER2";
+identified.connect("inproc://example");
+identified.send("ROUTER uses REQ's socket identity");
+
+setTimeout(function() {
+  anonymous.close();
+  identified.close();
+  sink.close();
+}, 250);
