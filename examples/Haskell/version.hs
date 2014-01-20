43cf046f90b6e1b6399dcdba706f9d7cca2567c5
PierreR <pierrer@pi3r.be>
2014-01-20 15:29:48 +0100

    Cosmetic: improve code readability

--- examples/Haskell/version.hs
+++ examples/Haskell/version.hs
@@ -4,5 +4,6 @@ import System.ZMQ4 (version)
 import Text.Printf (printf)
 
 main :: IO ()
-main = version >>= putStrLn . fmt where
-    fmt (major, minor, patch) = printf "Current 0MQ version is %d.%d.%d" major minor patch
+main = do
+    (major, minor, patch) <- version
+    printf "Current 0MQ version is %d.%d.%d" major minor patch
