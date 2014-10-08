955193703bc9a73f091a4528e4ebdc93a0d1d3ba
Matthew Wraith <wraithm@gmail.com>
2014-10-08 01:46:07 -0500

    Adding request-reply examples rrbroker, rrclient, and rrworker for OCaml.

---
+++ examples/OCaml/rrclient.ml
@@ -0,0 +1,20 @@
+(**
+ * Hello World client
+ * Connects REQ socket to tcp://localhost:5559
+ * Sends "Hello" to server, expects "World" back
+ *)
+open ZMQ
+open Helpers
+
+let () =
+    with_context @@ fun ctx ->
+
+    (* Socket to talk to server *)
+    with_socket ctx Socket.req @@ fun requester ->
+    Socket.connect requester "tcp://localhost:5559";
+
+    for requestNum = 0 to 9 do
+        Socket.send requester "Hello";
+        let s = Socket.recv requester in
+        printfn "Received reply %d [%S]" requestNum s;
+    done
