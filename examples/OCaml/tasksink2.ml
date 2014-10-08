0dc3a7925866b3885031ff58f911d82a418c838d
Matthew Wraith <wraithm@gmail.com>
2014-10-08 11:12:27 -0500

    Adding tasksink2 and taskwork2 to OCaml examples.

---
+++ examples/OCaml/tasksink2.ml
@@ -0,0 +1,33 @@
+(**
+ * Task sink - design 2
+ * Adds pub-sub flow to send kill signal to workers
+ *)
+open ZMQ
+open Helpers
+
+let () =
+  with_context @@ fun ctx ->
+  (* Socket to receive messages on *)
+  with_socket ctx Socket.pull @@ fun receiver ->
+  Socket.bind receiver "tcp://*:5558";
+
+  (* Scoket for worker control *)
+  with_socket ctx Socket.pub @@ fun controller ->
+  Socket.bind controller "tcp://*:5559";
+
+  (* Wait for start of batch *)
+  let _ = Socket.recv receiver in
+
+  (* Start our clock now *)
+  let start_time = clock_ms () in
+
+  (* Process 100 confirmations *)
+  for taskNum = 0 to 99 do
+    let _ = Socket.recv receiver in
+    printfn @@ if ((taskNum / 10) * 10 == taskNum) then ":" else ".";
+  done;
+
+  printfn "Total elapsed time: %d msec" (clock_ms () - start_time);
+
+  (* Send kill signal to workers *)
+  Socket.send controller "KILL";
---
+++ examples/OCaml/taskwork2.ml
@@ -0,0 +1,43 @@
+(**
+ * Task worker - design 2
+ * Adds pub-sub flow to receive and respond to kill signal
+ *)
+open ZMQ
+open Helpers
+
+let () =
+  with_context @@ fun ctx ->
+  (* Socket to receive messages on *)
+  with_socket ctx Socket.pull @@ fun receiver ->
+  Socket.connect receiver "tcp://localhost:5557";
+
+  (* Socket to send messages to *)
+  with_socket ctx Socket.push @@ fun sender ->
+  Socket.connect sender "tcp://localhost:5558";
+
+  (* Socket for control input *)
+  with_socket ctx Socket.sub @@ fun controller ->
+  Socket.connect controller "tcp://localhost:5559";
+  Socket.subscribe controller "";
+
+  let items = Poll.mask_of [| (receiver, Poll.In); (controller, Poll.In) |] in
+
+  (* Process tasks from either socket *)
+  while true do
+    let pollResults = Poll.poll items in
+    let receiverEvents = pollResults.(0) in
+    let ctrlEvents = pollResults.(1) in
+
+    match receiverEvents with
+    | Some _ ->
+      let s = Socket.recv receiver in
+      printfn "%S." s; (* Show progress *)
+      sleep_ms (int_of_string s); (* Do the work *)
+      Socket.send sender ""; (* Send results to sink *)
+    | _ -> ();
+
+    (* Any waiting controller command acts as 'KILL' *)
+    match ctrlEvents with
+    | Some _ -> exit 0; (* Exit program *)
+    | _ -> ();
+  done
