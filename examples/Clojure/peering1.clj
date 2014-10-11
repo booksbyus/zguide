bac6189c13c5172ce52b184dbde439974d6d283e
Maximilian Karasz <maximilian.karasz@googlemail.com>
2014-10-11 15:44:03 +0200

    port to cljzmq.

--- examples/Clojure/peering1.clj
+++ examples/Clojure/peering1.clj
@@ -1,39 +1,43 @@
 (ns peering1
-  (:refer-clojure :exclude [send])
-  (:require [zhelpers :as mq])
+  (:require [zeromq.zmq :as zmq])
   (:import [java.util Random]))
 
-;;
 ;; Broker peering simulation (part 1)
 ;; Prototypes the state flow
-;; Isaiah Peng <issaria@gmail.com>
 
 (defn -main
   "First argument is this broker's name, other arguments are our peer's name."
-  [& args]
-  (if (< (count args) 2)
-    (do
-      (println "syntax: peering me {you}...")
-      (System/exit)))
+  [& [self & peers :as args]]
+  (when (empty? peers)
+    (println "syntax: peering me {you}...")
+    (System/exit 0))
+  (println (format "I: preparing broker at %s..." self))
   (let [srandom (Random. (System/currentTimeMillis))
-        self (nth args 0)
-        ctx (mq/contenxt 1)
-        statebe (mq/socket ctx mq/pub)
-        statefe (mq/socket ctx mq/sub)]
-    (println (format "I: preparing broker at %s..." self))
-    (mq/bind statebe (format "ipc://%s-state.ipc" self))
+        ctx (zmq/context 1)
+        ;; Bind state backend to endpoint
+        statebe (doto (zmq/socket ctx :pub)
+                  (zmq/bind (format "ipc://%s-state.ipc" self)))
+        statefe (doto (zmq/socket ctx :sub)
+                  (zmq/subscribe ""))]
+
+
+
     ;; Connect statefe to all peers
-    (doseq [peer (rest args)]
+    (doseq [peer peers]
       (println (format "I: connecting to state backend at '%s'" peer))
-      (mq/connect statefe (format "ipc://%s-state.ipc", peer)))
-    ;; Send out status messages to peers, and collect from peers, the zmq_poll timeout define our own heatbeating
-    (while true
-      (let [poller (.poller ctx 1)]
-        (.register poller statefe (ZMQ$Poller/POLLIN))
-        (.poll poller 1000000)
-        (if (.pollin poller 0)
-          (let [addr body (mq/recv-all statefe)]
+      (zmq/connect statefe (format "ipc://%s-state.ipc", peer)))
+
+    ;; The main loop sends out status messages to peers, and collects
+    ;; status messages back from peers. The zmq_poll timeout defines
+    ;; our own heartbeat:
+
+    (let [poller (doto (zmq/poller ctx 1)
+                   (zmq/register statefe :pollin))]
+      (while (not= -1 (zmq/poll poller 1000))
+        (if (zmq/check-poller poller 0 :pollin)
+          (let [[addr body] (mapv #(String. %)
+                                  (zmq/receive-all statefe))]
             (println (format "%s - %s workers free" addr body)))
           (do
-            (mq/sndmore statefe self)
-            (mq/send statefe (.nextInt srandom 10))))))))
+            (zmq/send-str statebe self zmq/send-more)
+            (zmq/send-str statebe (format "%d" (.nextInt srandom 10)))))))))
