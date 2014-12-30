08afe7800004d4bac28c3e0a32b16bd7abfbfff9
Jean Chassoul <chassoul@gmail.com>
2014-12-30 16:39:33 -0600

    Julia examples for the first chapter of the guide

---
+++ examples/Julia/hwclient.jl
@@ -0,0 +1,29 @@
+#!/usr/bin/env julia
+
+#
+# Hello World client in Julia
+# Connects REQ socket to tcp://localhost:5555
+# Sends "Hello" to server, expects "World" back
+#
+
+using ZMQ
+
+context = Context()
+
+# Socket to talk to server
+println("Connecting to hello world server...")
+socket = Socket(context, REQ)
+ZMQ.connect(socket, "tcp://localhost:5555")
+
+for request in [1:10]
+    println("Sending request $request ...")
+    ZMQ.send(socket, "Hello")
+
+    # Get the reply.
+    message = bytestring(ZMQ.recv(socket))
+    println("Received reply $request [ $message ]")
+end
+
+# Making a clean exit.
+ZMQ.close(socket)
+ZMQ.close(context)
---
+++ examples/Julia/hwserver.jl
@@ -0,0 +1,29 @@
+#!/usr/bin/env julia
+
+#
+# Hello World server in Julia
+# Binds REP socket to tcp://*:5555
+# Expects "Hello" from client, replies "World"
+#
+
+using ZMQ
+
+context = Context()
+socket = Socket(context, REP)
+ZMQ.bind(socket, "tcp://*:5555")
+
+while true
+    # Wait for next request from client
+    message = bytestring(ZMQ.recv(socket))
+    println("Received request: $message")
+
+    # Do some 'work'
+    sleep(1)
+
+    # Send reply back to client
+    ZMQ.send(socket, "World")
+end
+
+# classy hit men always clean up when finish the job.
+ZMQ.close(socket)
+ZMQ.close(context)
