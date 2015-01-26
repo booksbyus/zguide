d811f452d25ec2b3c52014c6f5837a668caff4d4
metadings <metadings@github.com>
2015-01-26 21:05:40 +0100

    Updating C# Examples: FLServer2, FLClient2

---
+++ examples/C#/flclient2.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using ZeroMQ;
+
+namespace ZeroMQ.Test
+{
+	using FLClient2;
+
+	//
+	// Freelance client - Model 2
+	// Uses DEALER socket to blast one or more services
+	//
+	// Authors: Pieter Hintjens, Uli Riehm
+	//
+
+	namespace FLClient2
+	{
+		public class FLClient : IDisposable
+		{
+			// If not a single service replies within this time, give up
+			static readonly TimeSpan GLOBAL_TIMEOUT = TimeSpan.FromMilliseconds(2500);
+
+			// Here is the flclient class implementation. Each instance has a
+			// context, a DEALER socket it uses to talk to the servers, a counter
+			// of how many servers it's connected to, and a request sequence number:
+
+			// Our context wrapper
+			ZContext context;
+
+			// DEALER socket talking to servers
+			ZSocket socket;
+
+			// How many servers we have connected to
+			int servers;
+
+			// Number of requests ever sent
+			int sequence;
+
+			public FLClient() 
+			{
+				// Constructor
+
+				context = ZContext.Create();
+				socket = ZSocket.Create(context, ZSocketType.DEALER);
+			}
+
+			public void Dispose()
+			{
+				GC.SuppressFinalize(this);
+				Dispose(true);
+			}
+
+			protected void Dispose(bool disposing)
+			{
+				if (disposing)
+				{
+					// Destructor
+
+					if (socket != null)
+					{
+						socket.Dispose();
+						socket = null;
+					}
+					if (context != null)
+					{
+						context.Dispose();
+						context = null;
+					}
+				}
+			}
+
+			public void Connect(string endpoint) 
+			{
+				// Connect to new server endpoint
+
+				socket.Connect(endpoint);
+				servers++;
+			}
+
+			public ZMessage Request(ZMessage request)
+			{
+				// This method does the hard work. It sends a request to all
+				// connected servers in parallel (for this to work, all connections
+				// must be successful and completed by this time). It then waits
+				// for a single successful reply, and returns that to the caller.
+				// Any other replies are just dropped:
+
+				ZMessage reply = null;
+				using (request)
+				{
+					// Prefix request with sequence number and empty envelope
+					this.sequence++;
+					request.Prepend(new ZFrame(this.sequence));
+					request.Prepend(new ZFrame());
+
+					// Blast the request to all connected servers
+					for (int server = 0; server < this.servers; ++server)
+					{
+						using (var outgoing = request.Duplicate())
+						{
+							this.socket.Send(outgoing);
+						}
+					}
+
+					// Wait for a matching reply to arrive from anywhere
+					// Since we can poll several times, calculate each one
+					ZError error;
+					DateTime endtime = DateTime.UtcNow + GLOBAL_TIMEOUT;
+					var poll = ZPollItem.CreateReceiver();
+					while (endtime > DateTime.UtcNow)
+					{
+						if (this.socket.PollIn(poll, out reply, out error, TimeSpan.FromMilliseconds(64)))
+						{
+							// Reply is [empty][sequence][OK]
+							if (reply.Count < 3)
+							{
+								throw new InvalidOperationException();
+							}
+
+							reply.RemoveAt(0);
+
+							using (ZFrame sequenceFrame = reply.RemoveAt(0, false))
+							{
+								int sequence = sequenceFrame.ReadInt32();
+
+								if (sequence == this.sequence)
+								{
+									break;	// Reply is ok
+								}
+							}
+
+							reply.Dispose();
+						}
+						else
+						{
+							if (error == ZError.ETERM)
+								break;	// Interrupted
+							if (error != ZError.EAGAIN)
+								throw new ZException(error);
+						}
+					}
+				}
+				return reply;
+			}
+		}
+	}
+
+	static partial class Program
+	{
+		public static void FLClient2(IDictionary<string, string> dict, string[] args)
+		{
+			if (args == null || args.Length < 1)
+			{
+				Console.WriteLine("I: syntax: {0} FLClient2 [endpoint] ...", AppDomain.CurrentDomain.FriendlyName);
+				return;
+			}
+
+			// Create new freelance client object
+			using (var client = new FLClient())
+			{
+				// Connect to each endpoint
+				for (int i = 0; i < args.Length; ++i)
+				{
+					client.Connect(args[i]);
+				}
+
+				// Send a bunch of name resolution 'requests', measure time
+				int requests = 10000;
+				DateTime starttime = DateTime.UtcNow;
+				var error = ZError.None;
+				while (requests-- > 0)
+				{
+					var outgoing = new ZMessage();
+					outgoing.Add(new ZFrame("random name"));
+
+					ZMessage incoming = client.Request(outgoing);
+					if (incoming == null)
+					{
+						error = ZError.ETERM;
+						break;
+					}
+					incoming.Dispose(); // using (incoming) ;
+				}
+
+				if (error == ZError.ETERM)
+				{
+					Console.WriteLine("E: name service not available, aborted.");
+				}
+				else
+				{
+					Console.WriteLine("Average round trip cost: {0} ms", (DateTime.UtcNow - starttime));
+				}
+			}
+		}
+	}
+}
---
+++ examples/C#/flserver2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using ZeroMQ;
+
+namespace ZeroMQ.Test
+{
+	static partial class Program
+	{
+		public static void FLServer2(IDictionary<string, string> dict, string[] args)
+		{
+			//
+			// Freelance server - Model 2
+			// Does some work, replies OK, with message sequencing
+			//
+			// Authors: Pieter Hintjens, Uli Riehm
+			//
+
+			if (args == null || args.Length < 1)
+			{
+				Console.WriteLine("I: syntax: {0} FLServer2 [endpoint]", AppDomain.CurrentDomain.FriendlyName);
+				return;
+			}
+
+			string endpoint = args[0];
+
+			using (var context = ZContext.Create())
+			using (var server = ZSocket.Create(context, ZSocketType.REP))
+			{
+				server.Bind(endpoint);
+				Console.WriteLine("I: server is ready as {0}", endpoint);
+
+				ZError error;
+				ZMessage incoming;
+
+				while (true)
+				{
+					if (null == (incoming = server.ReceiveMessage(out error)))
+					{
+						if (error == ZError.ETERM)
+							break;	// Interrupted
+						throw new ZException(error);
+					}
+
+					ZFrame identity;
+					using (incoming)
+					{
+						// Fail nastily if run against wrong client
+						if (incoming.Count < 2)
+						{
+							throw new InvalidOperationException();
+						}
+
+						identity = incoming.RemoveAt(0, false);
+					}
+					using (identity)
+					using (var outgoing = new ZMessage())
+					{
+						outgoing.Add(identity);
+						outgoing.Add(new ZFrame("OK"));
+
+						if (!server.Send(outgoing, out error))
+						{
+							if (error == ZError.ETERM)
+								break;	// Interrupted
+							throw new ZException(error);
+						}
+					}
+				}
+
+				if (error == ZError.ETERM)
+				{
+					Console.WriteLine("W: interrupted");
+				}
+			}
+		}
+	}
+}
