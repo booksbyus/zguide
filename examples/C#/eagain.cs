631b25b93c1d1800ce765bb264cf8600a1169a6e
metadings <metadings@github.com>
2015-01-31 17:30:42 +0100

    Updating C# Examples

---
+++ examples/C#/eagain.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using ZeroMQ;
+
+namespace ZeroMQ.Test
+{
+	static partial class Program
+	{
+		public static void EAgain(IDictionary<string, string> dict, string[] args)
+		{
+			//
+			// Shows how to provoke EAGAIN when reaching HWM
+			//
+			// Author: metadings
+			//
+
+			using (var context = ZContext.Create())
+			using (var mailbox = ZSocket.Create(context, ZSocketType.DEALER))
+			{
+				mailbox.SendHighWatermark = 4;
+				mailbox.SendTimeout = TimeSpan.Zero;
+				mailbox.Linger = TimeSpan.Zero;
+				mailbox.Connect("tcp://127.0.0.1:9876");
+
+				ZError error;
+				for (int count = 0; count < 10; ++count)
+				{
+					Console.WriteLine("Sending {0}. message", count);
+
+					using (var outgoing = new ZFrame(string.Format("message {0}", count)))
+					{
+						if (!mailbox.Send(outgoing, ZSocketFlags.DontWait, out error))
+						{
+							if (error == ZError.EAGAIN)
+							{
+								Console.WriteLine(new ZException(error));
+								break;
+
+								/* Usually, when reaching EAGAIN you would do
+								error = ZError.None; // = default(ZError); // = null;
+								Thread.Sleep(1);
+
+								continue; /**/
+							}
+							if (error == ZError.ETERM)
+								return;	// Interrupted
+							throw new ZException(error);
+						}
+					}
+				}
+			}
+		}
+	}
+}
