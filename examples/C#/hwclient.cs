31a1450693f98e7bd0aba998ad5a13338f9efbb9
metadings <metadings@live.de>
2015-02-14 20:58:57 +0100

    Updating C# Examples

--- examples/C#/hwclient.cs
+++ examples/C#/hwclient.cs
@@ -18,12 +18,25 @@ namespace Examples
 			// Author: metadings
 			//
 
+			if (args == null || args.Length < 1)
+			{
+				Console.WriteLine();
+				Console.WriteLine("Usage: ./{0} HWClient [Endpoint]", AppDomain.CurrentDomain.FriendlyName);
+				Console.WriteLine();
+				Console.WriteLine("    Endpoint  Where WUClient should connect to.");
+				Console.WriteLine("              Default is tcp://127.0.0.1:5555");
+				Console.WriteLine();
+				args = new string[] { "tcp://127.0.0.1:5555" };
+			}
+
+			string endpoint = args[0];
+
 			// Create
 			using (var context = new ZContext())
 			using (var requester = new ZSocket(context, ZSocketType.REQ))
 			{
 				// Connect
-				requester.Connect("tcp://127.0.0.1:5555");
+				requester.Connect(endpoint);
 
 				for (int n = 0; n < 10; ++n)
 				{
