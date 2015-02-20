45aca7325ef0d33fe66bc0a4c87b20368c2a45b0
metadings <metadings@live.de>
2015-02-20 01:36:02 +0100

    Updating C# Examples

--- examples/C#/spqueue.cs
+++ examples/C#/spqueue.cs
@@ -38,40 +38,43 @@ namespace Examples
 				{
 					if (backend.PollIn(poll, out incoming, out error, TimeSpan.FromMilliseconds(64)))
 					{
-						// Handle worker activity on backend
-
-						// incoming[0] is worker_id
-						string worker_id = incoming[0].ReadString();
-						// Queue worker identity for load-balancing
-						worker_queue.Add(worker_id);
-
-						// incoming[1] is empty
-
-						// incoming[2] is READY or else client_id
-						string client_id = incoming[2].ReadString();
-
-						if (client_id == "READY")
+						using (incoming)
 						{
-							Console.WriteLine("I: ({0}) worker ready", worker_id);
-						}
-						else
-						{
-							// incoming[3] is empty
+							// Handle worker activity on backend
 
-							// incoming[4] is reply
-							// string reply = incoming[4].ReadString();
-							// int reply = incoming[4].ReadInt32();
+							// incoming[0] is worker_id
+							string worker_id = incoming[0].ReadString();
+							// Queue worker identity for load-balancing
+							worker_queue.Add(worker_id);
 
-							Console.WriteLine("I: ({0}) work complete", worker_id);
+							// incoming[1] is empty
 
-							using (var outgoing = new ZMessage())
+							// incoming[2] is READY or else client_id
+							string client_id = incoming[2].ReadString();
+
+							if (client_id == "READY")
 							{
-								outgoing.Add(new ZFrame(client_id));
-								outgoing.Add(new ZFrame());
-								outgoing.Add(incoming[4]);
+								Console.WriteLine("I: ({0}) worker ready", worker_id);
+							}
+							else
+							{
+								// incoming[3] is empty
 
-								// Send
-								frontend.Send(outgoing);
+								// incoming[4] is reply
+								// string reply = incoming[4].ReadString();
+								// int reply = incoming[4].ReadInt32();
+
+								Console.WriteLine("I: ({0}) work complete", worker_id);
+
+								using (var outgoing = new ZMessage())
+								{
+									outgoing.Add(new ZFrame(client_id));
+									outgoing.Add(new ZFrame());
+									outgoing.Add(incoming[4]);
+
+									// Send
+									frontend.Send(outgoing);
+								}
 							}
 						}
 					}
@@ -89,33 +92,36 @@ namespace Examples
 
 						if (frontend.PollIn(poll, out incoming, out error, TimeSpan.FromMilliseconds(64)))
 						{
-							// Here is how we handle a client request
-
-							// Dequeue the next worker identity
-							string worker_id = worker_queue[0];
-							worker_queue.RemoveAt(0);
-
-							// incoming[0] is client_id
-							string client_id = incoming[0].ReadString();
-
-							// incoming[1] is empty
-
-							// incoming[2] is request
-							// string request = incoming[2].ReadString();
-							int request = incoming[2].ReadInt32();
-
-							Console.WriteLine("I: ({0}) working on ({1}) {2}", worker_id, client_id, request);
-
-							using (var outgoing = new ZMessage())
+							using (incoming)
 							{
-								outgoing.Add(new ZFrame(worker_id));
-								outgoing.Add(new ZFrame());
-								outgoing.Add(new ZFrame(client_id));
-								outgoing.Add(new ZFrame());
-								outgoing.Add(incoming[2]);
+								// Here is how we handle a client request
 
-								// Send
-								backend.Send(outgoing);
+								// Dequeue the next worker identity
+								string worker_id = worker_queue[0];
+								worker_queue.RemoveAt(0);
+
+								// incoming[0] is client_id
+								string client_id = incoming[0].ReadString();
+
+								// incoming[1] is empty
+
+								// incoming[2] is request
+								// string request = incoming[2].ReadString();
+								int request = incoming[2].ReadInt32();
+
+								Console.WriteLine("I: ({0}) working on ({1}) {2}", worker_id, client_id, request);
+
+								using (var outgoing = new ZMessage())
+								{
+									outgoing.Add(new ZFrame(worker_id));
+									outgoing.Add(new ZFrame());
+									outgoing.Add(new ZFrame(client_id));
+									outgoing.Add(new ZFrame());
+									outgoing.Add(incoming[2]);
+
+									// Send
+									backend.Send(outgoing);
+								}
 							}
 						}
 						else
