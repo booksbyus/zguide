ae76d31f2f73b6f8c10fec349659527804236769
myd7349 <myd7349@126.com>
2015-02-27 13:25:50 +0800

    Fix #521

--- examples/C/rtdealer.c
+++ examples/C/rtdealer.c
@@ -1,15 +1,32 @@
 //  ROUTER-to-DEALER example
 
 #include "zhelpers.h"
+#include <stdint.h>
 #include <pthread.h>
 #define NBR_WORKERS 10
+
+#if (defined (WIN32)) // Fix #521
+static void
+set_identity(void *socket, intptr_t id)
+{
+    char identity[10];
+    sprintf(identity, "%04X", id);
+    zmq_setsockopt(socket, ZMQ_IDENTITY, identity, strlen(identity));
+}
+#endif
 
 static void *
 worker_task (void *args)
 {
     void *context = zmq_ctx_new ();
     void *worker = zmq_socket (context, ZMQ_DEALER);
-    s_set_id (worker);          //  Set a printable identity
+
+#if (defined (WIN32)) // Fix #521
+    set_identity(worker, (intptr_t)args);
+#else
+    s_set_id(worker);          //  Set a printable identity
+#endif
+
     zmq_connect (worker, "tcp://localhost:5671");
 
     int total = 0;
@@ -54,7 +71,7 @@ int main (void)
     int worker_nbr;
     for (worker_nbr = 0; worker_nbr < NBR_WORKERS; worker_nbr++) {
         pthread_t worker;
-        pthread_create (&worker, NULL, worker_task, NULL);
+        pthread_create(&worker, NULL, worker_task, (void *)((intptr_t)worker_nbr));
     }
     //  Run for five seconds and then tell workers to end
     int64_t end_time = s_clock () + 5000;
