03676f102f646e531c410d7920fdd7caf228806f
James Chapman <james@uplinkzero.com>
2015-04-01 11:18:30 +0100

    Updated udpping1.cpp example. Now compiles and runs on both Linux and Windows.

--- examples/C++/udpping1.cpp
+++ examples/C++/udpping1.cpp
@@ -1,48 +1,63 @@
 /*******************************************************************************
 * Author      : James Chapman
 * License     : MIT/X11 (http://directory.fsf.org/wiki/License:X11)
-* Description : Simple UDP ping example for Windows (VS2013)
+* Description : Simple UDP ping example
 ********************************************************************************/
 
-#include <atomic>      // std::atomic
-#include <iostream>    // std::cout, std::cerr
-#include <string>      // std::string
-#include <thread>      // std::thread
-#include <WS2tcpip.h>  // Provides sockets et al on Windows
+#include <atomic>
+#include <iostream>
+#include <string>
+#include <thread>
 
-#include "zmq.hpp"     // ZeroMQ cpp bindings
+#ifdef _WIN32
+#include <WS2tcpip.h>
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#endif // _WIN32
 
-#pragma comment(lib, "Ws2_32.lib") // Tells linker to include Ws2_32.lib
+#include "zmq.hpp"
 
-// Define some values
+#ifdef _WIN32
+#pragma comment(lib, "Ws2_32.lib")
+#endif // _WIN32
+
 #define PING_PORT_NUMBER    9999
 #define PING_MSG_SIZE       2
 #define PING_INTERVAL       500
 #define SOCKET_POLL_TIMEOUT 3000
 
-// Aesthetic output
 #define INFO_OUT(MSG)  std::cout << "[INFO]   " << " " << (MSG) << std::endl
 #define ERROR_OUT(MSG) std::cerr << "[ERROR]  " << " " << (MSG) << std::endl
 
-std::atomic<bool> g_threadInterupted = false;
+#ifndef _WIN32
+#define SOCKET int
+#define INVALID_SOCKET (SOCKET)(~0)
+#define SOCKET_ERROR (SOCKET)(~1)
+#define NO_ERROR 0
+#endif // _WIN32
+
+std::atomic<bool> g_threadInterupted(false);
 
 /**
-* Create a socket and use ZeroMQ to poll the socket
+* Create a socket and use ZeroMQ to poll.
 */
 void listener()
 {
+    #ifdef _WIN32
     WSADATA wsaData;
+    #endif // _WIN32
     int nResult = 0;
-    int nOptOffVal = 0;
-    int nOptOnVal = 1;
-    int nOptLen = sizeof(int);
 
+    #ifdef _WIN32
     // Initialize Winsock
     nResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (nResult != NO_ERROR)
     {
         ERROR_OUT("zmqListen : WSAStartup failed");
     }
+    #endif // _WIN32
 
     //  Create UDP socket
     SOCKET fdSocket;
@@ -75,8 +90,12 @@ void listener()
         if (items[0].revents & ZMQ_POLLIN)
         {
             char recvBuf[PING_MSG_SIZE] = {0};
+            #ifdef _WIN32
             int saSize = sizeof(struct sockaddr_in);
-            size_t size = recvfrom(fdSocket, recvBuf, PING_MSG_SIZE, 0, (sockaddr*)&saListen, &saSize);
+            #else
+            socklen_t saSize = sizeof(struct sockaddr_in);
+            #endif // _WIN32
+            size_t size = recvfrom(fdSocket, recvBuf, PING_MSG_SIZE + 1, 0, (sockaddr*)&saListen, &saSize);
             {
                 std::string ip(inet_ntoa(saListen.sin_addr));
                 INFO_OUT("received: " + std::string(recvBuf) + " from " + ip);
@@ -84,38 +103,43 @@ void listener()
         }
     }
 
+    #ifdef _WIN32
     closesocket(fdSocket);
     WSACleanup();
+    #endif // _WIN32
 }
 
 /**
-* Main function starts a listener thread and then sends out 5 broadcast pings
+* Run broadcast and listen in seperate threads
 */
 int main()
 {
     g_threadInterupted = false;
 
-    // Start listener thread
+    // Start listener in a seperate thread
     std::thread listenerThread(listener);
 
-    // Sleep for aesthetic reasons
-    Sleep(1000);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
     {
+        #ifdef _WIN32
         WSADATA wsaData;
+        #endif // _WIN32
         int nResult = 0;
         int nOptOffVal = 0;
         int nOptOnVal = 1;
         int nOptLen = sizeof(int);
 
+        #ifdef _WIN32
         // Initialize Winsock
         nResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
         if (nResult != NO_ERROR)
         {
             ERROR_OUT("broadcast : WSAStartup failed");
         }
+        #endif // _WIN32
 
-        // Create UDP socket
+        //  Create UDP socket
         SOCKET fdSocket;
         fdSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
         if (fdSocket == INVALID_SOCKET)
@@ -123,7 +147,7 @@ int main()
             ERROR_OUT("broadcast : socket creation failed");
         }
 
-        // Ask operating system to let us do broadcasts from socket
+        //  Ask operating system to let us do broadcasts from socket
         nResult = setsockopt(fdSocket, SOL_SOCKET, SO_BROADCAST, (char *)&nOptOnVal, nOptLen);
         if (nResult != NO_ERROR)
         {
@@ -136,28 +160,32 @@ int main()
         saBroadcast.sin_port = htons(PING_PORT_NUMBER);
         saBroadcast.sin_addr.s_addr = htonl(INADDR_BROADCAST);
 
-        // Broadcast 5 beacon messages
+        //  Broadcast 5 beacon messages
         for (int i = 0; i < 5; i++)
         {
             char buffer[PING_MSG_SIZE] = {0};
             strcpy(&buffer[0], "!");
-            int bytes = sendto(fdSocket, buffer, PING_MSG_SIZE, 0, (sockaddr*)&saBroadcast, sizeof(struct sockaddr_in));
+            int bytes = sendto(fdSocket, buffer, PING_MSG_SIZE + 1, 0, (sockaddr*)&saBroadcast, sizeof(struct sockaddr_in));
             if (bytes == SOCKET_ERROR)
             {
                 ERROR_OUT("broadcast : sendto failed");
             }
-            Sleep(PING_INTERVAL);
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(PING_INTERVAL));
         }
 
+        #ifdef _WIN32
         closesocket(fdSocket);
         WSACleanup();
+        #endif // _WIN32
     }
 
-    Sleep(1000);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-    // tell listener thread to stop listening and join
     g_threadInterupted = true;
+
     listenerThread.join();
 
     return 0;
 }
+
