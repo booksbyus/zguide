c0dcbab7d362ad59a2b18db7968745459d545f18
myd7349 <myd7349@gmail.com>
2015-05-13 16:47:35 +0800

    Rewrite lbbroker2

--- examples/C/lbbroker2.c
+++ examples/C/lbbroker2.c
@@ -5,54 +5,63 @@
 
 #define NBR_CLIENTS 10
 #define NBR_WORKERS 3
-#define WORKER_READY   "\001"      //  Signals worker is ready
+#define WORKER_READY   "READY"      //  Signals worker is ready
 
 //  Basic request-reply client using REQ socket
 //
 static void *
-client_task (void *args)
+client_task(void *args)
 {
-    zctx_t *ctx = zctx_new ();
-    void *client = zsocket_new (ctx, ZMQ_REQ);
-    zsocket_connect (client, "ipc://frontend.ipc");
+	zctx_t *ctx = zctx_new();
+	void *client = zsocket_new(ctx, ZMQ_REQ);
 
-    //  Send request, get reply
-    while (true) {
-        zstr_send (client, "HELLO");
-        char *reply = zstr_recv (client);
-        if (!reply)
-            break;
-        printf ("Client: %s\n", reply);
-        free (reply);
-        sleep (1);
-    }
-    zctx_destroy (&ctx);
-    return NULL;
+#if (defined (WIN32))
+	zsocket_connect(client, "tcp://localhost:5672"); // frontend
+#else
+	zsocket_connect(client, "ipc://frontend.ipc");
+#endif
+
+	//  Send request, get reply
+	zstr_send(client, "HELLO");
+	char *reply = zstr_recv(client);
+	if (reply) {
+		printf("Client: %s\n", reply);
+		free(reply);
+	}
+
+	zctx_destroy(&ctx);
+	return NULL;
 }
 
 //  Worker using REQ socket to do load-balancing
 //
 static void *
-worker_task (void *args)
+worker_task(void *args)
 {
-    zctx_t *ctx = zctx_new ();
-    void *worker = zsocket_new (ctx, ZMQ_REQ);
-    zsocket_connect (worker, "ipc://backend.ipc");
+	zctx_t *ctx = zctx_new();
+	void *worker = zsocket_new(ctx, ZMQ_REQ);
 
-    //  Tell broker we're ready for work
-    zframe_t *frame = zframe_new (WORKER_READY, 1);
-    zframe_send (&frame, worker, 0);
+#if (defined (WIN32))
+	zsocket_connect(worker, "tcp://localhost:5673"); // backend
+#else
+	zsocket_connect(worker, "ipc://backend.ipc");
+#endif
 
-    //  Process messages as they arrive
-    while (true) {
-        zmsg_t *msg = zmsg_recv (worker);
-        if (!msg)
-            break;              //  Interrupted
-        zframe_reset (zmsg_last (msg), "OK", 2);
-        zmsg_send (&msg, worker);
-    }
-    zctx_destroy (&ctx);
-    return NULL;
+	//  Tell broker we're ready for work
+	zframe_t *frame = zframe_new(WORKER_READY, strlen(WORKER_READY));
+	zframe_send(&frame, worker, 0);
+
+	//  Process messages as they arrive
+	while (1) {
+		zmsg_t *msg = zmsg_recv(worker);
+		if (!msg)
+			break;              //  Interrupted
+		zframe_print(zmsg_last(msg), "Worker: ");
+		zframe_reset(zmsg_last(msg), "OK", 2);
+		zmsg_send(&msg, worker);
+	}
+	zctx_destroy(&ctx);
+	return NULL;
 }
 
 //  .split main task
@@ -60,69 +69,95 @@ worker_task (void *args)
 //  the previous {{lbbroker}} broker example, but uses CZMQ to start child 
 //  threads, to hold the list of workers, and to read and send messages:
 
-int main (void)
+int main(void)
 {
-    zctx_t *ctx = zctx_new ();
-    void *frontend = zsocket_new (ctx, ZMQ_ROUTER);
-    void *backend = zsocket_new (ctx, ZMQ_ROUTER);
-    zsocket_bind (frontend, "ipc://frontend.ipc");
-    zsocket_bind (backend, "ipc://backend.ipc");
+	zctx_t *ctx = zctx_new();
+	void *frontend = zsocket_new(ctx, ZMQ_ROUTER);
+	void *backend = zsocket_new(ctx, ZMQ_ROUTER);
 
-    int client_nbr;
-    for (client_nbr = 0; client_nbr < NBR_CLIENTS; client_nbr++)
-        zthread_new (client_task, NULL);
-    int worker_nbr;
-    for (worker_nbr = 0; worker_nbr < NBR_WORKERS; worker_nbr++)
-        zthread_new (worker_task, NULL);
+	// IPC doesn't yet work on MS Windows.
+#if (defined (WIN32))
+	zsocket_bind(frontend, "tcp://*:5672");
+	zsocket_bind(backend, "tcp://*:5673");
+#else
+	zsocket_bind(frontend, "ipc://frontend.ipc");
+	zsocket_bind(backend, "ipc://backend.ipc");
+#endif
 
-    //  Queue of available workers
-    zlist_t *workers = zlist_new ();
+	int client_nbr;
+	for (client_nbr = 0; client_nbr < NBR_CLIENTS; client_nbr++)
+		zthread_new(client_task, NULL);
+	int worker_nbr;
+	for (worker_nbr = 0; worker_nbr < NBR_WORKERS; worker_nbr++)
+		zthread_new(worker_task, NULL);
 
-    //  .split main load-balancer loop
-    //  Here is the main loop for the load balancer. It works the same way
-    //  as the previous example, but is a lot shorter because CZMQ gives
-    //  us an API that does more with fewer calls:
-    while (true) {
-        zmq_pollitem_t items [] = {
-            { backend,  0, ZMQ_POLLIN, 0 },
-            { frontend, 0, ZMQ_POLLIN, 0 }
-        };
-        //  Poll frontend only if we have available workers
-        int rc = zmq_poll (items, zlist_size (workers)? 2: 1, -1);
-        if (rc == -1)
-            break;              //  Interrupted
+	//  Queue of available workers
+	zlist_t *workers = zlist_new();
 
-        //  Handle worker activity on backend
-        if (items [0].revents & ZMQ_POLLIN) {
-            //  Use worker identity for load-balancing
-            zmsg_t *msg = zmsg_recv (backend);
-            if (!msg)
-                break;          //  Interrupted
-            zframe_t *identity = zmsg_unwrap (msg);
-            zlist_append (workers, identity);
+	//  .split main load-balancer loop
+	//  Here is the main loop for the load balancer. It works the same way
+	//  as the previous example, but is a lot shorter because CZMQ gives
+	//  us an API that does more with fewer calls:
+	while (1) {
+		zmq_pollitem_t items[] = {
+				{ backend, 0, ZMQ_POLLIN, 0 },
+				{ frontend, 0, ZMQ_POLLIN, 0 }
+		};
+		//  Poll frontend only if we have available workers
+		int rc = zmq_poll(items, zlist_size(workers) ? 2 : 1, -1);
+		if (rc == -1)
+			break;              //  Interrupted
 
-            //  Forward message to client if it's not a READY
-            zframe_t *frame = zmsg_first (msg);
-            if (memcmp (zframe_data (frame), WORKER_READY, 1) == 0)
-                zmsg_destroy (&msg);
-            else
-                zmsg_send (&msg, frontend);
-        }
-        if (items [1].revents & ZMQ_POLLIN) {
-            //  Get client request, route to first available worker
-            zmsg_t *msg = zmsg_recv (frontend);
-            if (msg) {
-                zmsg_wrap (msg, (zframe_t *) zlist_pop (workers));
-                zmsg_send (&msg, backend);
-            }
-        }
-    }
-    //  When we're done, clean up properly
-    while (zlist_size (workers)) {
-        zframe_t *frame = (zframe_t *) zlist_pop (workers);
-        zframe_destroy (&frame);
-    }
-    zlist_destroy (&workers);
-    zctx_destroy (&ctx);
-    return 0;
+		//  Handle worker activity on backend
+		if (items[0].revents & ZMQ_POLLIN) {
+			//  Use worker identity for load-balancing
+			zmsg_t *msg = zmsg_recv(backend);
+			if (!msg)
+				break;          //  Interrupted
+
+#if 0
+			// zmsg_unwrap is DEPRECATED as over-engineered, poor style
+			zframe_t *identity = zmsg_unwrap(msg);
+#else
+			zframe_t *identity = zmsg_pop(msg);
+			zframe_t *delimiter = zmsg_pop(msg);
+			zframe_destroy(&delimiter); 
+#endif
+
+			zlist_append(workers, identity);
+
+			//  Forward message to client if it's not a READY
+			zframe_t *frame = zmsg_first(msg);
+			if (memcmp(zframe_data(frame), WORKER_READY, strlen(WORKER_READY)) == 0) {
+				zmsg_destroy(&msg);
+			} else {
+				zmsg_send(&msg, frontend);
+				if (--client_nbr == 0)
+					break; // Exit after N messages
+			}
+		}
+		if (items[1].revents & ZMQ_POLLIN) {
+			//  Get client request, route to first available worker
+			zmsg_t *msg = zmsg_recv(frontend);
+			if (msg) {
+#if 0
+				// zmsg_wrap is DEPRECATED as unsafe
+				zmsg_wrap(msg, (zframe_t *)zlist_pop(workers));
+#else
+				zmsg_pushmem(msg, NULL, 0); // delimiter
+				zmsg_push(msg, (zframe_t *)zlist_pop(workers));
+#endif
+
+				zmsg_send(&msg, backend);
+			}
+		}
+	}
+	//  When we're done, clean up properly
+	while (zlist_size(workers)) {
+		zframe_t *frame = (zframe_t *)zlist_pop(workers);
+		zframe_destroy(&frame);
+	}
+	zlist_destroy(&workers);
+	zctx_destroy(&ctx);
+	return 0;
 }
