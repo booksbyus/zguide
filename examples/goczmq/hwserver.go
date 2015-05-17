55fc793deaa2e40e3576dd0685de97b67408a101
michael guldan <michaelguldan@gmail.com>
2015-05-16 23:55:27 -0700

    copy in hwserver.go from Go examples

---
+++ examples/goczmq/hwserver.go
@@ -0,0 +1,34 @@
+//
+// Hello World Zeromq server
+//
+// Author: Aaron Raddon   github.com/araddon
+// Requires: http://github.com/alecthomas/gozmq
+//
+package main
+
+import (
+	"fmt"
+	zmq "github.com/alecthomas/gozmq"
+	"time"
+)
+
+func main() {
+	context, _ := zmq.NewContext()
+	socket, _ := context.NewSocket(zmq.REP)
+	defer context.Close()
+	defer socket.Close()
+	socket.Bind("tcp://*:5555")
+
+	// Wait for messages
+	for {
+		msg, _ := socket.Recv(0)
+		println("Received ", string(msg))
+
+		// do some fake "work"
+		time.Sleep(time.Second)
+
+		// send reply back to client
+		reply := fmt.Sprintf("World")
+		socket.Send([]byte(reply), 0)
+	}
+}
