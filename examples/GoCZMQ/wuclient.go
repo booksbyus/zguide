f4977a5b578a12988b0df2a098fa1ba7720ff362
michael guldan <michaelguldan@gmail.com>
2015-05-27 17:35:51 -0700

    port hwclient.go to goczmq

--- examples/GoCZMQ/wuclient.go
+++ examples/GoCZMQ/wuclient.go
@@ -1,23 +1,24 @@
-// 
+//
 //   Weather proxy listens to weather server which is constantly
 //   emitting weather data
 //   Binds SUB socket to tcp://*:5556
-// 
+//   Initial Commit in examples/Go bay Aaron Raddon
+//   Ported to goczmq by Michael Guldan github.com/michaelcoyote
+//   Requires: http://github.com/zeromq/goczmq
+//
 package main
 
 import (
 	"fmt"
-	zmq "github.com/alecthomas/gozmq"
+	goczmq "github.com/zeromq/goczmq"
 	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	context, _ := zmq.NewContext()
-	socket, _ := context.NewSocket(zmq.SUB)
-	defer context.Close()
-	defer socket.Close()
+	socket := goczmq.NewSock(goczmq.Sub)
+	defer socket.Destroy()
 
 	var temps []string
 	var err error
@@ -30,18 +31,18 @@ func main() {
 		filter = string(os.Args[1])
 	}
 
-	//  Subscribe to just one zipcode (whitefish MT 59937) 
+	//  Subscribe to just one zipcode (whitefish MT 59937)
 	fmt.Printf("Collecting updates from weather server for %s…\n", filter)
 	socket.SetSubscribe(filter)
 	socket.Connect("tcp://localhost:5556")
 
 	for i := 0; i < 101; i++ {
 		// found temperature point
-		datapt, _ := socket.Recv(0)
-		temps = strings.Split(string(datapt), " ")
+		datapt, _ := socket.RecvMessage()
+		temps = strings.Split(string(datapt[0]), " ")
 		temp, err = strconv.ParseInt(temps[1], 10, 64)
 		if err == nil {
-			// Invalid string 
+			// Invalid string
 			total_temp += int(temp)
 		}
 	}
