8387c13693cd6168e830feca753b4395588b88dd
michael guldan <michaelguldan@gmail.com>
2015-05-27 17:33:27 -0700

    update to correct version of the hwserver.go file

--- examples/GoCZMQ/wuserver.go
+++ examples/GoCZMQ/wuserver.go
@@ -1,50 +1,40 @@
 // 
-//   Weather proxy listens to weather server which is constantly
-//   emitting weather data
-//   Binds SUB socket to tcp://*:5556
+//   Weather update server
+//   Binds PUB socket to tcp://*:5556
+//   Publishes random weather updates
 // 
 package main
 
 import (
 	"fmt"
 	zmq "github.com/alecthomas/gozmq"
-	"os"
-	"strconv"
-	"strings"
+	"math/rand"
+	"time"
 )
 
 func main() {
 	context, _ := zmq.NewContext()
-	socket, _ := context.NewSocket(zmq.SUB)
+	socket, _ := context.NewSocket(zmq.PUB)
 	defer context.Close()
 	defer socket.Close()
+	socket.Bind("tcp://*:5556")
+	socket.Bind("ipc://weather.ipc")
 
-	var temps []string
-	var err error
-	var temp int64
-	total_temp := 0
-	filter := "59937"
+	// Seed the random number generator
+	rand.Seed(time.Now().UnixNano())
 
-	// find zipcode
-	if len(os.Args) > 1 { // ./wuclient 85678
-		filter = string(os.Args[1])
+	// loop for a while aparently
+	for {
+
+		//  make values that will fool the boss
+		zipcode := rand.Intn(100000)
+		temperature := rand.Intn(215) - 80
+		relhumidity := rand.Intn(50) + 10
+
+		msg := fmt.Sprintf("%d %d %d", zipcode, temperature, relhumidity)
+
+		//  Send message to all subscribers
+		socket.Send([]byte(msg), 0)
 	}
-
-	//  Subscribe to just one zipcode (whitefish MT 59937) 
-	fmt.Printf("Collecting updates from weather server for %s…\n", filter)
-	socket.SetSubscribe(filter)
-	socket.Connect("tcp://localhost:5556")
-
-	for i := 0; i < 101; i++ {
-		// found temperature point
-		datapt, _ := socket.Recv(0)
-		temps = strings.Split(string(datapt), " ")
-		temp, err = strconv.ParseInt(temps[1], 10, 64)
-		if err == nil {
-			// Invalid string 
-			total_temp += int(temp)
-		}
-	}
-
-	fmt.Printf("Average temperature for zipcode %s was %dF \n\n", filter, total_temp/100)
 }
+
