ca61e23e1c8ab6650b630e010ab241ed7f58d13f
michael guldan <michaelguldan@gmail.com>
2015-05-28 23:54:40 -0700

    port taskwork.go Go example to use GoCZMQ

--- examples/GoCZMQ/taskwork.go
+++ examples/GoCZMQ/taskwork.go
@@ -1,44 +1,45 @@
 //
-// Task Wroker
-// Connects PULL socket to tcp://localhost:5557
-// Collects workloads from ventilator via that socket
-// Connects PUSH socket to tcp://localhost:5558
-// Sends results to sink via that socket 
-// 
+//   Task Worker
+//   Connects PULL socket to tcp://localhost:5557
+//   Collects workloads from ventilator via that socket
+//   Connects PUSH socket to tcp://localhost:5558
+//   Sends results to sink via that socket
+//   Initial Commit in examples/Go bay Aaron Raddon
+//   Ported to goczmq by Michael Guldan github.com/michaelcoyote
+//   Requires: http://github.com/zeromq/goczmq
+//
 package main
 
 import (
 	"fmt"
-	zmq "github.com/alecthomas/gozmq"
+	goczmq "github.com/zeromq/goczmq"
 	"strconv"
 	"time"
 )
 
 func main() {
-	context, _ := zmq.NewContext()
-	defer context.Close()
 
 	//  Socket to receive messages on
-	receiver, _ := context.NewSocket(zmq.PULL)
-	defer receiver.Close()
+	receiver := goczmq.NewSock(goczmq.Pull)
+	defer receiver.Destroy()
 	receiver.Connect("tcp://localhost:5557")
 
 	//  Socket to send messages to task sink
-	sender, _ := context.NewSocket(zmq.PUSH)
-	defer sender.Close()
+	sender := goczmq.NewSock(goczmq.Push)
+	defer sender.Destroy()
 	sender.Connect("tcp://localhost:5558")
 
 	//  Process tasks forever
 	for {
-		msgbytes, _ := receiver.Recv(0)
-		fmt.Printf("%s.\n", string(msgbytes))
+		msgbytes, _ := receiver.RecvMessage()
+		fmt.Printf("%s.\n", string(msgbytes[0]))
 
 		//  Do the work
-		msec, _ := strconv.ParseInt(string(msgbytes), 10, 64)
+		msec, _ := strconv.ParseInt(string(msgbytes[0]), 10, 64)
 		time.Sleep(time.Duration(msec) * 1e6)
 
 		//  Send results to sink
-		sender.Send([]byte(""), 0)
+		sender.SendMessage([][]byte{[]byte("")})
 
 	}
 }
