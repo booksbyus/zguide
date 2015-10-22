482db7cea7005afc9a3bebb067a938fe4c305e59
Dylan Cali <calid1984@gmail.com>
2015-10-21 20:13:00 -0500

    Convert chapter 1 Perl examples to ffi bindings

--- examples/Perl/hwclient.pl
+++ examples/Perl/hwclient.pl
@@ -1,38 +1,20 @@
-#!/usr/bin/perl
-=pod
-
-Hello World client
-
-Connects REQ socket to tcp://localhost:5555
-
-Sends "Hello" to server, expects "World" back
-
-Author: Daisuke Maki (lestrrat)
-Original version Author: Alexander D'Archangel (darksuji) <darksuji(at)gmail(dot)com>
-
-=cut
+# Hello World client in Perl
 
 use strict;
 use warnings;
-use 5.10.0;
+use v5.10;
 
-use ZMQ::LibZMQ3;
-use ZMQ::Constants qw(ZMQ_REQ);
+use ZMQ::FFI;
+use ZMQ::FFI::Constants qw(ZMQ_REQ);
 
-my $MAX_MSGLEN = 255;
-
-my $context = zmq_init();
-
-# Socket to talk to server
-say 'Connecting to hello world server...';
-my $requester = zmq_socket($context, ZMQ_REQ);
-zmq_connect($requester, 'tcp://localhost:5555');
+say "Connecting to hello world server...";
+my $context = ZMQ::FFI->new();
+my $requestor = $context->socket(ZMQ_REQ);
+$requestor->connect("tcp://localhost:5555");
 
 for my $request_nbr (0..9) {
-    say "Sending request $request_nbr...";
-    zmq_send($requester, 'Hello');
-    my $message;
-    my $size = zmq_recv($requester, $message, $MAX_MSGLEN);
-    my $reply = substr($message, 0, $size);
-    say "Received reply $request_nbr: [". $reply .']';
+    say "Sending Hello $request_nbr...";
+    $requestor->send("Hello");
+    $requestor->recv();
+    say "Received World $request_nbr";
 }
--- examples/Perl/wuserver.pl
+++ examples/Perl/wuserver.pl
@@ -1,46 +1,25 @@
-#!/usr/bin/perl
-=pod
-
-Weather update server
-
-Binds PUB socket to tcp://*:5556
-
-Publishes random weather updates
-
-Author: Daisuke Maki (lestrrat)
-Original Author: Alexander D'Archangel (darksuji) <darksuji(at)gmail(dot)com>
-
-=cut
+# Weather update server in Perl
+# Binds PUB socket to tcp://*:5556
+# Publishes random weather updates
 
 use strict;
 use warnings;
-use 5.10.0;
 
-use ZMQ::LibZMQ3;
-use ZMQ::Constants qw(ZMQ_PUB);
-use zhelpers;
+use ZMQ::FFI;
+use ZMQ::FFI::Constants qw(ZMQ_PUB);
 
-sub within {
-    my ($upper) = @_;
+my $context   = ZMQ::FFI->new();
+my $publisher = $context->socket(ZMQ_PUB);
+$publisher->bind("tcp://*:5556");
 
-    return int(rand($upper)) + 1;
-}
+my ($update, $zipcode, $temperature, $relhumidity);
 
-# Prepare our context and publisher
-my $context = zmq_init();
-my $publisher = zmq_socket($context, ZMQ_PUB);
-zmq_bind($publisher, 'tcp://*:5556');
-zmq_bind($publisher, 'ipc://weather.ipc');
-
-# Initialize random number generator
-srand();
 while (1) {
-    # Get values that will fool the boss
-    my $zipcode     = within(100_000);
-    my $temperature = within(215) - 80;
-    my $relhumidity = within(50) + 10;
+    $zipcode     = rand(100_000);
+    $temperature = rand(215) - 80;
+    $relhumidity = rand(50) + 10;
 
-    # Send message to all subscribers
-    my $update = sprintf('%05d %d %d', $zipcode, $temperature, $relhumidity);
-    s_send($publisher, $update);
+    $update = sprintf('%d %d %d', $zipcode,$temperature,$relhumidity);
+
+    $publisher->send($update);
 }
