2fedf6f270a8f3303732c15dc6a3b20c358cd0f0
Dylan Cali <calid1984@gmail.com>
2015-10-22 21:57:42 -0500

    Convert chapter 3 Perl examples to ffi bindings
    also removed lbbroker_zmqv4.pl as separate zmq 4 example no longer necessary

--- examples/Perl/zhelpers.pm
+++ examples/Perl/zhelpers.pm
@@ -1,73 +1,39 @@
-=pod
+package zhelpers;
 
-Helper module for example applications.
+use strict;
+use warnings;
+use v5.10;
 
-Author: Klaas Nijkes
+sub dump {
+    my ($socket) = @_;
 
-=cut
+    say "----------------------------------------";
 
-use ZMQ::LibZMQ3;
-use ZMQ::Constants qw(:all);
-use Time::HiRes qw(usleep);
+    for my $message ($socket->recv_multipart()) {
+        my $msg_len = length($message);
 
-my $MAX_MSGLEN = 255;
+        my $is_text = 1;
 
-sub s_recv {
-    my $socket = shift;
-    my $size = zmq_recv($socket, $buf, $MAX_MSGLEN);
-    return undef if ($size < 0);
-    return substr($buf, 0, $size);
-}
+        CHECK_TEXT:
+        for (my $i = 0; $i < $msg_len; $i++) {
+            my $c = ord(substr($message, $i, 1));
 
-sub s_send {
-    my $socket = shift;
-    my $string = shift;
-    return zmq_send($socket, $string, -1);
-}
-
-sub s_sendmore {
-    my $socket = shift;
-    my $string = shift;
-    my $more = shift;
-    return zmq_send($socket, $string, -1, ZMQ_SNDMORE);
-}
-
-sub s_set_id {
-    my $socket = shift;
-    my $identity = sprintf("%04X-%04X", int(rand(10000)), int(rand(10000)));
-    zmq_setsockopt($socket, ZMQ_IDENTITY, $identity);
-}
-    
-sub s_dump {
-    my $socket = shift;
-    print "----------------------------------------\n";
-    while (1) {
-        my $message = zmq_recvmsg($socket);
-        my $size = zmq_msg_size($message);
-        my $data = zmq_msg_data($message);
-        my $isText = 1;
-        foreach $byte (split //, $data) {
-            if (ord($byte) < 32 || ord($byte) > 127) {
-                $isText = 0;
-                last;
+            if ($c < 32 || $c > 126) {
+                $is_text = 0;
+                last CHECK_TEXT;
             }
         }
-        my $str = "";
-        if ($isText) {
-            $str = $data;
-        } else {
-            foreach $byte (split //, $data) {
-                $str .= sprintf("%02X", ord($byte));
-            }
+
+        printf "[%03d] ", $msg_len;
+
+        if ($is_text) {
+            say $message;
         }
-        printf("[%03d] $str\n", $size);
-        last if (not zmq_getsockopt($socket, ZMQ_RCVMORE))
+        else {
+            say uc(unpack("H*", $message));
+        }
     }
-}
 
-sub s_sleep {
-    my $msecs = shift;
-    usleep(1000 * int($msecs));
 }
 
 1;
