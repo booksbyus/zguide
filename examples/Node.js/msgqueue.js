58305e496bb7e4cc69775436605d2251a9fefbb8
Abel Allison <abel.allison@gmail.com>
2016-11-07 16:41:53 -0800

    Node.js msgqueue example

---
+++ examples/Node.js/msgqueue.js
@@ -0,0 +1,23 @@
+//  Simple message queuing broker
+//  Same as request-reply broker but using shared queue proxy
+
+var zmq = require('zmq');
+
+//  Socket facing clients
+var frontend = zmq.socket('router');
+console.log('binding frontend...');
+frontend.bindSync('tcp://*:5559');
+
+//  Socket facing services
+var backend = zmq.socket('dealer');
+console.log('binding backend...');
+backend.bindSync('tcp://*:5560');
+
+//  Start the proxy
+console.log('starting proxy...');
+zmq.proxy(frontend, backend, null);
+
+process.on('SIGINT', function() {
+  frontend.close();
+  backend.close();
+});
