c74fdda865dfec2db17afc818f05892b5a10de8b
Kevin Sapper <mail@kevinsapper.de>
2019-08-29 12:47:49 +0200

    Update Java examples from JeroMQ guide package

--- examples/Java/mtrelay.java
+++ examples/Java/mtrelay.java
@@ -1,9 +1,8 @@
 package guide;
 
 import org.zeromq.SocketType;
-import org.zeromq.ZMQ;
-import org.zeromq.ZMQ.Socket;
 import org.zeromq.ZContext;
+import org.zeromq.ZMQ.Socket;
 
 /**
  * Multithreaded relay
@@ -48,8 +47,6 @@ public class mtrelay
             //  Bind to inproc: endpoint, then start upstream thread
             Socket receiver = context.createSocket(SocketType.PAIR);
             receiver.bind("inproc://step2");
-            Thread step1 = new Step1(context);
-            step1.start();
 
             //  Wait for signal
             receiver.recv(0);
@@ -58,27 +55,56 @@ public class mtrelay
             //  Connect to step3 and tell it we're ready
             Socket xmitter = context.createSocket(SocketType.PAIR);
             xmitter.connect("inproc://step3");
+            System.out.println("Step 2 ready, signaling step 3");
             xmitter.send("READY", 0);
-
             xmitter.close();
         }
 
     }
 
-    public static void main(String[] args)
+    private static class Step3 extends Thread
     {
-        try (ZContext context = new ZContext()) {
+        private ZContext context;
+
+        private Step3(ZContext context)
+        {
+            this.context = context;
+        }
+
+        @Override
+        public void run()
+        {
             //  Bind to inproc: endpoint, then start upstream thread
             Socket receiver = context.createSocket(SocketType.PAIR);
             receiver.bind("inproc://step3");
 
-            //  Step 2 relays the signal to step 3
-            Thread step2 = new Step2(context);
-            step2.start();
-
             //  Wait for signal
             receiver.recv(0);
             receiver.close();
+
+            System.out.println("Step 3 ready");
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException
+    {
+        try (ZContext context = new ZContext()) {
+            //  Step 1 signals to step 2
+            Thread step1 = new Step1(context);
+            step1.start();
+
+            //  Step 2 relays the signal from step 1 to step 3
+            Thread step2 = new Step2(context);
+            step2.start();
+
+            //  Step 3 waits for signal from step 2
+            Thread step3 = new Step3(context);
+            step3.start();
+
+            step1.join();
+            step2.join();
+            step3.join();
 
             System.out.println("Test successful!");
         }
