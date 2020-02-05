84b654620d2306a9bcb1fd9625ab92d60aa656c1
Brett Viren <brett.viren@gmail.com>
2020-02-05 16:54:41 -0500

    Problem: A forgotten Majordomo example does not work with Python3
    Solution: fix it.

--- examples/Python/mmiecho.py
+++ examples/Python/mmiecho.py
@@ -11,14 +11,14 @@ from mdcliapi import MajorDomoClient
 def main():
     verbose = '-v' in sys.argv
     client = MajorDomoClient("tcp://localhost:5555", verbose)
-    request = "echo"
-    reply = client.send("mmi.service", request)
+    request = b"echo"
+    reply = client.send(b"mmi.service", request)
 
     if reply:
         replycode = reply[0]
-        print "Lookup echo service:", replycode
+        print ("Lookup echo service:", replycode)
     else:
-        print "E: no response from broker, make sure it's running"
+        print ("E: no response from broker, make sure it's running")
 
 if __name__ == '__main__':
     main()
