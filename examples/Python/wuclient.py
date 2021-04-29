99340807dc759c344c567439139d5d7d5ac2d15e
pktl <peter@bckrnet.de>
2021-04-29 11:46:39 +0200

    Delete obsolete string decoding (only necessary for Py2 which is EOL)

--- examples/Python/wuclient.py
+++ examples/Python/wuclient.py
@@ -17,10 +17,6 @@ socket.connect("tcp://localhost:5556")
 
 # Subscribe to zipcode, default is NYC, 10001
 zip_filter = sys.argv[1] if len(sys.argv) > 1 else "10001"
-
-# Python 2 - ascii bytes to unicode str
-if isinstance(zip_filter, bytes):
-    zip_filter = zip_filter.decode("ascii")
 socket.setsockopt_string(zmq.SUBSCRIBE, zip_filter)
 
 # Process 5 updates
