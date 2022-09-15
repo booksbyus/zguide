b947b3ec9cc8f061776d05092a15443d56f8ab88
Cedric Schmeits <cedric@schmeits.net>
2022-09-15 15:33:31 +0200

    replaced all tabs by 4 spaces
    So that the alignment will show correctly in the documentation

--- examples/C++/rrclient.cpp
+++ examples/C++/rrclient.cpp
@@ -9,15 +9,15 @@ int main (int argc, char *argv[])
 {
     zmq::context_t context(1);
 
-	zmq::socket_t requester(context, ZMQ_REQ);
-	requester.connect("tcp://localhost:5559");
+    zmq::socket_t requester(context, ZMQ_REQ);
+    requester.connect("tcp://localhost:5559");
  
-	for( int request = 0 ; request < 10 ; request++) {
-		
-		s_send (requester, std::string("Hello"));
+    for( int request = 0 ; request < 10 ; request++) {
+        
+        s_send (requester, std::string("Hello"));
         std::string string = s_recv (requester);
 		
-		std::cout << "Received reply " << request 
-				<< " [" << string << "]" << std::endl;
+        std::cout << "Received reply " << request 
+                  << " [" << string << "]" << std::endl;
 	}
 }
